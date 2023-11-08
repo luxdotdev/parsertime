ce8c1a5614c0fc12cc59e9a3226c74d38c63c857
Lucas Doell <lmd248@cornell.edu>
2023-11-08 08:24:57 -0500

    Add xlsx dependency and hero and parser types

---
+++ types/heroes.ts
@@ -0,0 +1,225 @@
+export type Hero =
+  | Ana
+  | Ashe
+  | Baptiste
+  | Bastion
+  | Brigitte
+  | Cassidy
+  | Doomfist
+  | Dva
+  | Echo
+  | Genji
+  | Hanzo
+  | Illari
+  | JunkerQueen
+  | Junkrat
+  | Kiriko
+  | Lifeweaver
+  | Lucio
+  | Mei
+  | Mercy
+  | Moira
+  | Orisa
+  | Pharah
+  | Reaper
+  | Reinhardt
+  | Roadhog
+  | Sigma
+  | Sojourn
+  | Soldier76
+  | Sombra
+  | Symmetra
+  | Torbjorn
+  | Tracer
+  | Widowmaker
+  | Winston
+  | WreckingBall
+  | Zarya
+  | Zenyatta;
+
+export type HeroName = Hero["name"];
+
+export type Ana = {
+  name: "Ana";
+  image: "ana.png";
+};
+
+export type Ashe = {
+  name: "Ashe";
+  image: "ashe.png";
+};
+
+export type Baptiste = {
+  name: "Baptiste";
+  image: "baptiste.png";
+};
+
+export type Bastion = {
+  name: "Bastion";
+  image: "bastion.png";
+};
+
+export type Brigitte = {
+  name: "Brigitte";
+  image: "brigitte.png";
+};
+
+export type Cassidy = {
+  name: "Cassidy";
+  image: "cassidy.png";
+};
+
+export type Doomfist = {
+  name: "Doomfist";
+  image: "doomfist.png";
+};
+
+export type Dva = {
+  name: "D.Va";
+  image: "dva.png";
+};
+
+export type Echo = {
+  name: "Echo";
+  image: "echo.png";
+};
+
+export type Genji = {
+  name: "Genji";
+  image: "genji.png";
+};
+
+export type Hanzo = {
+  name: "Hanzo";
+  image: "hanzo.png";
+};
+
+export type Illari = {
+  name: "Illari";
+  image: "illari.png";
+};
+
+export type JunkerQueen = {
+  name: "Junker Queen";
+  image: "junkerqueen.png";
+};
+
+export type Junkrat = {
+  name: "Junkrat";
+  image: "junkrat.png";
+};
+
+export type Kiriko = {
+  name: "Kiriko";
+  image: "kiriko.png";
+};
+
+export type Lifeweaver = {
+  name: "Lifeweaver";
+  image: "lifeweaver.png";
+};
+
+export type Lucio = {
+  name: "Lúcio";
+  image: "lucio.png";
+};
+
+export type Mei = {
+  name: "Mei";
+  image: "mei.png";
+};
+
+export type Mercy = {
+  name: "Mercy";
+  image: "mercy.png";
+};
+
+export type Moira = {
+  name: "Moira";
+  image: "moira.png";
+};
+
+export type Orisa = {
+  name: "Orisa";
+  image: "orisa.png";
+};
+
+export type Pharah = {
+  name: "Pharah";
+  image: "pharah.png";
+};
+
+export type Reaper = {
+  name: "Reaper";
+  image: "reaper.png";
+};
+
+export type Reinhardt = {
+  name: "Reinhardt";
+  image: "reinhardt.png";
+};
+
+export type Roadhog = {
+  name: "Roadhog";
+  image: "roadhog.png";
+};
+
+export type Sigma = {
+  name: "Sigma";
+  image: "sigma.png";
+};
+
+export type Sojourn = {
+  name: "Sojourn";
+  image: "sojourn.png";
+};
+
+export type Soldier76 = {
+  name: "Soldier: 76";
+  image: "soldier76.png";
+};
+
+export type Sombra = {
+  name: "Sombra";
+  image: "sombra.png";
+};
+
+export type Symmetra = {
+  name: "Symmetra";
+  image: "symmetra.png";
+};
+
+export type Torbjorn = {
+  name: "Torbjörn";
+  image: "torbjorn.png";
+};
+
+export type Tracer = {
+  name: "Tracer";
+  image: "tracer.png";
+};
+
+export type Widowmaker = {
+  name: "Widowmaker";
+  image: "widowmaker.png";
+};
+
+export type Winston = {
+  name: "Winston";
+  image: "winston.png";
+};
+
+export type WreckingBall = {
+  name: "Wrecking Ball";
+  image: "wreckingball.png";
+};
+
+export type Zarya = {
+  name: "Zarya";
+  image: "zarya.png";
+};
+
+export type Zenyatta = {
+  name: "Zenyatta";
+  image: "zenyatta.png";
+};
---
+++ types/parser.ts
@@ -0,0 +1,222 @@
+import { HeroName } from "./heroes";
+
+export type EventType =
+  | "defensive_assist"
+  | "hero_spawn"
+  | "hero_swap"
+  | "kill"
+  | "match_start"
+  | "objective_captured"
+  | "objective_updated"
+  | "offensive_assist"
+  | "payload_progress"
+  | "player_stat"
+  | "round_end"
+  | "round_start"
+  | "setup_complete"
+  | "ultimate_charged"
+  | "ultimate_end"
+  | "ultimate_start";
+
+type DefaultPlayerTeam = "Team 1" | "Team 2";
+type PlayerTeam = DefaultPlayerTeam | string; // not sure how it handles custom team names
+
+type EventAbility =
+  | "Primary Fire"
+  | "Secondary Fire"
+  | "Ability 1"
+  | "Ability 2"
+  | "Ultimate"
+  | "Melee";
+
+export type DefensiveAssistTable = {
+  event_type: "defensive_assist";
+  match_time: number;
+  player_team: PlayerTeam;
+  player_name: string;
+  player_hero: HeroName;
+  hero_duplicated: number;
+};
+
+export type HeroSpawnTable = {
+  event_type: "hero_spawn";
+  match_time: number;
+  player_team: PlayerTeam;
+  player_name: string;
+  player_hero: HeroName;
+  previous_hero: HeroName | null;
+  hero_time_played: number;
+};
+
+export type HeroSwapTable = {
+  event_type: "hero_swap";
+  match_time: number;
+  player_team: PlayerTeam;
+  player_name: string;
+  player_hero: HeroName;
+  previous_hero: HeroName;
+  hero_time_played: number;
+};
+
+export type KillTable = {
+  event_type: "kill";
+  match_time: number;
+  attacker_team: PlayerTeam;
+  attacker_name: string;
+  attacker_hero: HeroName;
+  victim_team: PlayerTeam;
+  victim_name: string;
+  victim_hero: HeroName;
+  event_ability: EventAbility;
+  event_damage: number;
+  is_critical_hit: boolean;
+  is_environmental: boolean;
+};
+
+export type MatchStartTable = {
+  event_type: "match_start";
+  match_time: number;
+  map_name: string;
+  map_type: string;
+  team_1_name: string;
+  team_2_name: string;
+};
+
+export type ObjectiveCapturedTable = {
+  event_type: "objective_captured";
+  match_time: number;
+  round_number: number;
+  capturing_team: PlayerTeam;
+  objective_index: number;
+  control_team_1_progress: number;
+  control_team_2_progress: number;
+  match_time_remaining: number;
+};
+
+export type ObjectiveUpdatedTable = {
+  event_type: "objective_updated";
+  match_time: number;
+  round_number: number;
+  previous_objective_index: number;
+  current_objective_index: number;
+};
+
+export type OffensiveAssistTable = {
+  event_type: "offensive_assist";
+  match_time: number;
+  player_team: PlayerTeam;
+  player_name: string;
+  player_hero: HeroName;
+  hero_duplicated: number;
+};
+
+export type PayloadProgressTable = {
+  event_type: "payload_progress";
+  match_time: number;
+  round_number: number;
+  capturing_team: PlayerTeam;
+  objective_index: number;
+  payload_capture_progress: number;
+};
+
+export type PlayerStatTable = {
+  event_type: "player_stat";
+  match_time: number;
+  round_number: number;
+  player_team: PlayerTeam;
+  player_name: string;
+  player_hero: HeroName;
+  eliminations: number;
+  final_blows: number;
+  deaths: number;
+  all_damage_dealt: number;
+  barrier_damage_dealt: number;
+  hero_damage_dealt: number;
+  healing_dealt: number;
+  healing_received: number;
+  self_healing: number;
+  damage_taken: number;
+  damage_blocked: number;
+  defensive_assists: number;
+  offensive_assists: number;
+  ultimates_earned: number;
+  ultimates_used: number;
+  multikill_best: number;
+  multikills: number;
+  solo_kills: number;
+  objective_kills: number;
+  environmental_kills: number;
+  environmental_deaths: number;
+  critical_hits: number;
+  critical_hit_accuracy: number;
+  scoped_accuracy: number;
+  scoped_critical_hit_accuracy: number;
+  scoped_critical_hit_kills: number;
+  shots_fired: number;
+  shots_hit: number;
+  shots_missed: number;
+  scoped_shots: number;
+  scoped_shots_hit: number;
+  weapon_accuracy: number;
+  hero_time_played: number;
+};
+
+export type RoundEndTable = {
+  event_type: "round_end";
+  match_time: number;
+  round_number: number;
+  capturing_team: PlayerTeam;
+  team_1_score: number;
+  team_2_score: number;
+  objective_index: number;
+  control_team_1_progress: number;
+  control_team_2_progress: number;
+  match_time_remaining: number;
+};
+
+export type RoundStartTable = {
+  event_type: "round_start";
+  match_time: number;
+  round_number: number;
+  capturing_team: PlayerTeam;
+  team_1_score: number;
+  team_2_score: number;
+  objective_index: number;
+};
+
+export type SetupCompleteTable = {
+  event_type: "setup_complete";
+  match_time: number;
+  round_number: number;
+  match_time_remaining: number;
+};
+
+export type UltimateChargedTable = {
+  event_type: "ultimate_charged";
+  match_time: number;
+  player_team: PlayerTeam;
+  player_name: string;
+  player_hero: HeroName;
+  hero_duplicated: number;
+  ultimate_id: number;
+};
+
+export type UltimateEndTable = {
+  event_type: "ultimate_end";
+  match_time: number;
+  player_team: PlayerTeam;
+  player_name: string;
+  player_hero: HeroName;
+  hero_duplicated: number;
+  ultimate_id: number;
+};
+
+export type UltimateStartTable = {
+  event_type: "ultimate_start";
+  match_time: number;
+  player_team: PlayerTeam;
+  player_name: string;
+  player_hero: HeroName;
+  hero_duplicated: number;
+  ultimate_id: number;
+};
