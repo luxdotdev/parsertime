5fa338838a2c76b62cfbfc9f2b6ec590b01aa662
Lucas Doell <lmd248@cornell.edu>
2023-11-08 12:57:21 -0500

    Add toHero function to utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function toHero(name: string) {
+  // remove any accents and spaces, then convert to lowercase
+  return name
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/\s/g, "")
+    .toLowerCase();
+}
