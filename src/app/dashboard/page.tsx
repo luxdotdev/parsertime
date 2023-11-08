d4ac65ffd057c6d4755ab2aae67ce1d964a9b9f5
Lucas Doell <lmd248@cornell.edu>
2023-11-08 12:30:38 -0500

    rename data page to dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import PlayerSwitcher from "@/components/dashboard/player-switcher";
 import { ParserDataContext } from "@/lib/parser-context";
 import { useContext } from "react";
 
@@ -7,5 +8,9 @@ export default function Data() {
   const { data } = useContext(ParserDataContext);
   console.log(data);
 
-  return <main>Hello world</main>;
+  return (
+    <main>
+      <PlayerSwitcher />
+    </main>
+  );
 }
