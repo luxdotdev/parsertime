9e2b1d04c585f7cc4663c0570b691d43f325f806
Lucas Doell <lmd248@cornell.edu>
2023-11-09 04:52:22 -0500

    Refactor dashboard page component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,20 +1,11 @@
-"use client";
-
-import PlayerSwitcher, {
-  SelectedPlayerContext,
-} from "@/components/dashboard/player-switcher";
-import { MainNav } from "@/components/dashboard/main-nav";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { ModeToggle } from "@/components/theme-switcher";
 import { DefaultOverview } from "@/components/dashboard/default-overview";
-import { useContext } from "react";
-import { PlayerOverview } from "@/components/dashboard/player-overview";
-import { UserNav } from "@/components/dashboard/user-nav";
+import { MainNav } from "@/components/dashboard/main-nav";
+import PlayerSwitcher from "@/components/dashboard/player-switcher";
 import { Search } from "@/components/dashboard/search";
+import { UserNav } from "@/components/dashboard/user-nav";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function DashboardPage() {
-  const { selectedPlayer } = useContext(SelectedPlayerContext);
-
   return (
     <>
       <div className="hidden flex-col md:flex">
@@ -30,19 +21,7 @@ export default function DashboardPage() {
         </div>
         <div className="flex-1 space-y-4 p-8 pt-6">
           <div className="flex items-center justify-between space-y-2">
-            <h2 className="text-3xl font-bold tracking-tight">{`
-            ${
-              selectedPlayer.value === "default"
-                ? ""
-                : selectedPlayer.label +
-                  `'${
-                    selectedPlayer.label[selectedPlayer.label.length - 1] ===
-                    "s"
-                      ? ""
-                      : "s"
-                  } `
-            }
-            Dashboard`}</h2>
+            <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
           </div>
           <Tabs defaultValue="overview" className="space-y-4">
             <TabsList>
@@ -52,11 +31,7 @@ export default function DashboardPage() {
               <TabsTrigger value="notifications">Notifications</TabsTrigger>
             </TabsList>
             <TabsContent value="overview" className="space-y-4">
-              {selectedPlayer.value === "default" ? (
-                <DefaultOverview />
-              ) : (
-                <PlayerOverview />
-              )}
+              <DefaultOverview />
             </TabsContent>
           </Tabs>
         </div>
