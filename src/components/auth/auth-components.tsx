fd41dbb11b3d2e654036ca362e87d70edeb73ab8
Lucas Doell <lmd248@cornell.edu>
2023-11-09 04:30:38 -0500

    Add sign in and sign out components to auth section

---
+++ src/components/auth/auth-components.tsx
@@ -0,0 +1,37 @@
+import { Button } from "@/components/ui/button";
+import { signIn, signOut } from "@/lib/auth";
+import { redirect } from "next/navigation";
+
+export function SignIn({
+  provider,
+  ...props
+}: { provider?: string } & React.ComponentPropsWithRef<typeof Button>) {
+  return (
+    <form
+      action={async () => {
+        "use server";
+        const url = await signIn(provider, { redirect: false });
+        // TODO: fix in next-auth
+        redirect(url.replace("signin", "api/auth/signin"));
+      }}
+    >
+      <Button {...props}>Sign In</Button>
+    </form>
+  );
+}
+
+export function SignOut(props: React.ComponentPropsWithRef<typeof Button>) {
+  return (
+    <form
+      action={async () => {
+        "use server";
+        await signOut();
+      }}
+      className="w-full"
+    >
+      <Button variant="ghost" className="w-full p-0" {...props}>
+        Sign Out
+      </Button>
+    </form>
+  );
+}
