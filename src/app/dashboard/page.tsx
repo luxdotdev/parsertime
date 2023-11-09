1bd52b19bca603753ac6234e8bf47a4bc7712d45
Lucas Doell <lmd248@cornell.edu>
2023-11-08 23:41:34 -0500

    Add dashboard layout with tabs and components

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,44 @@
 "use client";
 
 import PlayerSwitcher from "@/components/dashboard/player-switcher";
-import { ParserDataContext } from "@/lib/parser-context";
-import { useContext } from "react";
+import { MainNav } from "@/components/dashboard/main-nav";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { ModeToggle } from "@/components/theme-switcher";
+import { Overview } from "@/components/dashboard/overview";
 
-export default function Data() {
-  const { data } = useContext(ParserDataContext);
-  console.log(data);
-
+export default function DashboardPage() {
   return (
-    <main>
-      <PlayerSwitcher />
-    </main>
+    <>
+      <div className="hidden flex-col md:flex">
+        <div className="border-b">
+          <div className="flex h-16 items-center px-4">
+            <PlayerSwitcher />
+            <MainNav className="mx-6" />
+            <div className="ml-auto flex items-center space-x-4">
+              <ModeToggle />
+            </div>
+          </div>
+        </div>
+        <div className="flex-1 space-y-4 p-8 pt-6">
+          <div className="flex items-center justify-between space-y-2">
+            <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
+          </div>
+          <Tabs defaultValue="overview" className="space-y-4">
+            <TabsList>
+              <TabsTrigger value="overview">Overview</TabsTrigger>
+              <TabsTrigger value="analytics">Analytics</TabsTrigger>
+              <TabsTrigger value="reports">Reports</TabsTrigger>
+              <TabsTrigger value="notifications">Notifications</TabsTrigger>
+            </TabsList>
+            <TabsContent value="overview" className="space-y-4">
+              <Overview />
+            </TabsContent>
+            <TabsContent value="analytics" className="space-y-4">
+              <Overview />
+            </TabsContent>
+          </Tabs>
+        </div>
+      </div>
+    </>
   );
 }
