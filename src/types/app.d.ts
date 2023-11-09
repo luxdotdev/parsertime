38b7b2a562abf9410eaa576d76925d5456232c31
Lucas Doell <lmd248@cornell.edu>
2023-11-09 04:09:34 -0500

    Add new environment variables to app.d.ts

--- src/types/app.d.ts
+++ src/types/app.d.ts
@@ -1,9 +1,13 @@
 import { z } from "zod";
 
 const envVariables = z.object({
+  DATABASE_URL: z.string(),
   GITHUB_CLIENT_ID: z.string(),
   GITHUB_CLIENT_SECRET: z.string(),
-  DATABASE_URL: z.string(),
+  DISCORD_CLIENT_ID: z.string(),
+  DISCORD_CLIENT_SECRET: z.string(),
+  GOOGLE_CLIENT_ID: z.string(),
+  GOOGLE_CLIENT_SECRET: z.string(),
 });
 
 envVariables.parse(process.env);
