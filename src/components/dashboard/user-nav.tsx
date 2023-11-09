473671aeb6122972576af90d872257716e2d60cd
Lucas Doell <lmd248@cornell.edu>
2023-11-09 04:52:38 -0500

    Add sign-in and sign-out functionality to UserNav component.

--- src/components/dashboard/user-nav.tsx
+++ src/components/dashboard/user-nav.tsx
@@ -1,3 +1,4 @@
+import { SignInButton, SignOutButton } from "@/components/auth/auth-components";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import {
@@ -10,14 +11,20 @@ import {
   DropdownMenuShortcut,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { auth, signOut } from "@/lib/auth";
 
-export function UserNav() {
+export async function UserNav() {
+  const session = await auth();
+  if (!session?.user) return <SignInButton />;
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
           <Avatar className="h-8 w-8">
-            <AvatarImage src="/avatars/01.png" alt="@shadcn" />
+            <AvatarImage
+              src={session?.user?.image || "/heroes/default"}
+              alt={session?.user?.name || "User"}
+            />
             <AvatarFallback>SC</AvatarFallback>
           </Avatar>
         </Button>
@@ -25,9 +32,11 @@ export function UserNav() {
       <DropdownMenuContent className="w-56" align="end" forceMount>
         <DropdownMenuLabel className="font-normal">
           <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">shadcn</p>
+            <p className="text-sm font-medium leading-none">
+              {session?.user?.name || ""}
+            </p>
             <p className="text-xs leading-none text-muted-foreground">
-              m@example.com
+              {session?.user?.email || ""}
             </p>
           </div>
         </DropdownMenuLabel>
@@ -49,8 +58,7 @@ export function UserNav() {
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
         <DropdownMenuItem>
-          Log out
-          <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+          <SignOutButton />
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
