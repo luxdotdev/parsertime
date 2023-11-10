21b3a7c4c6022e714b090917c618955a7dfa16c1
Lucas Doell <lmd248@cornell.edu>
2023-11-09 20:31:26 -0500

    Update EmptyScrimList component with new styles and height

--- src/components/dashboard/empty-scrim-list.tsx
+++ src/components/dashboard/empty-scrim-list.tsx
@@ -6,8 +6,8 @@ import { Card } from "@/components/ui/card";
 
 export function EmptyScrimList() {
   return (
-    <Card>
-      <div className="flex flex-col items-center justify-center space-y-2 h-screen">
+    <Card className="border-dashed">
+      <div className="flex flex-col items-center justify-center space-y-2 h-[36rem]">
         <h2 className="text-2xl font-bold tracking-tight">No Scrims</h2>
         <p className="text-gray-500">Create a new scrim to get started.</p>
         <CreateScrimButton />
