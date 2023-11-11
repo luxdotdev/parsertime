c570dd1deeeab86b63a16f6c33a6ad748735d5fc
Lucas Doell <lmd248@cornell.edu>
2023-11-11 03:25:50 -0500

    Add route for getting scrim data by ID

---
+++ src/app/api/get-scrim-data-by-id/route.ts
@@ -0,0 +1,24 @@
+import { getScrimDataById } from "@/lib/get-scrim-data";
+import { PrismaClient } from "@prisma/client";
+import { NextRequest } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const prisma = new PrismaClient();
+
+  const id = req.nextUrl.searchParams.get("id");
+
+  if (!id) {
+    return {
+      status: 400,
+      body: "No ID provided",
+    };
+  }
+
+  const data = await getScrimDataById(prisma, parseInt(id));
+  return new Response(JSON.stringify(data), {
+    headers: {
+      "content-type": "application/json",
+    },
+    status: 200,
+  });
+}
