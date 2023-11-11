6c8208d4b5f7f87415b9d25778de8171e6466c9c
Lucas Doell <lmd248@cornell.edu>
2023-11-11 00:47:59 -0500

    Add sign up page and update sign in page UI

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,63 @@
+import { Metadata } from "next";
+import Link from "next/link";
+
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
+import { UserAuthForm } from "@/components/auth/user-auth-form";
+
+export const metadata: Metadata = {
+  title: "Sign Up",
+  description: "Sign up for an account.",
+};
+
+export default function AuthenticationPage() {
+  return (
+    <>
+      <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+        <Link
+          href="/sign-up"
+          className={cn(
+            buttonVariants({ variant: "ghost" }),
+            "absolute right-4 top-4 md:right-8 md:top-8"
+          )}
+        >
+          Sign Up
+        </Link>
+        <div className="relative hidden h-full flex-col bg-muted p-10 text-black dark:text-white dark:border-r lg:flex">
+          <div className="absolute inset-0 bg-topography dark:bg-dark-topography" />
+          <Link href="/">
+            <div className="relative z-20 flex items-center text-lg font-medium">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="mr-2 h-6 w-6"
+              >
+                <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
+              </svg>
+              Parsertime
+            </div>
+          </Link>
+          <div className="relative z-20 mt-auto">
+            <p className="space-y-2 text-lg">Made with ❤️ by lux.dev</p>
+          </div>
+        </div>
+        <div className="lg:p-8">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+            <div className="flex flex-col space-y-2 text-center">
+              <h1 className="text-2xl font-semibold tracking-tight">Sign In</h1>
+              <p className="text-sm text-muted-foreground">
+                Enter your email below to sign in
+              </p>
+            </div>
+            <UserAuthForm />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
