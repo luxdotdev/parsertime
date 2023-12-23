327e7a6822c06f5a2659df628c727c4792acd87e
Lucas Doell <lmd248@cornell.edu>
2023-12-23 11:59:02 -0500

    Add email verification functionality

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,8 +3,10 @@ import { PrismaClient } from "@prisma/client";
 import GithubProvider from "next-auth/providers/github";
 import DiscordProvider from "next-auth/providers/discord";
 import GoogleProvider from "next-auth/providers/google";
-import EmailProvider from "next-auth/providers/email";
 import NextAuth, { NextAuthConfig } from "next-auth";
+import isEmail from "validator/lib/isEmail";
+import { render } from "@react-email/render";
+import MagicLinkEmail from "@/components/email/magic-link";
 
 const prisma = new PrismaClient();
 
@@ -26,50 +28,52 @@ export const config = {
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-    // EmailProvider({
-    //   id: "sendgrid",
-    //   type: "email",
-    //   async sendVerificationRequest({ identifier: email, url }) {
-    //     // Call the cloud Email provider API for sending emails
-    //     // See https://docs.sendgrid.com/api-reference/mail-send/mail-send
-    //     const response = await fetch("https://api.sendgrid.com/v3/mail/send", {
-    //       // The body format will vary depending on provider, please see their documentation
-    //       // for further details.
-    //       body: JSON.stringify({
-    //         personalizations: [{ to: [{ email }] }],
-    //         from: { email: "noreply@lux.dev" },
-    //         subject: "Sign in to Your page",
-    //         content: [
-    //           {
-    //             type: "text/plain",
-    //             value: `Please click here to authenticate - ${url}`,
-    //           },
-    //         ],
-    //       }),
-    //       headers: {
-    //         // Authentication will also vary from provider to provider, please see their docs.
-    //         Authorization: `Bearer ${process.env.SENDGRID_API_KEY}`,
-    //         "Content-Type": "application/json",
-    //       },
-    //       method: "POST",
-    //     });
+    {
+      id: "email",
+      type: "email",
+      name: "Email",
+      from: "",
+      server: "",
+      maxAge: 60 * 10,
+      options: {},
+      async sendVerificationRequest({ identifier: email, url }) {
+        if (!isEmail(email)) {
+          throw new Error("Invalid email address");
+        }
 
-    //     console.log(response);
+        const emailHtml = render(MagicLinkEmail({ magicLink: url }));
 
-    //     if (!response.ok) {
-    //       const { errors } = await response.json();
-    //       throw new Error(JSON.stringify(errors));
-    //     }
-    //   },
-    // }),
+        const response = await fetch("https://api.sendgrid.com/v3/mail/send", {
+          body: JSON.stringify({
+            personalizations: [{ to: [{ email }] }],
+            from: { email: "noreply@lux.dev" },
+            subject: "Sign in to Parsertime",
+            content: [
+              {
+                type: "text/html",
+                value: emailHtml,
+              },
+            ],
+          }),
+          headers: {
+            Authorization: `Bearer ${process.env.SENDGRID_API_KEY}`,
+            "Content-Type": "application/json",
+          },
+          method: "POST",
+        });
+
+        if (!response.ok) {
+          const { errors } = await response.json();
+          throw new Error(JSON.stringify(errors));
+        }
+      },
+    },
   ],
-
   callbacks: {
     async redirect({ url, baseUrl }) {
       return `${baseUrl}/dashboard`;
     },
   },
-
   pages: {
     signIn: "/sign-in",
   },
