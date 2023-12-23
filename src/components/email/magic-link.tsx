c65461f0f575a28795fb88ed5b7b4f4a5ca1fc8a
Lucas Doell <lmd248@cornell.edu>
2023-12-23 11:58:36 -0500

    add magic link email template

---
+++ src/components/email/magic-link.tsx
@@ -0,0 +1,92 @@
+import {
+  Body,
+  Container,
+  Head,
+  Heading,
+  Hr,
+  Html,
+  Img,
+  Link,
+  Preview,
+  Section,
+  Text,
+} from "@react-email/components";
+import * as React from "react";
+
+interface MagicLinkEmailProps {
+  magicLink?: string;
+}
+
+export const MagicLinkEmail = ({ magicLink }: MagicLinkEmailProps) => (
+  <Html>
+    <Head />
+    <Preview>Log in with this magic link.</Preview>
+    <Body style={main}>
+      <Container style={container}>
+        <Heading style={heading}>🪄 Your magic link</Heading>
+        <Section style={body}>
+          <Text style={paragraph}>
+            <Link style={link} href={magicLink}>
+              👉 Click here to sign in 👈
+            </Link>
+          </Text>
+          <Text style={paragraph}>
+            If you didn&apos;t request this, please ignore this email.
+          </Text>
+        </Section>
+        <Text style={paragraph}>
+          Best,
+          <br />- Parsertime Team
+        </Text>
+        <Hr style={hr} />
+        <Text style={footer}>lux.dev</Text>
+      </Container>
+    </Body>
+  </Html>
+);
+
+export default MagicLinkEmail;
+
+const main = {
+  backgroundColor: "#ffffff",
+  fontFamily:
+    '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif',
+};
+
+const container = {
+  margin: "0 auto",
+  padding: "20px 25px 48px",
+  backgroundImage: 'url("/assets/raycast-bg.png")',
+  backgroundPosition: "bottom",
+  backgroundRepeat: "no-repeat, no-repeat",
+};
+
+const heading = {
+  fontSize: "28px",
+  fontWeight: "bold",
+  marginTop: "48px",
+};
+
+const body = {
+  margin: "24px 0",
+};
+
+const paragraph = {
+  fontSize: "16px",
+  lineHeight: "26px",
+};
+
+const link = {
+  color: "#FF6363",
+};
+
+const hr = {
+  borderColor: "#dddddd",
+  marginTop: "48px",
+};
+
+const footer = {
+  color: "#8898aa",
+  fontSize: "12px",
+  marginLeft: "4px",
+};
