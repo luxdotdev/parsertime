b2d3cd4eb8d318c3be6f4b2296d3b8df0c5e0a9f
Lucas Doell <lmd248@cornell.edu>
2024-01-23 07:08:35 -0500

    Add TEST_DB_URL to envVariables

--- src/types/app.d.ts
+++ src/types/app.d.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 
 const envVariables = z.object({
   DATABASE_URL: z.string(),
+  TEST_DB_URL: z.string(),
   GITHUB_CLIENT_ID: z.string(),
   GITHUB_CLIENT_SECRET: z.string(),
   DISCORD_CLIENT_ID: z.string(),
