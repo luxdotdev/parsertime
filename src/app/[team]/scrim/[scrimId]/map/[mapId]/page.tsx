bc37348c35fc6d7d2167ebfeb88d2be86ee5a72d
Lucas Doell <lmd248@cornell.edu>
2024-01-27 19:53:01 -0500

    Add map name to dashboard

--- src/app/[team]/scrim/[scrimId]/map/[mapId]/page.tsx
+++ src/app/[team]/scrim/[scrimId]/map/[mapId]/page.tsx
@@ -6,11 +6,12 @@ import { UserNav } from "@/components/user-nav";
 import { ModeToggle } from "@/components/theme-switcher";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import prisma from "@/lib/prisma";
+import Link from "next/link";
 
 export default async function MapDashboardPage({
   params,
 }: {
-  params: { team: string; mapId: string };
+  params: { team: string; scrimId: string; mapId: string };
 }) {
   const id = parseInt(params.mapId);
 
@@ -30,6 +31,15 @@ export default async function MapDashboardPage({
     distinct: ["player_name"],
   });
 
+  const mapName = await prisma.matchStart.findFirst({
+    where: {
+      MapDataId: id,
+    },
+    select: {
+      map_name: true,
+    },
+  });
+
   return (
     <>
       <div className="hidden flex-col md:flex">
@@ -47,8 +57,17 @@ export default async function MapDashboardPage({
           </div>
         </div>
         <div className="flex-1 space-y-4 p-8 pt-6">
+          <div>
+            <h4 className="text-gray-600 dark:text-gray-400">
+              <Link href={`/${params.team}/scrim/${params.scrimId}`}>
+                &larr; Back to scrim overview
+              </Link>
+            </h4>
+          </div>
           <div className="flex items-center justify-between space-y-2">
-            <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
+            <h2 className="text-3xl font-bold tracking-tight">
+              {mapName?.map_name ?? "Dashboard"}
+            </h2>
           </div>
           <Tabs defaultValue="overview" className="space-y-4">
             <TabsList>
