8605cd56076175067c064817a1fd321931881c47
Lucas Doell <lmd248@cornell.edu>
2024-02-02 08:41:26 -0500

    Add route to delete expired tokens

---
+++ src/app/api/(team)/delete-expired-tokens/route.ts
@@ -0,0 +1,36 @@
+import { NextRequest } from "next/server";
+import prisma from "@/lib/prisma";
+import Logger from "@/lib/logger";
+
+export async function DELETE() {
+  const now = new Date();
+  const tokens = await prisma.teamInviteToken.findMany({
+    where: {
+      expires: {
+        lt: now,
+      },
+    },
+  });
+
+  Logger.log(
+    `Deleting ${tokens.length} expired token${tokens.length === 1 ? "" : "s"}`,
+    tokens.length > 0 ? tokens : ""
+  );
+
+  await prisma.teamInviteToken.deleteMany({
+    where: {
+      id: {
+        in: tokens.map((token) => token.id),
+      },
+    },
+  });
+
+  return new Response("OK", {
+    status: 200,
+  });
+}
+
+// This is necessary for using Vercel Cron Jobs
+export async function GET() {
+  return await DELETE();
+}
