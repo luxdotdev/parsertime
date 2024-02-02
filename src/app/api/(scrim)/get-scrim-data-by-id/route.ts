e70a10488b3744d5c50be50e3e0937ca30403d63
Lucas Doell <lmd248@cornell.edu>
2024-02-01 19:36:38 -0500

    Add authorization check in getScrimDataById route

--- src/app/api/(scrim)/get-scrim-data-by-id/route.ts
+++ src/app/api/(scrim)/get-scrim-data-by-id/route.ts
@@ -1,8 +1,19 @@
 import { getScrimDataById } from "@/lib/get-scrim-data";
+import Logger from "@/lib/logger";
 import { NextRequest } from "next/server";
 
 export async function GET(req: NextRequest) {
   const id = req.nextUrl.searchParams.get("id");
+  const token = req.headers.get("Authorization");
+
+  if (token !== process.env.DEV_TOKEN) {
+    Logger.log("Unauthorized request to get scrim data by id");
+    return new Response("Unauthorized", {
+      status: 401,
+    });
+  } else {
+    Logger.log("Authorized request with dev token to get scrim data by id");
+  }
 
   if (!id) {
     return new Response("No id provided", {
