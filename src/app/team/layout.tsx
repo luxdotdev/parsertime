792799b52b3695e81c0e2b8ab28c7f34914361fa
Lucas Doell <lmd248@cornell.edu>
2024-02-02 13:02:29 -0500

    Add team layout components

--- src/app/team/layout.tsx
+++ src/app/team/layout.tsx
@@ -1,3 +1,8 @@
+import { TeamSwitcher } from "@/components/dashboard/team-switcher";
+import { MainNav } from "@/components/dashboard/main-nav";
+import { Search } from "@/components/map/search";
+import { ModeToggle } from "@/components/theme-switcher";
+import { UserNav } from "@/components/user-nav";
 import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
 
@@ -6,11 +11,26 @@ export default async function TeamLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const isAuthed = await auth();
+  const session = await auth();
 
-  if (!isAuthed) {
+  if (!session) {
     redirect("/sign-in");
   }
 
-  return <>{children}</>;
+  return (
+    <div className="hidden flex-col md:flex">
+      <div className="border-b">
+        <div className="flex h-16 items-center px-4">
+          <TeamSwitcher session={session} />
+          <MainNav className="mx-6" />
+          <div className="ml-auto flex items-center space-x-4">
+            <Search />
+            <ModeToggle />
+            <UserNav />
+          </div>
+        </div>
+      </div>
+      {children}
+    </div>
+  );
 }
