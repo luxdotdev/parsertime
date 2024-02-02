b15bdc73e0b221bfe048ec78b78f932a9271805e
Lucas Doell <lmd248@cornell.edu>
2024-02-01 19:37:12 -0500

    Add authorization check in remove-scrim route

--- src/app/api/(scrim)/remove-scrim/route.ts
+++ src/app/api/(scrim)/remove-scrim/route.ts
@@ -1,9 +1,25 @@
+import { auth } from "@/lib/auth";
+import Logger from "@/lib/logger";
 import prisma from "@/lib/prisma";
 import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
   const params = req.nextUrl.searchParams;
   const id = params.get("id");
+  const token = req.headers.get("Authorization");
+
+  const session = await auth();
+
+  if (!session) {
+    if (token !== process.env.DEV_TOKEN) {
+      Logger.warn("Unauthorized request to remove scrim: ", id);
+      return new Response("Unauthorized", {
+        status: 401,
+      });
+    } else {
+      Logger.log("Authorized removal of scrim with dev token");
+    }
+  }
 
   if (!id) {
     return new Response("Missing ID", {
