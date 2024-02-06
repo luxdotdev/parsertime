358187f0f46d9c42eb456225ecb527283ba6f6da
Lucas Doell <lmd248@cornell.edu>
2024-02-06 16:18:19 -0500

    redesign hero page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,7 @@ import Image from "next/image";
 
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-topography dark:bg-dark-topography">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by uploading&nbsp;
-          <code className="font-mono font-bold">
-            a parsed scrim .xlsx spreadsheet
-          </code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none"></div>
-      </div>
-
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black">
       <HeroComponent />
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left"></div>
--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -1,40 +1,47 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { SparklesCore } from "@/components/ui/sparkles";
 
 export default function HeroComponent() {
   return (
-    <div className="container px-4 md:px-6">
-      <div className="grid gap-6 items-center">
+    <div className="h-[40rem] w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
+      <h1 className="md:text-7xl text-3xl lg:text-9xl font-bold text-center text-white relative z-20">
+        Parsertime
+      </h1>
+      <div className="w-[40rem] h-40 relative">
+        {/* Gradients */}
+        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
+        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
+        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
+        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
+
+        {/* Core component */}
+        <SparklesCore
+          background="transparent"
+          minSize={0.4}
+          maxSize={1}
+          particleDensity={1200}
+          className="w-full h-full"
+          particleColor="#FFFFFF"
+        />
+
+        {/* Radial Gradient to prevent sharp edges */}
+        <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
+
         <div className="flex flex-col justify-center space-y-4 text-center">
-          <div className="space-y-2 ">
-            <h1 className="invert dark:invert-0 text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none bg-clip-text text-transparent bg-gradient-to-r from-white to-gray-500">
-              Revolutionize Your Overwatch Scrim Experience
-            </h1>
-            <p className="max-w-[600px] text-zinc-900 md:text-xl dark:text-zinc-100 mx-auto">
-              Track your data, see your progress, and improve your team&apos;s
-              performance.
-            </p>
-          </div>
           <div className="w-full max-w-sm space-y-2 mx-auto">
-            <Button
-              className="bg-black dark:bg-white text-white dark:text-black p-2"
-              type="submit"
-              asChild
-            >
+            <Button className="bg-white text-black p-2" type="submit" asChild>
               <Link href="/sign-up">Sign Up</Link>
             </Button>
             <span className="px-2" />
-            <Button variant="outline" type="submit">
+            <Button variant="outline" className="text-zinc-100" type="submit">
               Use without Signing In
             </Button>
           </div>
           <div>
-            <p>
+            <p className="text-white">
               Already have an account?{" "}
-              <Link
-                href="/sign-in"
-                className="text-zinc-900 dark:text-zinc-100 font-semibold"
-              >
+              <Link href="/sign-in" className="text-zinc-100 font-semibold">
                 Sign In
               </Link>
             </p>
