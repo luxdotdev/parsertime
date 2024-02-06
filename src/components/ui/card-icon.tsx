0007d03e669b9aa8fdf3ca5c231845905ac6ac3a
Lucas Doell <lmd248@cornell.edu>
2024-02-05 19:24:28 -0500

    Add CardIcon component

---
+++ src/components/ui/card-icon.tsx
@@ -0,0 +1,16 @@
+export default function CardIcon({ children }: { children: React.ReactNode }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      className="h-4 w-4 text-muted-foreground"
+    >
+      {children}
+    </svg>
+  );
+}
