25658623f7f13b35e91c35a4858e32e66a46084a
Lucas Doell <lmd248@cornell.edu>
2024-02-06 18:56:22 -0500

    Use CardIcon component in DefaultOverview

--- src/components/map/default-overview.tsx
+++ src/components/map/default-overview.tsx
@@ -3,6 +3,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { removeDuplicateRows } from "@/lib/utils";
 import { PlayerStatRows } from "@/types/prisma";
 import prisma from "@/lib/prisma";
+import CardIcon from "@/components/ui/card-icon";
 
 export async function DefaultOverview({ id }: { id: number }) {
   const finalRound = await prisma.roundEnd.findFirst({
@@ -57,19 +58,10 @@ export async function DefaultOverview({ id }: { id: number }) {
             <CardTitle className="text-sm font-medium">
               Total Match Time
             </CardTitle>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="h-4 w-4 text-muted-foreground"
-            >
+            <CardIcon>
               <circle cx="12" cy="12" r="10" />
               <polyline points="12 6 12 12 16 14" />
-            </svg>
+            </CardIcon>
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-bold">
@@ -80,20 +72,11 @@ export async function DefaultOverview({ id }: { id: number }) {
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">Subscriptions</CardTitle>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="h-4 w-4 text-muted-foreground"
-            >
+            <CardIcon>
               <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
               <circle cx="9" cy="7" r="4" />
               <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
-            </svg>
+            </CardIcon>
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-bold">+2350</div>
@@ -105,19 +88,10 @@ export async function DefaultOverview({ id }: { id: number }) {
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">Sales</CardTitle>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="h-4 w-4 text-muted-foreground"
-            >
+            <CardIcon>
               <rect width="20" height="14" x="2" y="5" rx="2" />
               <path d="M2 10h20" />
-            </svg>
+            </CardIcon>
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-bold">+12,234</div>
@@ -129,18 +103,9 @@ export async function DefaultOverview({ id }: { id: number }) {
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">Active Now</CardTitle>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="h-4 w-4 text-muted-foreground"
-            >
+            <CardIcon>
               <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
-            </svg>
+            </CardIcon>
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-bold">+573</div>
