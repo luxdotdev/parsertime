86d72791b54ffdcbc4227e309077996698e7fa18
Lucas Doell <lmd248@cornell.edu>
2024-02-08 17:16:55 -0500

    add dashboard layout

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,31 @@
+import { MainNav } from "@/components/dashboard/main-nav";
+import { Search } from "@/components/dashboard/search";
+import { TeamSwitcher } from "@/components/dashboard/team-switcher";
+import { ModeToggle } from "@/components/theme-switcher";
+import { UserNav } from "@/components/user-nav";
+import { auth } from "@/lib/auth";
+
+export default async function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await auth();
+
+  return (
+    <div className="hidden flex-col md:flex">
+      <div className="border-b">
+        <div className="flex h-16 items-center px-4">
+          <TeamSwitcher session={session} />
+          <MainNav className="mx-6" />
+          <div className="ml-auto flex items-center space-x-4">
+            <Search />
+            <ModeToggle />
+            <UserNav />
+          </div>
+        </div>
+      </div>
+      {children}
+    </div>
+  );
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,15 +1,10 @@
+import { AdminScrimView } from "@/components/dashboard/admin-scrim-view";
 import { ScrimList } from "@/components/dashboard/scrim-list";
-import { TeamSwitcher } from "@/components/dashboard/team-switcher";
-import { MainNav } from "@/components/dashboard/main-nav";
-import { Search } from "@/components/dashboard/search";
-import { UserNav } from "@/components/user-nav";
-import { ModeToggle } from "@/components/theme-switcher";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { auth } from "@/lib/auth";
-import { $Enums } from "@prisma/client";
 import prisma from "@/lib/prisma";
-import { AdminScrimView } from "@/components/dashboard/admin-scrim-view";
 import { SearchParams } from "@/types/next";
+import { $Enums } from "@prisma/client";
 
 type Props = {
   searchParams: SearchParams;
@@ -28,37 +23,24 @@ export default async function DashboardPage({ searchParams }: Props) {
 
   return (
     <>
-      <div className="hidden flex-col md:flex">
-        <div className="border-b">
-          <div className="flex h-16 items-center px-4">
-            <TeamSwitcher session={session} />
-            <MainNav className="mx-6" />
-            <div className="ml-auto flex items-center space-x-4">
-              <Search />
-              <ModeToggle />
-              <UserNav />
-            </div>
-          </div>
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <div className="flex items-center justify-between space-y-2">
+          <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
         </div>
-        <div className="flex-1 space-y-4 p-8 pt-6">
-          <div className="flex items-center justify-between space-y-2">
-            <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
-          </div>
-          <Tabs defaultValue="overview" className="space-y-4">
-            {isAdmin && (
-              <TabsList>
-                <TabsTrigger value="overview">Your Scrims</TabsTrigger>
-                <TabsTrigger value="admin">Admin View</TabsTrigger>
-              </TabsList>
-            )}
-            <TabsContent value="overview" className="space-y-4">
-              <ScrimList searchParams={searchParams} />
-            </TabsContent>
-            <TabsContent value="admin" className="space-y-4">
-              <AdminScrimView />
-            </TabsContent>
-          </Tabs>
-        </div>
+        <Tabs defaultValue="overview" className="space-y-4">
+          {isAdmin && (
+            <TabsList>
+              <TabsTrigger value="overview">Your Scrims</TabsTrigger>
+              <TabsTrigger value="admin">Admin View</TabsTrigger>
+            </TabsList>
+          )}
+          <TabsContent value="overview" className="space-y-4">
+            <ScrimList searchParams={searchParams} />
+          </TabsContent>
+          <TabsContent value="admin" className="space-y-4">
+            <AdminScrimView />
+          </TabsContent>
+        </Tabs>
       </div>
     </>
   );
