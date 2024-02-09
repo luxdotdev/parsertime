6557c8ce38d48c88d68f96a3e66f3606eabd2d2f
Lucas Doell <lmd248@cornell.edu>
2024-02-09 17:21:03 -0500

    Add Footer component and set minimum height for dashboard layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,7 @@
 import { MainNav } from "@/components/dashboard/main-nav";
 import { Search } from "@/components/dashboard/search";
 import { TeamSwitcher } from "@/components/dashboard/team-switcher";
+import Footer from "@/components/footer";
 import { ModeToggle } from "@/components/theme-switcher";
 import { UserNav } from "@/components/user-nav";
 import { auth } from "@/lib/auth";
@@ -13,19 +14,22 @@ export default async function DashboardLayout({
   const session = await auth();
 
   return (
-    <div className="hidden flex-col md:flex">
-      <div className="border-b">
-        <div className="flex h-16 items-center px-4">
-          <TeamSwitcher session={session} />
-          <MainNav className="mx-6" />
-          <div className="ml-auto flex items-center space-x-4">
-            <Search />
-            <ModeToggle />
-            <UserNav />
+    <>
+      <div className="hidden flex-col md:flex min-h-[90vh]">
+        <div className="border-b">
+          <div className="flex h-16 items-center px-4">
+            <TeamSwitcher session={session} />
+            <MainNav className="mx-6" />
+            <div className="ml-auto flex items-center space-x-4">
+              <Search />
+              <ModeToggle />
+              <UserNav />
+            </div>
           </div>
         </div>
+        {children}
       </div>
-      {children}
-    </div>
+      <Footer />
+    </>
   );
 }
