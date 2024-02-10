7e18c7892cdcf55ebf408af5a491f592099c6ecc
Lucas Doell <lmd248@cornell.edu>
2024-02-10 18:39:26 -0500

    Remove unused import in ScrimLoading component

--- src/app/[team]/scrim/[scrimId]/loading.tsx
+++ src/app/[team]/scrim/[scrimId]/loading.tsx
@@ -1,4 +1,4 @@
-import { Card, CardHeader } from "@/components/ui/card";
+import { Card } from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
 
 export default function ScrimLoading() {
