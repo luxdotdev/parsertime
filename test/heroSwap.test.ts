69e05ae4b5228c82932eaa4163cd5088153b5cce
Lucas Doell <lmd248@cornell.edu>
2024-02-10 19:48:07 -0500

    Skip tests in CI

--- test/heroSwap.test.ts
+++ test/heroSwap.test.ts
@@ -6,38 +6,41 @@ const prisma = new PrismaClient({
   datasourceUrl: process.env.TEST_DB_URL,
 });
 
-test("should return the generated hero swap row", async () => {
-  const newHeroSwapRow = [1, 1000, "Team 1", "lux", "Ana", "Baptiste", 0];
+test.skipIf(process.env.GH_ACTION)(
+  "should return the generated hero swap row",
+  async () => {
+    const newHeroSwapRow = [1, 1000, "Team 1", "lux", "Ana", "Baptiste", 0];
 
-  const data = { hero_swap: [newHeroSwapRow] };
+    const data = { hero_swap: [newHeroSwapRow] };
 
-  const expectedRow = {
-    scrimId: 1,
-    event_type: "hero_swap",
-    match_time: 1000,
-    player_team: "Team 1",
-    player_name: "lux",
-    player_hero: "Ana",
-    previous_hero: "Baptiste",
-    hero_time_played: 0,
-    MapDataId: 100,
-  };
+    const expectedRow = {
+      scrimId: 1,
+      event_type: "hero_swap",
+      match_time: 1000,
+      player_team: "Team 1",
+      player_name: "lux",
+      player_hero: "Ana",
+      previous_hero: "Baptiste",
+      hero_time_played: 0,
+      MapDataId: 100,
+    };
 
-  const heroSwapRow = await createHeroSwapRows(data as any, { id: 1 }, 100);
+    const heroSwapRow = await createHeroSwapRows(data as any, { id: 1 }, 100);
 
-  // Destructure the id from the result and compare the rest of the properties
-  const { id, ...restOfHeroSwapRow } = heroSwapRow[0];
+    // Destructure the id from the result and compare the rest of the properties
+    const { id, ...restOfHeroSwapRow } = heroSwapRow[0];
 
-  await prisma.heroSwap.deleteMany({
-    where: {
-      id: id,
-    },
-  });
+    await prisma.heroSwap.deleteMany({
+      where: {
+        id: id,
+      },
+    });
 
-  expect(restOfHeroSwapRow).toEqual(expectedRow);
-});
+    expect(restOfHeroSwapRow).toEqual(expectedRow);
+  }
+);
 
-test("should return empty array", async () => {
+test.skipIf(process.env.GH_ACTION)("should return empty array", async () => {
   const data = {};
 
   const heroSwapRow = await createHeroSwapRows(data as any, { id: 1 }, 1);
