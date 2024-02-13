ccd62cffd4b355b5ba07bf87335572537a4f770d
Lucas Doell <lmd248@cornell.edu>
2024-02-12 19:30:26 -0500

    Add DangerZone component for deleting a scrim

---
+++ src/components/scrim/danger-zone.tsx
@@ -0,0 +1,113 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { toast } from "@/components/ui/use-toast";
+import { Scrim } from "@prisma/client";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export function DangerZone({ scrim }: { scrim: Scrim }) {
+  const [deleteModalOpen, setDeleteModalOpen] = useState(false);
+  const [deleteInput, setDeleteInput] = useState("");
+  const [deleteEnabled, setDeleteEnabled] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    setDeleteEnabled(deleteInput === scrim.name);
+  }, [deleteInput, scrim.name]);
+
+  async function handleDelete() {
+    setLoading(true);
+    const res = await fetch(`/api/remove-scrim?id=${scrim.id}`, {
+      method: "POST",
+    });
+
+    if (res.ok) {
+      toast({
+        title: "Scrim deleted",
+        description: "The scrim has been deleted.",
+        duration: 5000,
+      });
+      router.push("/dashboard");
+    } else {
+      toast({
+        title: "Error",
+        description: `An error occurred: ${res.statusText} (${res.status})`,
+        duration: 5000,
+        variant: "destructive",
+      });
+    }
+
+    setLoading(false);
+  }
+
+  // fix LastPass hydration issue
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) return null;
+
+  return (
+    <Card className="max-w-lg border-red-500 dark:border-red-700">
+      <CardTitle className="py-2 pl-6 text-red-500 dark:text-red-700">
+        Danger Zone
+      </CardTitle>
+      <CardContent>
+        <h3 className="text-lg font-semibold">Delete Scrim</h3>
+        <p className="pb-2">
+          Once you delete a scrim, there is no going back. Please be certain.
+        </p>
+        <Dialog open={deleteModalOpen} onOpenChange={setDeleteModalOpen}>
+          <DialogTrigger>
+            <Button variant="destructive">Delete Scrim</Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <h2 className="text-lg font-semibold">Delete Scrim</h2>
+            </DialogHeader>
+            <p>
+              Are you sure you want to delete the scrim{" "}
+              <strong>{scrim.name}</strong>? This action cannot be undone.
+            </p>
+            <p>
+              Type <strong>{scrim.name}</strong> to confirm.
+            </p>
+            <Input
+              type="text"
+              className="border border-solid rounded p-2 w-full mt-4"
+              onChange={(e) => setDeleteInput(e.target.value)}
+            />
+            <div className="flex justify-end space-x-4 mt-4">
+              <Button
+                variant="destructive"
+                type="submit"
+                disabled={!deleteEnabled || loading}
+                onClick={handleDelete}
+              >
+                Delete
+              </Button>
+              <Button
+                variant="secondary"
+                onClick={() => setDeleteModalOpen(false)}
+              >
+                Cancel
+              </Button>
+            </div>
+          </DialogContent>
+        </Dialog>
+      </CardContent>
+    </Card>
+  );
+}
