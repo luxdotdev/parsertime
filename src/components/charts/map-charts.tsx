72e735f1b7feed9f636fac99f67685b64a34adba
Lucas Doell <lmd248@cornell.edu>
2024-02-13 22:24:25 -0500

    Add Kills By Role Chart to MapCharts component

--- src/components/charts/map-charts.tsx
+++ src/components/charts/map-charts.tsx
@@ -1,7 +1,14 @@
 import { KillsByFightChart } from "@/components/charts/kills-by-fight-chart";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { KillsByRoleChart } from "@/components/charts/kills-by-role-chart";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import prisma from "@/lib/prisma";
-import { Kill } from "@prisma/client";
+import { Kill, PlayerStat } from "@prisma/client";
 
 async function groupKillsByInterval(id: number, maxInterval: number) {
   // Fetch the data
@@ -54,9 +61,23 @@ export async function MapCharts({ id }: { id: number }) {
 
   const team1Name = teams?.team_1_name ?? "Team 1";
   const team2Name = teams?.team_2_name ?? "Team 2";
+  const teamNames = [team1Name, team2Name] as const;
 
   const fights = await groupKillsByInterval(id, 15);
-  const teamNames = [team1Name, team2Name] as const;
+
+  const team1Kills = await prisma.kill.findMany({
+    where: {
+      MapDataId: id,
+      attacker_team: team1Name,
+    },
+  });
+
+  const team2Kills = await prisma.kill.findMany({
+    where: {
+      MapDataId: id,
+      attacker_team: team2Name,
+    },
+  });
 
   return (
     <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
@@ -66,6 +87,18 @@ export async function MapCharts({ id }: { id: number }) {
         </CardHeader>
         <CardContent className="pl-2">
           <KillsByFightChart fights={fights} teamNames={teamNames} />
+        </CardContent>
+      </Card>
+      <Card className="col-span-full">
+        <CardHeader>
+          <CardTitle>Kills By Role Chart</CardTitle>
+        </CardHeader>
+        <CardContent className="pl-2">
+          <KillsByRoleChart
+            team1Kills={team1Kills}
+            team2Kills={team2Kills}
+            teamNames={teamNames}
+          />
         </CardContent>
       </Card>
     </div>
