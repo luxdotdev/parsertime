264917ef85c5cd2bc9f66af469d1ad6d6d2d64c3
Lucas Doell <lmd248@cornell.edu>
2024-02-14 13:25:23 -0500

    Update eslint and add Prettify utility type

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,2 +1,7 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 export type TODO = any;
+
+export type Prettify<T> = {
+  [K in keyof T]: T[K];
+} & unknown;
