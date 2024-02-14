6602b85e40762d45d590c016fd86a5b9e37cec29
Lucas Doell <lmd248@cornell.edu>
2024-02-13 23:32:18 -0500

    Add objectiveCapture.test.ts for objective capture functionality

---
+++ test/objectiveCapture.test.ts
@@ -0,0 +1,69 @@
+import { expect, test } from "vitest";
+import { PrismaClient } from "@prisma/client";
+import { createObjectiveCapturedRows } from "../src/lib/parser";
+import { ObjectiveCapturedTableRow, ParserData } from "@/types/parser";
+
+const prisma = new PrismaClient({
+  datasourceUrl: process.env.TEST_DB_URL,
+});
+
+test("should return the generated objective capture row", async () => {
+  const newObjectiveCapturedRow: ObjectiveCapturedTableRow = [
+    "objective_captured",
+    100,
+    1,
+    "Team 1",
+    1,
+    0.01,
+    0,
+    0,
+  ];
+
+  const data: Pick<ParserData, "objective_capture"> = {
+    objective_capture: [newObjectiveCapturedRow],
+  };
+
+  const expectedRow = {
+    scrimId: 1,
+    event_type: "objective_capture",
+    match_time: 100,
+    round_number: 1,
+    capturing_team: "Team 1",
+    objective_index: 1,
+    control_team_1_progress: 0.01,
+    control_team_2_progress: 0,
+    match_time_remaining: 0,
+    MapDataId: 100,
+  };
+
+  const objCapturedRow = await createObjectiveCapturedRows(
+    data as any,
+    { id: 1 },
+    100
+  );
+
+  // Destructure the id from the result and compare the rest of the properties
+  const { id, ...restOfObjCapturedRow } = objCapturedRow[0];
+
+  await prisma.objectiveCaptured.deleteMany({
+    where: {
+      id: id,
+    },
+  });
+
+  expect(restOfObjCapturedRow).toEqual(expectedRow);
+});
+
+test("should return empty array", async () => {
+  const data = {};
+
+  const heroSwapRow = await createObjectiveCapturedRows(
+    data as any,
+    { id: 1 },
+    1
+  );
+
+  expect(heroSwapRow).toEqual([]);
+});
+
+prisma.$disconnect();
