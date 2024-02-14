d7aa973fd11b39435287ba59228951bd2599e60f
Lucas Doell <lmd248@cornell.edu>
2024-02-13 22:28:40 -0500

    Add margin to bar chart

--- src/components/charts/kills-by-role-chart.tsx
+++ src/components/charts/kills-by-role-chart.tsx
@@ -68,7 +68,17 @@ export function KillsByRoleChart({ team1Kills, team2Kills, teamNames }: Props) {
   ];
 
   return (
-    <BarChart width={1000} height={500} data={data}>
+    <BarChart
+      width={1000}
+      height={500}
+      data={data}
+      margin={{
+        top: 20,
+        right: 30,
+        left: 20,
+        bottom: 5,
+      }}
+    >
       <CartesianGrid strokeDasharray="3 3" />
       <XAxis dataKey="name" />
       <YAxis />
