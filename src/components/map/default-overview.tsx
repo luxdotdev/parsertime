a43ac842a77617c7952125295b2278148f72a64f
Lucas Doell <lmd248@cornell.edu>
2024-02-14 00:26:09 -0500

    Update DefaultOverview component to calculate score and winner

--- src/components/map/default-overview.tsx
+++ src/components/map/default-overview.tsx
@@ -1,9 +1,16 @@
 import { OverviewTable } from "@/components/map/overview-table";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { removeDuplicateRows } from "@/lib/utils";
 import { PlayerStatRows } from "@/types/prisma";
 import prisma from "@/lib/prisma";
 import CardIcon from "@/components/ui/card-icon";
+import { $Enums } from "@prisma/client";
 
 export async function DefaultOverview({ id }: { id: number }) {
   const finalRound = await prisma.roundEnd.findFirst({
@@ -14,6 +21,28 @@ export async function DefaultOverview({ id }: { id: number }) {
       round_number: "desc",
     },
   });
+
+  const matchDetails = await prisma.matchStart.findFirst({
+    where: {
+      MapDataId: id,
+    },
+  });
+
+  const team1Captures = await prisma.objectiveCaptured.findMany({
+    where: {
+      MapDataId: id,
+      capturing_team: matchDetails?.team_1_name ?? "Team 1",
+    },
+  });
+
+  const team2Captures = await prisma.objectiveCaptured.findMany({
+    where: {
+      MapDataId: id,
+      capturing_team: matchDetails?.team_2_name ?? "Team 2",
+    },
+  });
+
+  const mapType = matchDetails ? matchDetails.map_type : $Enums.MapType.Control;
 
   /**
    * This query performs the following operations:
@@ -50,6 +79,77 @@ export async function DefaultOverview({ id }: { id: number }) {
     // sort by team name
     .sort((a, b) => a.player_team.localeCompare(b.player_team));
 
+  function calculateScore() {
+    switch (mapType) {
+      case $Enums.MapType.Control:
+        return `${finalRound?.team_1_score} - ${finalRound?.team_2_score}`;
+      case $Enums.MapType.Escort:
+        // account for game setting score to 3 to ensure map completion
+        return `${team1Captures.length} - ${team2Captures.length}`;
+      case $Enums.MapType.Flashpoint:
+        return `${finalRound?.team_1_score} - ${finalRound?.team_2_score}`;
+      case $Enums.MapType.Hybrid:
+        // account for game setting score to 3 to ensure map completion
+        return `${team1Captures.length} - ${team2Captures.length}`;
+      case $Enums.MapType.Push:
+        return "Not supported for Push";
+      default:
+        return "N/A";
+    }
+  }
+
+  function calculateWinner() {
+    if (!matchDetails) return "N/A";
+    if (!finalRound) return "N/A";
+
+    switch (mapType) {
+      case $Enums.MapType.Control:
+        return finalRound.team_1_score > finalRound.team_2_score
+          ? matchDetails.team_1_name
+          : matchDetails.team_2_name;
+
+      case $Enums.MapType.Escort:
+        if (team1Captures.length === 0) return matchDetails.team_2_name;
+        if (team2Captures.length === 0) return matchDetails.team_1_name;
+
+        if (team1Captures.length === team2Captures.length) {
+          return team1Captures[team1Captures.length - 1]?.match_time_remaining >
+            team2Captures[team2Captures.length - 1]?.match_time_remaining
+            ? matchDetails.team_1_name
+            : matchDetails.team_2_name;
+        }
+
+        return team1Captures.length > team2Captures.length
+          ? matchDetails.team_1_name
+          : matchDetails.team_2_name;
+
+      case $Enums.MapType.Flashpoint:
+        return finalRound.team_1_score > finalRound.team_2_score
+          ? matchDetails.team_1_name
+          : matchDetails.team_2_name;
+
+      case $Enums.MapType.Hybrid:
+        if (!team1Captures) return matchDetails.team_2_name;
+        if (!team2Captures) return matchDetails.team_1_name;
+
+        if (team1Captures.length === team2Captures.length) {
+          return team1Captures[team1Captures.length - 1]?.match_time_remaining >
+            team2Captures[team2Captures.length - 1]?.match_time_remaining
+            ? matchDetails.team_1_name
+            : matchDetails.team_2_name;
+        }
+
+        return team1Captures.length > team2Captures.length
+          ? matchDetails.team_1_name
+          : matchDetails.team_2_name;
+
+      case $Enums.MapType.Push:
+        return "Not supported for Push";
+      default:
+        return "N/A";
+    }
+  }
+
   return (
     <>
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
@@ -71,19 +171,26 @@ export async function DefaultOverview({ id }: { id: number }) {
         </Card>
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Subscriptions</CardTitle>
+            <CardTitle className="text-sm font-medium">Score</CardTitle>
             <CardIcon>
-              <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-              <circle cx="9" cy="7" r="4" />
-              <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
+              <path d="M6 9H4.5a2.5 2.5 0 0 1 0-5H6" />
+              <path d="M18 9h1.5a2.5 2.5 0 0 0 0-5H18" />
+              <path d="M4 22h16" />
+              <path d="M10 14.66V17c0 .55-.47.98-.97 1.21C7.85 18.75 7 20.24 7 22" />
+              <path d="M14 14.66V17c0 .55.47.98.97 1.21C16.15 18.75 17 20.24 17 22" />
+              <path d="M18 2H6v7a6 6 0 0 0 12 0V2Z" />
             </CardIcon>
           </CardHeader>
           <CardContent>
-            <div className="text-2xl font-bold">+2350</div>
-            <p className="text-xs text-muted-foreground">
-              +180.1% from last month
-            </p>
+            <div className="text-2xl font-bold">{calculateScore()}</div>
           </CardContent>
+          <CardFooter>
+            <p className="text-xs text-muted-foreground">
+              {mapType !== $Enums.MapType.Push
+                ? `Winner: ${calculateWinner()}`
+                : "Not supported for Push due to limitations with the scrim code. :("}
+            </p>
+          </CardFooter>
         </Card>
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
