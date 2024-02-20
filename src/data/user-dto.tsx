9e39c1a420a027ea6e489a090dd18574426c5a0c
Lucas Doell <lmd248@cornell.edu>
2024-02-19 23:49:45 -0500

    Refactor getUser function to handle null or undefined email

--- src/data/user-dto.tsx
+++ src/data/user-dto.tsx
@@ -2,7 +2,11 @@ import "server-only";
 import prisma from "@/lib/prisma";
 import { cache } from "react";
 
-export const getUser = cache(async (email: string) => {
+export const getUser = cache(async (email: string | null | undefined) => {
+  if (!email) {
+    return null;
+  }
+
   return await prisma.user.findFirst({
     where: {
       email: email,
