fda2ea7c3b2bb98df8771b5c3f8adf7b2398cadf
Lucas Doell <lmd248@cornell.edu>
2024-02-26 03:29:32 -0500

    Refactor getUser function and add documentation

--- src/data/user-dto.tsx
+++ src/data/user-dto.tsx
@@ -1,15 +1,32 @@
 import "server-only";
 import prisma from "@/lib/prisma";
 import { cache } from "react";
+import { User } from "@prisma/client";
 
-export const getUser = cache(async (email: string | null | undefined) => {
+async function getUserFn(email: string | null | undefined) {
   if (!email) {
     return null;
   }
 
-  return await prisma.user.findFirst({
+  const user = await prisma.user.findFirst({
     where: {
       email: email,
     },
   });
-});
+
+  if (!user) {
+    return null;
+  }
+
+  return user;
+}
+
+/**
+ * Get a user from the database by email address.
+ * This function is cached for performance.
+ *
+ * @param {string} email - The email address of the user to get.
+ * @returns {User} The user with the specified email address or null if no user was found.
+ * @see {@link User}
+ */
+export const getUser = cache(getUserFn);
