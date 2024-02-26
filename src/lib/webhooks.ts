d68f991df36772839560ee281c58e490dd586891
Lucas Doell <lmd248@cornell.edu>
2024-02-26 04:28:46 -0500

    Add Discord webhook functionality for sending new user notifications

---
+++ src/lib/webhooks.ts
@@ -0,0 +1,72 @@
+import Logger from "@/lib/logger";
+import { User } from "next-auth";
+
+/**
+ * The structure of a Discord webhook. This is a simplified version of the actual
+ * structure, which can be found at https://discord.com/developers/docs/resources/webhook#execute-webhook.
+ * This structure only includes the fields that are used in this application.
+ */
+type DiscordWebhook = {
+  username?: string;
+  avatar_url?: string;
+  embeds: {
+    title?: string;
+    description?: string;
+    url?: string;
+    timestamp?: Date;
+    color?: number;
+    thumbnail?: {
+      url?: string;
+    };
+  }[];
+};
+
+/**
+ * Construct a new user webhook.
+ *
+ * @param {User} user - The user that was created.
+ * @returns {DiscordWebhook} The constructed webhook.
+ */
+export function newUserWebhookConstructor(user: User): DiscordWebhook {
+  return {
+    username: "Parsertime",
+    avatar_url: "https://parsertime.app/icon.png",
+    embeds: [
+      {
+        title: "New User",
+        description: `New user registered: ${user.name} (${user.email})`,
+        timestamp: new Date(),
+        color: 0x0ea5e9,
+        thumbnail: {
+          url: user.image ?? `https://avatar.vercel.sh/${user.email}.png`,
+        },
+      },
+    ],
+  };
+}
+
+/**
+ * Send a message to a Discord webhook.
+ *
+ * @param {string} url - The URL of the webhook to send the message to.
+ * @param {DiscordWebhook} data - The message to send.
+ */
+export async function sendDiscordWebhook(url: string, data: DiscordWebhook) {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  if (!response.ok) {
+    Logger.error("Failed to send Discord webhook", {
+      status: response.status,
+      statusText: response.statusText,
+      url,
+      data,
+    });
+    throw new Error("Failed to send Discord webhook");
+  }
+}
