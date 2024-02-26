38f85b13e8b02c0b481c9feef32d6ce1711d435a
Lucas Doell <lmd248@cornell.edu>
2024-02-25 19:50:59 -0500

    Add new environment variables

--- src/types/app.d.ts
+++ src/types/app.d.ts
@@ -4,6 +4,9 @@ const envVariables = z.object({
   DATABASE_URL: z.string(),
   TEST_DB_URL: z.string(),
   BLOB_READ_WRITE_TOKEN: z.string(),
+  EDGE_CONFIG: z.string(),
+  UPSTASH_REDIS_REST_URL: z.string(),
+  UPSTASH_REDIS_REST_TOKEN: z.string(),
   GITHUB_CLIENT_ID: z.string(),
   GITHUB_CLIENT_SECRET: z.string(),
   DISCORD_CLIENT_ID: z.string(),
@@ -11,6 +14,8 @@ const envVariables = z.object({
   GOOGLE_CLIENT_ID: z.string(),
   GOOGLE_CLIENT_SECRET: z.string(),
   SENDGRID_API_KEY: z.string(),
+  NEXTAUTH_SECRET: z.string(),
+  NEXTAUTH_URL: z.string(),
   DEV_TOKEN: z.string(),
 });
 
