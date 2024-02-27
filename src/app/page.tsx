f7f27af10435d29b036a0455543920f5b740a8ad
Lucas Doell <lmd248@cornell.edu>
2024-02-27 00:24:36 -0500

    Refactor home page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
-import HeroComponent from "@/components/home/hero";
+import ClosedBetaBanner from "@/components/home/banner";
+import LandingPage from "@/components/home/landing-page";
+import { Availability } from "@/lib/auth";
+import { get } from "@vercel/edge-config";
 
 export default async function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black">
-      <HeroComponent />
+  const appAvailability = (await get("availability")) as Availability;
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left"></div>
-    </main>
+  const isPrivate = appAvailability === "private";
+
+  return (
+    <>
+      {isPrivate && <ClosedBetaBanner />}
+      <LandingPage />
+    </>
   );
 }
