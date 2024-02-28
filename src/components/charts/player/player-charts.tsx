018d3e94e4921c26afa21c587162aff35925addf
Lucas Doell <lmd248@cornell.edu>
2024-02-28 03:25:44 -0500

    Refactor player charts to use generic sumStatByRound function

--- src/components/charts/player/player-charts.tsx
+++ src/components/charts/player/player-charts.tsx
@@ -7,88 +7,15 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { getPlayerFinalStats } from "@/data/scrim-dto";
+import { sumStatByRound } from "@/lib/player-charts";
 import prisma from "@/lib/prisma";
+import { HeroName, heroRoleMapping } from "@/types/heroes";
 
 type Props = {
   id: number;
   playerName: string;
 };
-
-type PlayerDamage = {
-  round_number: number;
-  player_hero: string;
-  hero_damage_dealt: number;
-};
-
-type PlayerHealing = {
-  round_number: number;
-  player_hero: string;
-  healing_dealt: number;
-};
-
-function sumDamageByRound(playerDamages: PlayerDamage[]) {
-  const sumByRound = new Map<number, number>();
-  const uniqueEntries = new Set<string>();
-
-  // Accumulate damage by round, ensuring no double counting for the same player-hero in the same round
-  playerDamages.forEach(({ round_number, player_hero, hero_damage_dealt }) => {
-    const key = `${round_number}-${player_hero}`;
-    if (!uniqueEntries.has(key)) {
-      uniqueEntries.add(key);
-      const currentSum = sumByRound.get(round_number) || 0;
-      sumByRound.set(round_number, currentSum + hero_damage_dealt);
-    }
-  });
-
-  // Prepare the final output, subtracting the damage of the previous round from the current one
-  const result = [] as { round_number: number; hero_damage_dealt: number }[];
-  let previousRoundDamage = 0; // Track damage from the previous round
-  Array.from(sumByRound.keys())
-    .sort((a, b) => a - b) // Ensure rounds are in ascending order
-    .forEach((round_number) => {
-      const currentRoundDamage = sumByRound.get(round_number)!; // Non-null assertion since we know it exists
-      const damageDifference = currentRoundDamage - previousRoundDamage;
-      result.push({
-        round_number,
-        hero_damage_dealt: damageDifference,
-      });
-      previousRoundDamage = currentRoundDamage; // Update the previous round damage for the next iteration
-    });
-
-  return result;
-}
-
-function sumHealingByRound(playerHealings: PlayerHealing[]) {
-  const sumByRound = new Map<number, number>();
-  const uniqueEntries = new Set<string>();
-
-  // Accumulate healing by round, ensuring no double counting for the same player-hero in the same round
-  playerHealings.forEach(({ round_number, player_hero, healing_dealt }) => {
-    const key = `${round_number}-${player_hero}`;
-    if (!uniqueEntries.has(key)) {
-      uniqueEntries.add(key);
-      const currentSum = sumByRound.get(round_number) || 0;
-      sumByRound.set(round_number, currentSum + healing_dealt);
-    }
-  });
-
-  // Prepare the final output, subtracting the healing of the previous round from the current one
-  const result = [] as { round_number: number; healing_dealt: number }[];
-  let previousRoundHealing = 0; // Track healing from the previous round
-  Array.from(sumByRound.keys())
-    .sort((a, b) => a - b) // Ensure rounds are in ascending order
-    .forEach((round_number) => {
-      const currentRoundHealing = sumByRound.get(round_number)!; // Non-null assertion since we know it exists
-      const healingDifference = currentRoundHealing - previousRoundHealing;
-      result.push({
-        round_number,
-        healing_dealt: healingDifference,
-      });
-      previousRoundHealing = currentRoundHealing; // Update the previous round healing for the next iteration
-    });
-
-  return result;
-}
 
 export async function PlayerCharts({ id, playerName }: Props) {
   const teams = await prisma.matchStart.findFirst({
@@ -115,7 +42,10 @@ export async function PlayerCharts({ id, playerName }: Props) {
     },
   });
 
-  const dmgSumByRound = sumDamageByRound(playerDamageByRound);
+  const dmgSumByRound = sumStatByRound(
+    playerDamageByRound,
+    "hero_damage_dealt"
+  );
 
   const playerHealingByRound = await prisma.playerStat.findMany({
     where: {
@@ -129,7 +59,10 @@ export async function PlayerCharts({ id, playerName }: Props) {
     },
   });
 
-  const healingSumByRound = sumHealingByRound(playerHealingByRound);
+  const healingSumByRound = sumStatByRound(
+    playerHealingByRound,
+    "healing_dealt"
+  );
 
   const playerTeamName = await prisma.playerStat.findFirst({
     where: {
@@ -143,6 +76,16 @@ export async function PlayerCharts({ id, playerName }: Props) {
 
   const playerTeam =
     playerTeamName?.player_team === team1Name ? "Team1" : "Team2";
+
+  const finalStats = await getPlayerFinalStats(id, playerName);
+
+  const mostPlayedHero = finalStats.filter(
+    (stat) =>
+      stat.hero_time_played ===
+      Math.max(...finalStats.map((stat) => stat.hero_time_played))
+  )[0].player_hero as HeroName;
+
+  const playerRole = heroRoleMapping[mostPlayedHero];
 
   return (
     <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-6">
@@ -159,34 +102,35 @@ export async function PlayerCharts({ id, playerName }: Props) {
         </CardContent>
         <CardFooter>
           <p className="text-sm text-gray-500">
-            This chart shows the hero damage done by round for the selected
-            player. The x-axis represents the round, and the y-axis represents
-            the damage done. Note that the damage is NOT cumulative, so the
-            damage done in round 2 does not include the damage done in round 1.
+            This chart shows the hero damage done by round for {playerName}. The
+            x-axis represents the round, and the y-axis represents the damage
+            done. Note that the damage is NOT cumulative, so the damage done in
+            round 2 does not include the damage done in round 1.
           </p>
         </CardFooter>
       </Card>
-      <Card className="col-span-3">
-        <CardHeader>
-          <CardTitle>Healing By Round Chart</CardTitle>
-        </CardHeader>
-        <CardContent className="pl-2">
-          <HealingByRoundChart
-            playerHealingByRound={healingSumByRound}
-            playerName={playerName}
-            playerTeam={playerTeam}
-          />
-        </CardContent>
-        <CardFooter>
-          <p className="text-sm text-gray-500">
-            This chart shows the healing done by round for the selected player.
-            The x-axis represents the round, and the y-axis represents the
-            healing done. Note that the healing is NOT cumulative, so the
-            healing done in round 2 does not include the healing done in round
-            1.
-          </p>
-        </CardFooter>
-      </Card>
+      {playerRole === "Support" && (
+        <Card className="col-span-3">
+          <CardHeader>
+            <CardTitle>Healing By Round Chart</CardTitle>
+          </CardHeader>
+          <CardContent className="pl-2">
+            <HealingByRoundChart
+              playerHealingByRound={healingSumByRound}
+              playerName={playerName}
+              playerTeam={playerTeam}
+            />
+          </CardContent>
+          <CardFooter>
+            <p className="text-sm text-gray-500">
+              This chart shows the healing done by round for {playerName}. The
+              x-axis represents the round, and the y-axis represents the healing
+              done. Note that the healing is NOT cumulative, so the healing done
+              in round 2 does not include the healing done in round 1.
+            </p>
+          </CardFooter>
+        </Card>
+      )}
     </div>
   );
 }
