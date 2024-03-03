296ce46e72bcce3e388f06d539965f4d15088598
Lucas Doell <lmd248@cornell.edu>
2024-03-02 22:07:08 -0500

    Refactor envVariables to be more descriptive

--- src/types/app.d.ts
+++ src/types/app.d.ts
@@ -1,25 +1,36 @@
 import { z } from "zod";
 
 const envVariables = z.object({
+  // Database connection strings
   DATABASE_URL: z.string(),
   TEST_DB_URL: z.string(),
+
+  // Vercel storage secrets
   BLOB_READ_WRITE_TOKEN: z.string(),
   EDGE_CONFIG: z.string(),
-  UPSTASH_REDIS_REST_URL: z.string(),
-  UPSTASH_REDIS_REST_TOKEN: z.string(),
+  KV_REST_API_URL: z.string(),
+  KV_REST_API_TOKEN: z.string(),
+  KV_REST_API_READ_ONLY_TOKEN: z.string(),
+  KV_URL: z.string(),
+
+  // Auth secrets
   GITHUB_CLIENT_ID: z.string(),
   GITHUB_CLIENT_SECRET: z.string(),
   DISCORD_CLIENT_ID: z.string(),
   DISCORD_CLIENT_SECRET: z.string(),
   GOOGLE_CLIENT_ID: z.string(),
   GOOGLE_CLIENT_SECRET: z.string(),
+
+  // Stripe keys
+  STRIPE_PUBLISHABLE_KEY: z.string(),
+  STRIPE_SECRET_KEY: z.string(),
+
+  // Misc
   SENDGRID_API_KEY: z.string(),
   NEXTAUTH_SECRET: z.string(),
   NEXTAUTH_URL: z.string(),
   DEV_TOKEN: z.string(),
   DISCORD_WEBHOOK_URL: z.string(),
-  STRIPE_PUBLISHABLE_KEY: z.string(),
-  STRIPE_SECRET_KEY: z.string(),
 });
 
 envVariables.parse(process.env);
