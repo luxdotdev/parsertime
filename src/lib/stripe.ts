ff09485f97f0b62b1af5abab7b2743c2bba9cfc9
Lucas Doell <lmd248@cornell.edu>
2024-03-04 03:14:11 -0500

    Update Stripe import in stripe.ts

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,7 +1,7 @@
-import { Stripe as StripeType, loadStripe } from "@stripe/stripe-js";
+import { Stripe as TStripe, loadStripe } from "@stripe/stripe-js";
 import Stripe from "stripe";
 
-let stripePromise: Promise<StripeType | null>;
+let stripePromise: Promise<TStripe | null>;
 export const getStripe = () => {
   if (!stripePromise) {
     stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
