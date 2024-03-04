b5587237f691b1035381bfba0c96ae7b817ff479
Lucas Doell <lmd248@cornell.edu>
2024-03-03 23:42:10 -0500

    Update Stripe integration

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,11 +1,24 @@
-import { Stripe, loadStripe } from "@stripe/stripe-js";
+import { Stripe as StripeType, loadStripe } from "@stripe/stripe-js";
+import Stripe from "stripe";
 
-let stripePromise: Promise<Stripe | null>;
-const getStripe = () => {
+let stripePromise: Promise<StripeType | null>;
+export const getStripe = () => {
   if (!stripePromise) {
     stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
   }
   return stripePromise;
 };
 
-export default getStripe;
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  // https://github.com/stripe/stripe-node#configuration
+  // https://stripe.com/docs/api/versioning
+  // @ts-expect-error - Use latest Stripe API version
+  apiVersion: null,
+  // Register this as an official Stripe plugin.
+  // https://stripe.com/docs/building-plugins#setappinfo
+  appInfo: {
+    name: "Next.js Subscription Starter",
+    version: "0.0.0",
+    url: "https://github.com/vercel/nextjs-subscription-payments",
+  },
+});
