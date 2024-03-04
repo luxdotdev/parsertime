accdd46dc362492bb34076ff980c6d86ddce20fc
Lucas Doell <lmd248@cornell.edu>
2024-03-04 01:12:47 -0500

    Remove unused code in stripe.ts

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -14,11 +14,4 @@ export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   // https://stripe.com/docs/api/versioning
   // @ts-expect-error - Use latest Stripe API version
   apiVersion: null,
-  // Register this as an official Stripe plugin.
-  // https://stripe.com/docs/building-plugins#setappinfo
-  appInfo: {
-    name: "Next.js Subscription Starter",
-    version: "0.0.0",
-    url: "https://github.com/vercel/nextjs-subscription-payments",
-  },
 });
