c8098daab856c198a0cf7bea221695f84b8daf9a
Lucas Doell <lmd248@cornell.edu>
2024-03-05 12:05:56 -0500

    Use property shorthand syntax

--- src/app/team/[teamId]/page.tsx
+++ src/app/team/[teamId]/page.tsx
@@ -70,9 +70,7 @@ export default async function Team({ params }: { params: { teamId: string } }) {
   })) ?? { users: [] };
 
   const teamManagers = await prisma.teamManager.findMany({
-    where: {
-      teamId: teamId,
-    },
+    where: { teamId },
   });
 
   const user = await getUser(session?.user?.email);
@@ -88,72 +86,70 @@ export default async function Team({ params }: { params: { teamId: string } }) {
     user?.role === $Enums.UserRole.ADMIN;
 
   return (
-    <>
-      <div className="flex-1 space-y-4 p-8 pt-6">
-        <div className="flex items-center justify-between space-y-2">
-          <h2 className="text-3xl font-bold tracking-tight">
-            {teamData?.name ?? "Team Name"}
-          </h2>
-        </div>
-
-        <Tabs defaultValue="members" className="space-y-4">
-          {hasPerms && (
-            <TabsList>
-              <TabsTrigger value="members">Members</TabsTrigger>
-              <TabsTrigger value="settings">Settings</TabsTrigger>
-            </TabsList>
-          )}
-          <TabsContent value="members" className="space-y-4">
-            <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
-              Members
-            </h3>
-
-            {teamMembers?.users.length > 0 && (
-              <div className="flex flex-wrap -m-2">
-                {teamMembers?.users.map((user) => (
-                  <div key={user.id} className="p-2 w-1/3">
-                    <Card className="max-w-md relative min-h-[144px]">
-                      <Image
-                        src={
-                          user.image ??
-                          `https://avatar.vercel.sh/${user.email}.png`
-                        }
-                        alt={
-                          user.name
-                            ? `Profile picture of ${user.name}`
-                            : "Default user avatar"
-                        }
-                        width={100}
-                        height={100}
-                        className="rounded-full float-right p-4"
-                      />
-                      <CardHeader className="flex">
-                        <div>
-                          <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
-                            {user.name} {userIsManager(user) && "(Manager)"}{" "}
-                            {user.id === teamData?.ownerId && "(Owner)"}{" "}
-                            {user.name === session?.user?.name && "(You)"}
-                          </h4>
-                        </div>
-                      </CardHeader>
-                      <CardContent>
-                        <p>{user.email}</p>
-                      </CardContent>
-                      {hasPerms && user.email !== session?.user?.email && (
-                        <UserCardButtons user={user} managers={teamManagers} />
-                      )}
-                    </Card>
-                  </div>
-                ))}
-                {hasPerms && <AddMemberCard />}
-              </div>
-            )}
-          </TabsContent>
-          <TabsContent value="settings" className="space-y-4">
-            <TeamSettingsForm team={teamData!} />
-          </TabsContent>
-        </Tabs>
+    <div className="flex-1 space-y-4 p-8 pt-6">
+      <div className="flex items-center justify-between space-y-2">
+        <h2 className="text-3xl font-bold tracking-tight">
+          {teamData?.name ?? "Team Name"}
+        </h2>
       </div>
-    </>
+
+      <Tabs defaultValue="members" className="space-y-4">
+        {hasPerms && (
+          <TabsList>
+            <TabsTrigger value="members">Members</TabsTrigger>
+            <TabsTrigger value="settings">Settings</TabsTrigger>
+          </TabsList>
+        )}
+        <TabsContent value="members" className="space-y-4">
+          <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+            Members
+          </h3>
+
+          {teamMembers?.users.length > 0 && (
+            <div className="flex flex-wrap -m-2">
+              {teamMembers?.users.map((user) => (
+                <div key={user.id} className="p-2 w-1/3">
+                  <Card className="max-w-md relative min-h-[144px]">
+                    <Image
+                      src={
+                        user.image ??
+                        `https://avatar.vercel.sh/${user.email}.png`
+                      }
+                      alt={
+                        user.name
+                          ? `Profile picture of ${user.name}`
+                          : "Default user avatar"
+                      }
+                      width={100}
+                      height={100}
+                      className="rounded-full float-right p-4"
+                    />
+                    <CardHeader className="flex">
+                      <div>
+                        <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+                          {user.name} {userIsManager(user) && "(Manager)"}{" "}
+                          {user.id === teamData?.ownerId && "(Owner)"}{" "}
+                          {user.name === session?.user?.name && "(You)"}
+                        </h4>
+                      </div>
+                    </CardHeader>
+                    <CardContent>
+                      <p>{user.email}</p>
+                    </CardContent>
+                    {hasPerms && user.email !== session?.user?.email && (
+                      <UserCardButtons user={user} managers={teamManagers} />
+                    )}
+                  </Card>
+                </div>
+              ))}
+              {hasPerms && <AddMemberCard />}
+            </div>
+          )}
+        </TabsContent>
+        <TabsContent value="settings" className="space-y-4">
+          <TeamSettingsForm team={teamData!} />
+        </TabsContent>
+      </Tabs>
+    </div>
   );
 }
