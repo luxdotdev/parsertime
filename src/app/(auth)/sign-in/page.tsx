3761c14bc2b7c9bd859f93ce69a9c2f137a2b34e
Lucas Doell <lmd248@cornell.edu>
2024-03-05 11:52:07 -0500

    Change template literal to string and remove unused fragment

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -36,47 +36,45 @@ export default async function AuthenticationPage() {
   }
 
   return (
-    <>
-      <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        <Link
-          href="/sign-up"
-          className={cn(
-            buttonVariants({ variant: "ghost" }),
-            "absolute right-4 top-4 md:right-8 md:top-8"
-          )}
-        >
-          Sign Up
+    <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <Link
+        href="/sign-up"
+        className={cn(
+          buttonVariants({ variant: "ghost" }),
+          "absolute right-4 top-4 md:right-8 md:top-8"
+        )}
+      >
+        Sign Up
+      </Link>
+      <div className="relative hidden h-full flex-col bg-muted p-10 text-black dark:text-white dark:border-r lg:flex">
+        <div className="absolute inset-0 bg-topography dark:bg-dark-topography" />
+        <Link href="/">
+          <div className="relative z-20 flex items-center text-lg font-medium">
+            <Image
+              src="/parsertime.png"
+              alt=""
+              width={50}
+              height={50}
+              className="dark:invert"
+            />
+            Parsertime
+          </div>
         </Link>
-        <div className="relative hidden h-full flex-col bg-muted p-10 text-black dark:text-white dark:border-r lg:flex">
-          <div className="absolute inset-0 bg-topography dark:bg-dark-topography" />
-          <Link href="/">
-            <div className="relative z-20 flex items-center text-lg font-medium">
-              <Image
-                src={`/parsertime.png`}
-                alt=""
-                width={50}
-                height={50}
-                className="dark:invert"
-              />
-              Parsertime
-            </div>
-          </Link>
-          <div className="relative z-20 mt-auto">
-            <p className="space-y-2 text-lg">Made with ❤️ by lux.dev</p>
-          </div>
-        </div>
-        <div className="lg:p-8">
-          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-            <div className="flex flex-col space-y-2 text-center">
-              <h1 className="text-2xl font-semibold tracking-tight">Sign In</h1>
-              <p className="text-sm text-muted-foreground">
-                Enter your email below to sign in
-              </p>
-            </div>
-            <UserAuthForm />
-          </div>
+        <div className="relative z-20 mt-auto">
+          <p className="space-y-2 text-lg">Made with ❤️ by lux.dev</p>
         </div>
       </div>
-    </>
+      <div className="lg:p-8">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div className="flex flex-col space-y-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">Sign In</h1>
+            <p className="text-sm text-muted-foreground">
+              Enter your email below to sign in
+            </p>
+          </div>
+          <UserAuthForm />
+        </div>
+      </div>
+    </div>
   );
 }
