ff4a397dd569c425901315d2ac99fea6dbe31ead
Lucas Doell <lmd248@cornell.edu>
2024-03-05 04:03:08 -0500

    Add testId and testPriceId to BillingPlans

--- src/types/billing-plans.ts
+++ src/types/billing-plans.ts
@@ -2,8 +2,10 @@ import { $Enums } from "@prisma/client";
 
 export type BillingPlans = {
   id: string;
+  testId: string;
   name: $Enums.BillingPlan;
   price: number;
   priceId: string;
+  testPriceId: string;
   teamSize: number;
 }[];
