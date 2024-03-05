b1b32328200091cbee5c07d5eb99d829f0eda60e
Lucas Doell <lmd248@cornell.edu>
2024-03-05 02:57:54 -0500

    Add billing portal link and display current plan

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -3,6 +3,10 @@ import { ProfileForm } from "@/components/settings/profile-form";
 import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
 import prisma from "@/lib/prisma";
+import { toTitleCase } from "@/lib/utils";
+import Link from "next/link";
+import { getCustomerPortalUrl } from "@/lib/stripe";
+import { ExternalLinkIcon } from "@radix-ui/react-icons";
 
 export default async function SettingsProfilePage() {
   const session = await auth();
@@ -20,6 +24,8 @@ export default async function SettingsProfilePage() {
     redirect("/sign-up");
   }
 
+  const billingPortalUrl = await getCustomerPortalUrl(user);
+
   return (
     <div className="space-y-6">
       <div>
@@ -29,6 +35,20 @@ export default async function SettingsProfilePage() {
         </p>
       </div>
       <Separator />
+      <p>Your current plan is the {toTitleCase(user.billingPlan)} plan.</p>
+      <Link
+        href={user.billingPlan === "FREE" ? "/pricing" : billingPortalUrl}
+        className="text-sky-500 underline"
+      >
+        {user.billingPlan === "FREE" ? (
+          "Upgrade your plan"
+        ) : (
+          <span className="inline-flex items-center">
+            Manage your subscription{" "}
+            <ExternalLinkIcon className="w-4 h-4 ml-1" />
+          </span>
+        )}
+      </Link>
       <ProfileForm user={user} />
     </div>
   );
