2ea19dc19211da10b4e824717bb1547a0063320e
Lucas Doell <lmd248@cornell.edu>
2024-03-08 12:58:43 -0500

    Add Discord settings page

---
+++ src/app/settings/discord/page.tsx
@@ -0,0 +1,47 @@
+import { DiscordSettingsForm } from "@/components/settings/discord-form";
+import { Separator } from "@/components/ui/separator";
+import { getUser } from "@/data/user-dto";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import prisma from "@/lib/prisma";
+import { DiscordLoginButton } from "@/components/settings/discord-login-button";
+
+export default async function DiscordSettingsPage() {
+  const session = await auth();
+  if (!session || !session.user) {
+    redirect("/sign-in");
+  }
+
+  const user = await getUser(session.user.email);
+
+  if (!user) {
+    redirect("/sign-up");
+  }
+
+  const accounts = await prisma.account.findMany({
+    where: {
+      userId: user.id,
+    },
+  });
+
+  const discordAccount = accounts.find(
+    (account) => account.provider === "discord"
+  );
+
+  return (
+    <div className="space-y-6">
+      <div>
+        <h3 className="text-lg font-medium">Discord Settings</h3>
+        <p className="text-sm text-muted-foreground">
+          Manage linked account settings and preferences.
+        </p>
+      </div>
+      <Separator />
+      {discordAccount ? (
+        <DiscordSettingsForm user={user} />
+      ) : (
+        <DiscordLoginButton />
+      )}
+    </div>
+  );
+}
