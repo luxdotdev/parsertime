ff5114e953803bdeb940a738a0b9b28069ea16f7
Lucas Doell <lmd248@cornell.edu>
2024-03-09 16:43:29 -0500

    Add rate limiting and error logging to contact form email route

--- src/app/api/(email)/send-contact-form-email/route.ts
+++ src/app/api/(email)/send-contact-form-email/route.ts
@@ -1,6 +1,9 @@
 import ContactFormEmail from "@/components/email/contact-form";
+import Logger from "@/lib/logger";
 import { render } from "@react-email/render";
 import sendgrid from "@sendgrid/mail";
+import { Ratelimit } from "@upstash/ratelimit";
+import { kv } from "@vercel/kv";
 import { NextRequest } from "next/server";
 
 type ContactFormEmailBody = {
@@ -12,6 +15,24 @@ type ContactFormEmailBody = {
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
 export async function POST(req: NextRequest) {
+  // Create a new ratelimiter, that allows 5 requests per 1 minute
+  const ratelimit = new Ratelimit({
+    redis: kv,
+    limiter: Ratelimit.slidingWindow(3, "1 m"),
+    analytics: true,
+  });
+
+  // Limit the requests to 5 per minute per user
+  const identifier = req.ip ?? "127.0.0.1";
+  const { success } = await ratelimit.limit(identifier);
+
+  if (!success) {
+    Logger.log("Rate limit exceeded for contact form email", identifier);
+    return new Response("Rate limit exceeded", {
+      status: 429,
+    });
+  }
+
   const body = (await req.json()) as ContactFormEmailBody;
 
   const emailHtml = render(
@@ -30,7 +51,7 @@ export async function POST(req: NextRequest) {
       html: emailHtml,
     });
   } catch (error) {
-    console.log(error);
+    Logger.error("Error sending email", error);
 
     return new Response("Error sending email", {
       status: 500,
