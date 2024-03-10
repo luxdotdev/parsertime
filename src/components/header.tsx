24c5583ed1e4ca80fbb81421f2db699a6b0f238c
Lucas Doell <lmd248@cornell.edu>
2024-03-09 19:41:39 -0500

    Refactor header component styles

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -19,25 +19,27 @@ export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <header className="bg-white dark:bg-black">
+    <header className="absolute inset-x-0 top-0 z-50">
       <nav
         className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
         aria-label="Global"
       >
-        <Link href="/" className="-m-1.5 p-1.5">
-          <span className="sr-only">Parsertime</span>
-          <Image
-            className="h-10 w-auto dark:invert"
-            src="/parsertime.png"
-            alt=""
-            width={50}
-            height={50}
-          />
-        </Link>
+        <div className="flex lg:flex-1">
+          <Link href="/" className="-m-1.5 p-1.5">
+            <span className="sr-only">Parsertime</span>
+            <Image
+              className="h-12 w-auto dark:invert"
+              height={48}
+              width={48}
+              src="/parsertime.png"
+              alt="Parsertime Logo"
+            />
+          </Link>
+        </div>
         <div className="flex lg:hidden">
           <button
             type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700 dark:text-gray-200"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-600 dark:text-gray-400"
             onClick={() => setMobileMenuOpen(true)}
           >
             <span className="sr-only">Open main menu</span>
@@ -49,12 +51,13 @@ export default function Header() {
             <Link
               key={item.name}
               href={item.href}
-              target={item.href.startsWith("https://") ? "_blank" : undefined}
               className="text-sm font-semibold leading-6 text-gray-900 dark:text-white"
             >
               {item.name}
             </Link>
           ))}
+        </div>
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
           <Link
             href="/sign-in"
             className="text-sm font-semibold leading-6 text-gray-900 dark:text-white"
@@ -69,22 +72,22 @@ export default function Header() {
         open={mobileMenuOpen}
         onClose={setMobileMenuOpen}
       >
-        <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white dark:bg-black px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10 dark:ring-gray-700">
+        <div className="fixed inset-0 z-50" />
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white dark:bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10 dark:sm:ring-white/10">
           <div className="flex items-center justify-between">
             <Link href="/" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
+              <span className="sr-only">Parsertime</span>
               <Image
-                className="h-8 w-auto dark:invert"
+                className="h-12 w-auto dark:invert"
+                height={48}
+                width={48}
                 src="/parsertime.png"
-                alt=""
-                width={32}
-                height={32}
+                alt="Parsertime Logo"
               />
             </Link>
             <button
               type="button"
-              className="-m-2.5 rounded-md p-2.5 text-gray-700 dark:text-gray-200"
+              className="-m-2.5 rounded-md p-2.5 text-gray-600 dark:text-gray-400"
               onClick={() => setMobileMenuOpen(false)}
             >
               <span className="sr-only">Close menu</span>
@@ -92,16 +95,13 @@ export default function Header() {
             </button>
           </div>
           <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10 dark:divide-gray-200">
+            <div className="-my-6 divide-y divide-gray-500/25">
               <div className="space-y-2 py-6">
                 {navigation.map((item) => (
                   <Link
                     key={item.name}
                     href={item.href}
-                    target={
-                      item.href.startsWith("https://") ? "_blank" : undefined
-                    }
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 dark:text-white hover:bg-gray-50 dark:hover:bg-black"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-200 dark:text-white dark:hover:bg-gray-800"
                   >
                     {item.name}
                   </Link>
@@ -110,7 +110,7 @@ export default function Header() {
               <div className="py-6">
                 <Link
                   href="/sign-in"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 dark:text-white hover:bg-gray-50 dark:hover:bg-black  "
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-200 dark:text-white dark:hover:bg-gray-800"
                 >
                   Log in
                 </Link>
