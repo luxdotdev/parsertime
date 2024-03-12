d67189b6600a5858ac08e348b8f238158c2b4b94
Lucas Doell <lucas@lux.dev>
2024-03-12 19:02:04 -0400

    Add authentication check to MapDashboardLayout component

--- src/app/[team]/scrim/[scrimId]/map/[mapId]/layout.tsx
+++ src/app/[team]/scrim/[scrimId]/map/[mapId]/layout.tsx
@@ -1,9 +1,22 @@
+import NoAuthCard from "@/components/auth/no-auth";
 import { SelectedPlayerProvider } from "@/components/map/player-switcher";
+import { isAuthedToViewMap } from "@/lib/auth";
 
-export default function MapDashboardLayout({
+export default async function MapDashboardLayout({
   children,
+  params,
 }: {
   children: React.ReactNode;
+  params: { team: string; scrimId: string; mapId: string };
 }) {
+  const scrimId = parseInt(params.scrimId);
+  const mapId = parseInt(params.mapId);
+
+  const isAuthed = await isAuthedToViewMap(scrimId, mapId);
+
+  if (!isAuthed) {
+    return <NoAuthCard />;
+  }
+
   return <SelectedPlayerProvider>{children}</SelectedPlayerProvider>;
 }
