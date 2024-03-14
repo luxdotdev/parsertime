1c0f886decb9a4be508edbeff9f137ecb3bea351
Lucas Doell <lucas@lux.dev>
2024-03-14 18:46:04 -0400

    Add ultimates used list to MapEvents component

--- src/components/map/map-events.tsx
+++ src/components/map/map-events.tsx
@@ -5,14 +5,15 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { getMapEvents } from "@/lib/get-map-events";
+import { getMapEvents, getUltimatesUsedList } from "@/lib/get-map-events";
 
 export async function MapEvents({ id }: { id: number }) {
   const events = await getMapEvents(id);
+  const ultimates = await getUltimatesUsedList(id);
 
   return (
-    <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
-      <Card className="col-span-full">
+    <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-6">
+      <Card className="col-span-3">
         <CardHeader>
           <CardTitle>Events</CardTitle>
           <CardDescription>
@@ -20,7 +21,21 @@ export async function MapEvents({ id }: { id: number }) {
             captures, rounds starting and ending, multikills, and more.
           </CardDescription>
         </CardHeader>
-        <CardContent className="pl-4">{events}</CardContent>
+        <CardContent className="pl-4 max-h-[150vh] overflow-y-auto">
+          {events}
+        </CardContent>
+      </Card>
+      <Card className="col-span-3">
+        <CardHeader>
+          <CardTitle>Ultimates Used</CardTitle>
+          <CardDescription>
+            A list of all ultimates used during the match and the times when
+            they occurred.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="pl-4 max-h-[150vh] overflow-y-auto">
+          {ultimates}
+        </CardContent>
       </Card>
     </div>
   );
