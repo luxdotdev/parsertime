3a3fa9a7aae60b032b2683edc8f4cc099d9d0de2
Lucas Doell <lucas@lux.dev>
2024-03-15 16:08:37 -0400

    Refactor search component to include command menu

--- src/components/dashboard/search.tsx
+++ src/components/dashboard/search.tsx
@@ -1,13 +1,31 @@
-import { Input } from "@/components/ui/input";
+"use client";
 
-export function Search() {
+import { CommandDialogMenu } from "@/components/command-menu";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { DialogProps } from "@radix-ui/react-alert-dialog";
+import { CommandMenuContext } from "@/components/command-menu-provider";
+import { use } from "react";
+
+export function Search({ ...props }: DialogProps) {
+  const { setOpen } = use(CommandMenuContext);
+
   return (
-    <div>
-      <Input
-        type="search"
-        placeholder="Search..."
-        className="md:w-[100px] lg:w-[300px]"
-      />
-    </div>
+    <>
+      <Button
+        variant="outline"
+        className={cn(
+          "relative h-9 w-full justify-start rounded-[0.5rem] bg-background text-sm font-normal text-muted-foreground shadow-none sm:pr-12 md:w-40 lg:w-64"
+        )}
+        onClick={() => setOpen(true)}
+        {...props}
+      >
+        <span className="inline-flex">Search...</span>
+        <kbd className="pointer-events-none absolute right-[0.3rem] top-[0.45rem] hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
+          <span className="text-xs">⌘</span>K
+        </kbd>
+      </Button>
+      <CommandDialogMenu />
+    </>
   );
 }
