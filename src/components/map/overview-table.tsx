cd9c625364df013ffb5b6553197381ef2144ad8d
Lucas Doell <lucas@lux.dev>
2024-03-15 16:57:41 -0400

    Remove pagination buttons from overview table

--- src/components/map/overview-table.tsx
+++ src/components/map/overview-table.tsx
@@ -331,26 +331,6 @@ export function OverviewTable({
           </TableBody>
         </Table>
       </div>
-      <div className="flex items-center justify-end space-x-2 py-4">
-        <div className="space-x-2">
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}
-          >
-            Previous
-          </Button>
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}
-          >
-            Next
-          </Button>
-        </div>
-      </div>
     </div>
   );
 }
