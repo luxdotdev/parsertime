5c7f8ff05dcd41673ae375f0a7260429604e2515
Lucas Doell <lucas@lux.dev>
2024-03-17 15:24:32 -0400

    Add duel winrates to player analytics

--- src/components/player/analytics.tsx
+++ src/components/player/analytics.tsx
@@ -9,9 +9,12 @@ import CardIcon from "@/components/ui/card-icon";
 import {
   getAverageTimeToUseUlt,
   getAverageUltChargeTime,
+  getDuelWinrates,
   getKillsPerUltimate,
 } from "@/lib/analytics";
-import { toTimestamp } from "@/lib/utils";
+import { cn, toHero, toTimestamp } from "@/lib/utils";
+import Image from "next/image";
+import prisma from "@/lib/prisma";
 
 export async function PlayerAnalytics({
   id,
@@ -23,6 +26,14 @@ export async function PlayerAnalytics({
   const averageTimeToUltimate = await getAverageUltChargeTime(id, playerName);
   const averageTimeToUseUlt = await getAverageTimeToUseUlt(id, playerName);
   const killsPerUltimate = await getKillsPerUltimate(id, playerName);
+
+  const duels = await getDuelWinrates(id, playerName);
+
+  const match = await prisma.matchStart.findFirst({
+    where: {
+      MapDataId: id,
+    },
+  });
 
   return (
     <main className="min-h-[65vh]">
@@ -97,6 +108,101 @@ export async function PlayerAnalytics({
               <span className="font-bold">final blows</span> a player gets with
               their ultimate. Note that for some heroes, the ultimate ability
               itself may not result in a kill.
+            </p>
+          </CardFooter>
+        </Card>
+        <Card></Card>
+        <Card className="col-span-2 2xl:col-span-1">
+          <CardHeader>
+            <CardTitle className="text-sm font-medium">
+              Versus Other Players
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            {duels.map((duel) => (
+              <div key={duel.enemy_name} className="pb-4">
+                <div
+                  key={duel.player_name}
+                  className="flex items-center space-x-2"
+                >
+                  <div className="flex items-center space-x-2">
+                    <Image
+                      src={`/heroes/${toHero(duel.player_hero)}.png`}
+                      alt=""
+                      width={256}
+                      height={256}
+                      className={cn(
+                        "h-12 w-12 border-2 rounded",
+                        duel.player_team === match?.team_1_name
+                          ? "border-blue-500"
+                          : "border-red-500"
+                      )}
+                    />
+                    <div className="text-lg font-medium">
+                      {duel.player_name}
+                    </div>
+                    <div className="text-lg font-medium">&rarr;</div>
+                    <Image
+                      src={`/heroes/${toHero(duel.enemy_hero)}.png`}
+                      alt=""
+                      width={256}
+                      height={256}
+                      className={cn(
+                        "h-12 w-12 border-2 rounded",
+                        duel.enemy_team === match?.team_1_name
+                          ? "border-blue-500"
+                          : "border-red-500"
+                      )}
+                    />
+                    <div className="text-lg font-medium">{duel.enemy_name}</div>
+                  </div>
+                </div>
+                <div className="text-lg align-middle">
+                  Score:{" "}
+                  <span
+                    className={cn(
+                      duel.player_team === match?.team_1_name
+                        ? "text-blue-500"
+                        : "text-red-500"
+                    )}
+                  >
+                    {duel.enemy_deaths}
+                  </span>{" "}
+                  -{" "}
+                  <span
+                    className={cn(
+                      duel.enemy_team === match?.team_1_name
+                        ? "text-blue-500"
+                        : "text-red-500"
+                    )}
+                  >
+                    {duel.enemy_kills}
+                  </span>
+                </div>
+                Winrate:{" "}
+                <span
+                  className={cn(
+                    duel.enemy_deaths > duel.enemy_kills
+                      ? "text-blue-500"
+                      : duel.enemy_deaths < duel.enemy_kills
+                      ? "text-red-500"
+                      : "text-purple-500"
+                  )}
+                >
+                  {(
+                    (duel.enemy_deaths /
+                      (duel.enemy_kills + duel.enemy_deaths)) *
+                    100
+                  ).toFixed(2)}
+                  %
+                </span>
+              </div>
+            ))}
+          </CardContent>
+          <CardFooter>
+            <p className="text-xs text-muted-foreground">
+              {playerName}&apos;s score and winrates against players on the
+              enemy team.
             </p>
           </CardFooter>
         </Card>
