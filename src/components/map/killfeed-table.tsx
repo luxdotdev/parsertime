4c066d1c7edde80b94b6345ccdcca69337f7b9b1
Lucas Doell <lucas@lux.dev>
2024-03-17 15:56:56 -0400

    Hide fights with zero kills in the killfeed table

--- src/components/map/killfeed-table.tsx
+++ src/components/map/killfeed-table.tsx
@@ -39,104 +39,108 @@ export function KillfeedTable({
   return (
     <>
       {fights.map((fight, i) => (
-        <Table key={fight.start}>
-          <TableCaption>Fight {i + 1}</TableCaption>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="w-20">Time</TableHead>
-              <TableHead className="w-80">Kill</TableHead>
-              <TableHead className="w-20">Method</TableHead>
-              <TableHead className="w-20">Start</TableHead>
-              <TableHead className="w-20">End</TableHead>
-              <TableHead className="w-20">Fight Winner</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {fight.kills.map((kill, j) => (
-              <TableRow key={kill.id}>
-                <TableCell>{kill.match_time}</TableCell>
-                <TableCell>
-                  <span className="flex items-center space-x-2">
-                    <div className="pr-2">
-                      <Image
-                        src={`/heroes/${toHero(kill.attacker_hero)}.png`}
-                        alt=""
-                        width={256}
-                        height={256}
-                        className={cn(
-                          "h-8 w-8 border-2 rounded",
-                          kill.attacker_team === team1
-                            ? "border-blue-500"
-                            : "border-red-500",
-                          kill.attacker_name === kill.victim_name
-                            ? "opacity-0"
-                            : ""
-                        )}
-                      />
-                    </div>
-                    <div
-                      className={cn(
-                        "w-32",
-                        kill.attacker_name === kill.victim_name
-                          ? "opacity-0"
-                          : ""
-                      )}
-                    >
-                      {kill.attacker_name}
-                    </div>
-                    <div className="pr-16">&rarr;</div>
-                    <div className="pr-2">
-                      <Image
-                        src={`/heroes/${toHero(kill.victim_hero)}.png`}
-                        alt=""
-                        width={256}
-                        height={256}
-                        className={cn(
-                          "h-8 w-8 border-2 rounded",
-                          kill.victim_team === team1
-                            ? "border-blue-500"
-                            : "border-red-500"
-                        )}
-                      />
-                    </div>
-                    <div className="w-32">{kill.victim_name}</div>
-                  </span>
-                </TableCell>
-                <TableCell>
-                  {kill.attacker_name === kill.victim_name
-                    ? kill.is_environmental
-                      ? environmentalString
-                      : kill.event_ability === "0"
-                      ? "Primary Fire"
-                      : kill.event_ability
-                    : kill.event_ability === "0"
-                    ? "Primary Fire"
-                    : kill.event_ability}
-                </TableCell>
-                {j === 0 ? (
-                  <>
-                    <TableCell>{toTimestamp(fight.start)}</TableCell>
-                    <TableCell>{toTimestamp(fight.end)}</TableCell>
+        <div key={fight.start}>
+          {fight.kills.length > 0 && (
+            <Table key={fight.start}>
+              <TableCaption>Fight {i + 1}</TableCaption>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-20">Time</TableHead>
+                  <TableHead className="w-80">Kill</TableHead>
+                  <TableHead className="w-20">Method</TableHead>
+                  <TableHead className="w-20">Start</TableHead>
+                  <TableHead className="w-20">End</TableHead>
+                  <TableHead className="w-20">Fight Winner</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {fight.kills.map((kill, j) => (
+                  <TableRow key={kill.id}>
+                    <TableCell>{kill.match_time}</TableCell>
                     <TableCell>
-                      {fight.kills.filter(
-                        (kill) => kill.attacker_team === team1
-                      ).length >
-                      fight.kills.length / 2
-                        ? team1
-                        : team2}
+                      <span className="flex items-center space-x-2">
+                        <div className="pr-2">
+                          <Image
+                            src={`/heroes/${toHero(kill.attacker_hero)}.png`}
+                            alt=""
+                            width={256}
+                            height={256}
+                            className={cn(
+                              "h-8 w-8 border-2 rounded",
+                              kill.attacker_team === team1
+                                ? "border-blue-500"
+                                : "border-red-500",
+                              kill.attacker_name === kill.victim_name
+                                ? "opacity-0"
+                                : ""
+                            )}
+                          />
+                        </div>
+                        <div
+                          className={cn(
+                            "w-32",
+                            kill.attacker_name === kill.victim_name
+                              ? "opacity-0"
+                              : ""
+                          )}
+                        >
+                          {kill.attacker_name}
+                        </div>
+                        <div className="pr-16">&rarr;</div>
+                        <div className="pr-2">
+                          <Image
+                            src={`/heroes/${toHero(kill.victim_hero)}.png`}
+                            alt=""
+                            width={256}
+                            height={256}
+                            className={cn(
+                              "h-8 w-8 border-2 rounded",
+                              kill.victim_team === team1
+                                ? "border-blue-500"
+                                : "border-red-500"
+                            )}
+                          />
+                        </div>
+                        <div className="w-32">{kill.victim_name}</div>
+                      </span>
                     </TableCell>
-                  </>
-                ) : (
-                  <>
-                    <TableCell></TableCell>
-                    <TableCell></TableCell>
-                    <TableCell></TableCell>
-                  </>
-                )}
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+                    <TableCell>
+                      {kill.attacker_name === kill.victim_name
+                        ? kill.is_environmental
+                          ? environmentalString
+                          : kill.event_ability === "0"
+                          ? "Primary Fire"
+                          : kill.event_ability
+                        : kill.event_ability === "0"
+                        ? "Primary Fire"
+                        : kill.event_ability}
+                    </TableCell>
+                    {j === 0 ? (
+                      <>
+                        <TableCell>{toTimestamp(fight.start)}</TableCell>
+                        <TableCell>{toTimestamp(fight.end)}</TableCell>
+                        <TableCell>
+                          {fight.kills.filter(
+                            (kill) => kill.attacker_team === team1
+                          ).length >
+                          fight.kills.length / 2
+                            ? team1
+                            : team2}
+                        </TableCell>
+                      </>
+                    ) : (
+                      <>
+                        <TableCell></TableCell>
+                        <TableCell></TableCell>
+                        <TableCell></TableCell>
+                      </>
+                    )}
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          )}
+        </div>
       ))}
     </>
   );
