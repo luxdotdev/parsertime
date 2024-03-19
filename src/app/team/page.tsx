989d5017aece54d40b8c5e5d6a8a44dfc0a282a9
Lucas Doell <lucas@lux.dev>
2024-03-19 18:41:59 -0400

    Fix hidden css in TeamPage component

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -50,7 +50,7 @@ export default async function TeamPage() {
     userData?.role === $Enums.UserRole.MANAGER;
 
   return (
-    <div className="hidden flex-col md:flex">
+    <div className="flex-col md:flex">
       <div className="flex-1 space-y-4 p-8 pt-6">
         <div className="flex items-center justify-between space-y-2">
           <h2 className="text-3xl font-bold tracking-tight">
