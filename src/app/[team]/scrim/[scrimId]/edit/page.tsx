16800977f73c497c2c6e9f9f594769b6643b3e4a
Lucas Doell <lucas@lux.dev>
2024-03-19 18:06:09 -0400

    Add mobile support to edit scrim page

--- src/app/[team]/scrim/[scrimId]/edit/page.tsx
+++ src/app/[team]/scrim/[scrimId]/edit/page.tsx
@@ -8,6 +8,8 @@ import { MainNav } from "@/components/dashboard/main-nav";
 import Link from "next/link";
 import { DangerZone } from "@/components/scrim/danger-zone";
 import { getScrim } from "@/data/scrim-dto";
+import { MobileNav } from "@/components/mobile-nav";
+import { auth } from "@/lib/auth";
 
 type Props = {
   params: { team: string; scrimId: string };
@@ -16,18 +18,26 @@ type Props = {
 
 export default async function EditScrimPage({ params }: Props) {
   const scrim = await getScrim(parseInt(params.scrimId));
+  const session = await auth();
 
   if (!scrim) {
     return <div>Scrim not found</div>;
   }
 
   return (
-    <div className="hidden flex-col md:flex min-h-[90vh]">
+    <div className="flex-col md:flex min-h-[90vh]">
       <div className="border-b">
-        <div className="flex h-16 items-center px-4">
+        <div className="hidden md:flex h-16 items-center px-4">
           <MainNav className="mx-6" />
           <div className="ml-auto flex items-center space-x-4">
             <Search />
+            <ModeToggle />
+            <UserNav />
+          </div>
+        </div>
+        <div className="flex md:hidden h-16 items-center px-4">
+          <MobileNav session={session} />
+          <div className="ml-auto flex items-center space-x-4">
             <ModeToggle />
             <UserNav />
           </div>
