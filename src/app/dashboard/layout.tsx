dc1d95da306f0a552539d94e0941bbbbd98b8b17
Lucas Doell <lucas@lux.dev>
2024-03-19 17:59:46 -0400

    Add mobile navigation to dashboard layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,6 +2,7 @@ import { MainNav } from "@/components/dashboard/main-nav";
 import { Search } from "@/components/dashboard/search";
 import { TeamSwitcher } from "@/components/dashboard/team-switcher";
 import Footer from "@/components/footer";
+import { MobileNav } from "@/components/mobile-nav";
 import { ModeToggle } from "@/components/theme-switcher";
 import { UserNav } from "@/components/user-nav";
 import { auth } from "@/lib/auth";
@@ -15,13 +16,20 @@ export default async function DashboardLayout({
 
   return (
     <>
-      <div className="hidden flex-col md:flex min-h-[90vh]">
+      <div className="flex-col md:flex min-h-[90vh]">
         <div className="border-b">
-          <div className="flex h-16 items-center px-4">
+          <div className="hidden md:flex h-16 items-center px-4">
             <TeamSwitcher session={session} />
             <MainNav className="mx-6" />
             <div className="ml-auto flex items-center space-x-4">
               <Search />
+              <ModeToggle />
+              <UserNav />
+            </div>
+          </div>
+          <div className="flex md:hidden h-16 items-center px-4">
+            <MobileNav session={session} />
+            <div className="ml-auto flex items-center space-x-4">
               <ModeToggle />
               <UserNav />
             </div>
