83f95ac59618a6529d752207dd2599dc6263e053
Lucas Doell <lucas@lux.dev>
2024-03-19 18:58:34 -0400

    Update auth pages for mobile

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -28,7 +28,19 @@ export const metadata: Metadata = {
 
 export default function AuthenticationPage() {
   return (
-    <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+    <div className="container relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <Link href="/" className="md:hidden">
+        <div className="relative z-20 right-4 top-2 md:right-8 md:top-8 flex items-center text-lg font-medium">
+          <Image
+            src="/parsertime.png"
+            alt=""
+            width={50}
+            height={50}
+            className="dark:invert"
+          />
+          Parsertime
+        </div>
+      </Link>
       <Link
         href="/sign-in"
         className={cn(
@@ -56,7 +68,7 @@ export default function AuthenticationPage() {
           <p className="space-y-2 text-lg">Made with ❤️ by lux.dev</p>
         </div>
       </div>
-      <div className="lg:p-8">
+      <div className="pt-20 lg:pt-8 lg:p-8">
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col space-y-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
