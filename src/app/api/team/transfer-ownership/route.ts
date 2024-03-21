09906ca9c7b32fa544dbff7d93b30b4553b14d87
Lucas Doell <lucas@lux.dev>
2024-03-21 00:20:35 -0400

    Add transfer ownership route for team API

---
+++ src/app/api/team/transfer-ownership/route.ts
@@ -0,0 +1,95 @@
+import { getUser } from "@/data/user-dto";
+import { auth } from "@/lib/auth";
+import Logger from "@/lib/logger";
+import prisma from "@/lib/prisma";
+import { $Enums } from "@prisma/client";
+import { NextRequest } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const params = req.nextUrl.searchParams;
+  const id = parseInt(params.get("id") ?? "");
+  const owner = params.get("owner");
+  const token = req.headers.get("Authorization");
+
+  const session = await auth();
+
+  if (!session) {
+    if (token !== process.env.DEV_TOKEN) {
+      Logger.warn("Unauthorized request to remove team: ", id);
+      return new Response("Unauthorized", {
+        status: 401,
+      });
+    }
+    Logger.log("Authorized removal of team with dev token");
+  }
+
+  const user = await getUser(session?.user?.email ?? "lucas@lux.dev");
+
+  if (!user) {
+    return new Response("User not found", {
+      status: 404,
+    });
+  }
+
+  if (!id) {
+    return new Response("Missing ID", {
+      status: 400,
+    });
+  }
+
+  if (!owner) {
+    return new Response("Missing owner", {
+      status: 400,
+    });
+  }
+
+  const team = await prisma.team.findFirst({ where: { id } });
+
+  if (!team) {
+    return new Response("Team not found", {
+      status: 404,
+    });
+  }
+
+  const hasPerms =
+    user.role === $Enums.UserRole.ADMIN || // Admins can transfer anything
+    user.role === $Enums.UserRole.MANAGER || // Managers can transfer anything
+    user.id === team.ownerId; // Creators can transfer their own teams
+
+  if (!hasPerms) {
+    return new Response("Unauthorized", {
+      status: 401,
+    });
+  }
+
+  const newOwner = await getUser(owner);
+
+  if (!newOwner) {
+    return new Response("New owner not found", {
+      status: 404,
+    });
+  }
+
+  await prisma.team.update({
+    where: { id },
+    data: {
+      ownerId: newOwner.id,
+    },
+  });
+
+  // add new owner to team if they aren't already a member
+  await prisma.team.update({
+    where: { id },
+    data: {
+      users: {
+        connect: {
+          id: newOwner.id,
+        },
+      },
+    },
+  });
+
+  return new Response("OK", {
+    status: 200,
+  });
+}
