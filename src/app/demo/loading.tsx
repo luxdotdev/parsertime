0fb5f25cc47ba03236be248707ca0a1c6e749123
Lucas Doell <lucas@lux.dev>
2024-03-23 18:47:23 -0400

    Improve demo loading states

--- src/app/demo/loading.tsx
+++ src/app/demo/loading.tsx
@@ -1,23 +1,137 @@
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import CardIcon from "@/components/ui/card-icon";
 import { Skeleton } from "@/components/ui/skeleton";
 
-export default function DashboardLoading() {
+export default function MapDashboardLoading() {
   return (
-    <div className="hidden flex-col md:flex">
+    <div className="flex-col md:flex">
       <div className="border-b">
         <div className="flex h-16 items-center px-4">
           <Skeleton className="w-24 h-6" />
           <div className="ml-auto flex items-center space-x-4">
-            <Skeleton className="w-24 h-6" />
+            <Skeleton className="hidden md:flex w-24 h-6" />
             <Skeleton className="w-6 h-6" />
-            <Skeleton className="w-24 h-6" />S
+            <Skeleton className="md:hidden w-6 h-6" />
+            <Skeleton className="hidden md:flex w-24 h-6" />
           </div>
         </div>
       </div>
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <div className="flex items-center justify-between space-y-2">
-          <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
+        <div>
+          <h4 className="text-gray-600 dark:text-gray-400">
+            <Skeleton className="w-48 h-6" />
+          </h4>
         </div>
-        <Skeleton className="w-full h-96" />
+        <div className="flex items-center justify-between space-y-2">
+          <h2 className="text-3xl font-bold tracking-tight">
+            <Skeleton className="w-40 h-10" />
+          </h2>
+        </div>
+        <div className="space-y-4">
+          <Skeleton className="w-96 h-8" />
+        </div>
+        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">
+                Total Match Time
+              </CardTitle>
+              <CardIcon>
+                <circle cx="12" cy="12" r="10" />
+                <polyline points="12 6 12 12 16 14" />
+              </CardIcon>
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">
+                <Skeleton className="w-24 h-8" />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Skeleton className="w-32 h-4" />
+            </CardFooter>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">Score</CardTitle>
+              <CardIcon>
+                <path d="M6 9H4.5a2.5 2.5 0 0 1 0-5H6" />
+                <path d="M18 9h1.5a2.5 2.5 0 0 0 0-5H18" />
+                <path d="M4 22h16" />
+                <path d="M10 14.66V17c0 .55-.47.98-.97 1.21C7.85 18.75 7 20.24 7 22" />
+                <path d="M14 14.66V17c0 .55.47.98.97 1.21C16.15 18.75 17 20.24 17 22" />
+                <path d="M18 2H6v7a6 6 0 0 0 12 0V2Z" />
+              </CardIcon>
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">
+                <Skeleton className="w-24 h-8" />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Skeleton className="w-32 h-4" />
+            </CardFooter>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">
+                Hero Damage Dealt
+              </CardTitle>
+              <CardIcon>
+                <path d="M8.5 14.5A2.5 2.5 0 0 0 11 12c0-1.38-.5-2-1-3-1.072-2.143-.224-4.054 2-6 .5 2.5 2 4.9 4 6.5 2 1.6 3 3.5 3 5.5a7 7 0 1 1-14 0c0-1.153.433-2.294 1-3a2.5 2.5 0 0 0 2.5 2.5z" />
+              </CardIcon>
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">
+                <Skeleton className="w-24 h-8" />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Skeleton className="w-32 h-4" />
+            </CardFooter>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">
+                Team Healing Dealt
+              </CardTitle>
+              <CardIcon>
+                <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z" />
+                <path d="M3.22 12H9.5l.5-1 2 4.5 2-7 1.5 3.5h5.27" />
+              </CardIcon>
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">
+                <Skeleton className="w-24 h-8" />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Skeleton className="w-32 h-4" />
+            </CardFooter>
+          </Card>
+        </div>
+        <div className="hidden md:grid gap-4 md:grid-cols-2 lg:grid-cols-7">
+          <Card className="col-span-full">
+            <CardHeader>
+              <CardTitle>Overview</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="space-y-4">
+                <Skeleton className="w-64 h-8" />
+                <div className="w-full">
+                  <div className="rounded-md border">
+                    <Skeleton className="w-full h-96" />
+                  </div>
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
       </div>
     </div>
   );
