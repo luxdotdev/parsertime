5e3e8293b020c53a2e42d700f0d2f1c5a3bf6c28
Lucas Doell <lucas@lux.dev>
2024-03-23 17:00:45 -0400

    Add support for permanent team join links

--- src/app/team/join/[token]/page.tsx
+++ src/app/team/join/[token]/page.tsx
@@ -16,61 +16,91 @@ export default async function TokenPage({
     redirect("/login");
   }
 
-  const teamInviteToken = await prisma.teamInviteToken.findUnique({
-    where: {
-      token,
-    },
-  });
+  try {
+    const teamCreatedAt = new Date(atob(token));
 
-  if (!teamInviteToken) {
-    Logger.error("Invalid or expired token provided to join team");
-    redirect("/team/join?error=invalid-token");
+    const team = await prisma.team.findFirst({
+      where: { createdAt: teamCreatedAt },
+    });
+
+    if (!team) {
+      Logger.error("Team not found for date token");
+      redirect("/team/join?error=invalid-token");
+    }
+
+    await prisma.team.update({
+      where: {
+        id: team.id,
+      },
+      data: {
+        users: {
+          connect: {
+            email: session?.user?.email ?? "",
+          },
+        },
+      },
+    });
+
+    Logger.log(`User now belongs to team: ${JSON.stringify(team)}`);
+  } catch (e) {
+    const teamInviteToken = await prisma.teamInviteToken.findUnique({
+      where: {
+        token,
+      },
+    });
+
+    if (!teamInviteToken) {
+      Logger.error("Invalid or expired token provided to join team");
+      redirect("/team/join?error=invalid-token");
+    }
+
+    await prisma.team.update({
+      where: {
+        id: teamInviteToken.teamId,
+      },
+      data: {
+        users: {
+          connect: {
+            email: session?.user?.email ?? "",
+          },
+        },
+      },
+    });
+
+    await prisma.teamInviteToken.delete({
+      where: {
+        token,
+      },
+    });
+
+    Logger.log(
+      `User ${session?.user?.email} joined team ${teamInviteToken.teamId}`
+    );
+
+    const teams = await prisma.team.findMany({
+      where: {
+        users: {
+          some: {
+            email: session?.user?.email,
+          },
+        },
+      },
+    });
+
+    const user = await getUser(session?.user?.email);
+
+    prisma.user.update({
+      where: {
+        id: user?.id,
+      },
+      data: {
+        teamId: teamInviteToken.teamId,
+      },
+    });
+
+    Logger.log(`User now belongs to team: ${JSON.stringify(teams)}`);
+    redirect("/team/join/success");
   }
 
-  await prisma.team.update({
-    where: {
-      id: teamInviteToken.teamId,
-    },
-    data: {
-      users: {
-        connect: {
-          email: session?.user?.email ?? "",
-        },
-      },
-    },
-  });
-
-  await prisma.teamInviteToken.delete({
-    where: {
-      token,
-    },
-  });
-
-  Logger.log(
-    `User ${session?.user?.email} joined team ${teamInviteToken.teamId}`
-  );
-
-  const teams = await prisma.team.findMany({
-    where: {
-      users: {
-        some: {
-          email: session?.user?.email,
-        },
-      },
-    },
-  });
-
-  const user = await getUser(session?.user?.email);
-
-  prisma.user.update({
-    where: {
-      id: user?.id,
-    },
-    data: {
-      teamId: teamInviteToken.teamId,
-    },
-  });
-
-  Logger.log(`User now belongs to team: ${JSON.stringify(teams)}`);
   redirect("/team/join/success");
 }
