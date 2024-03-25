ee0e5186210c99ec13c571690875b8271e652fe1
Lucas Doell <lucas@lux.dev>
2024-03-24 20:47:31 -0400

    Remove unnecessary "use client" directive

--- src/components/dashboard/empty-scrim-list.tsx
+++ src/components/dashboard/empty-scrim-list.tsx
@@ -1,29 +1,7 @@
-"use client";
-
 import { CreateScrimButton } from "@/components/dashboard/create-scrim";
 import { Card, CardDescription } from "@/components/ui/card";
-import { Skeleton } from "@/components/ui/skeleton";
-import { useEffect, useState } from "react";
 
 export function EmptyScrimList() {
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
-  if (!isClient)
-    return (
-      <Card className="border-dashed">
-        <div className="flex h-[36rem] flex-col items-center justify-center space-y-2">
-          <Skeleton className="h-8 w-28" />
-          <Skeleton className="h-5 w-96" />
-          <Skeleton className="h-8 w-64" />
-          <Skeleton className="h-8 w-24" />
-        </div>
-      </Card>
-    );
-
   return (
     <Card className="border-dashed">
       <CardDescription>
