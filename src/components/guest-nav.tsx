314c7c9b33577f9acb00df2d08eccb9f2a906a47
Lucas Doell <lucas@lux.dev>
2024-04-01 20:45:50 -0400

    Add GuestNav component

---
+++ src/components/guest-nav.tsx
@@ -0,0 +1,45 @@
+import { Avatar } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { PersonIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+export async function GuestNav({ guestMode }: { guestMode: boolean }) {
+  if (!guestMode) {
+    redirect("/sign-in");
+  }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="relative h-8 w-8 rounded-full">
+          <Avatar className="h-8 w-8">
+            <PersonIcon className="h-full w-full p-1" />
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel className="font-normal">
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium leading-none">Guest User</p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <Link href="/sign-in">
+            <DropdownMenuItem>Sign In</DropdownMenuItem>
+          </Link>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
