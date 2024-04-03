91e5ea1261f8632cd807915b7fe47b4d58b330ed
Lucas Doell <lucas@lux.dev>
2024-04-02 21:15:51 -0400

    Add player-dto.tsx file with getMostPlayedHeroes function

---
+++ src/data/player-dto.tsx
@@ -0,0 +1,23 @@
+import "server-only";
+import prisma from "@/lib/prisma";
+import { cache } from "react";
+
+async function getMostPlayedHeroesFn(id: number) {
+  return await prisma.playerStat.findMany({
+    where: {
+      MapDataId: id,
+    },
+    select: {
+      player_name: true,
+      player_team: true,
+      player_hero: true,
+      hero_time_played: true,
+    },
+    orderBy: {
+      hero_time_played: "desc",
+    },
+    distinct: ["player_name"],
+  });
+}
+
+export const getMostPlayedHeroes = cache(getMostPlayedHeroesFn);
