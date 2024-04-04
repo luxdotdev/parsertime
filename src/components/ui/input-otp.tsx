de708b0b04628617ad9380a4de88187e856ba59f
Lucas Doell <lucas@lux.dev>
2024-04-03 21:46:49 -0400

    Refactor input-otp component

--- src/components/ui/input-otp.tsx
+++ src/components/ui/input-otp.tsx
@@ -2,17 +2,21 @@
 
 import * as React from "react";
 import { DashIcon } from "@radix-ui/react-icons";
-import { OTPInput, SlotProps } from "input-otp";
+import { OTPInput, OTPInputContext } from "input-otp";
 
 import { cn } from "@/lib/utils";
 
 const InputOTP = React.forwardRef<
   React.ElementRef<typeof OTPInput>,
   React.ComponentPropsWithoutRef<typeof OTPInput>
->(({ className, ...props }, ref) => (
+>(({ className, containerClassName, ...props }, ref) => (
   <OTPInput
     ref={ref}
-    containerClassName={cn("flex items-center gap-2", className)}
+    containerClassName={cn(
+      "flex items-center gap-2 has-[:disabled]:opacity-50",
+      containerClassName
+    )}
+    className={cn("disabled:cursor-not-allowed", className)}
     {...props}
   />
 ));
@@ -28,8 +32,11 @@ InputOTPGroup.displayName = "InputOTPGroup";
 
 const InputOTPSlot = React.forwardRef<
   React.ElementRef<"div">,
-  SlotProps & React.ComponentPropsWithoutRef<"div">
->(({ char, hasFakeCaret, isActive, className, ...props }, ref) => {
+  React.ComponentPropsWithoutRef<"div"> & { index: number }
+>(({ index, className, ...props }, ref) => {
+  const inputOTPContext = React.useContext(OTPInputContext);
+  const { char, hasFakeCaret, isActive } = inputOTPContext.slots[index];
+
   return (
     <div
       ref={ref}
