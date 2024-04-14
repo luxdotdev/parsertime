f5256e1a516ee9872ea7843597b3ef96ad24769b
Lucas Doell <lucas@lux.dev>
2024-04-14 17:20:43 -0400

    Move data table back to overview-table.tsx
    - avoid adding components to components/ui if they are not shadcn components
    - we do not use this component throughout the app so a reusable component is actually not necessary

--- src/components/map/overview-table.tsx
+++ src/components/map/overview-table.tsx
@@ -6,6 +6,7 @@ import {
   Header,
   SortingState,
   VisibilityState,
+  flexRender,
   getCoreRowModel,
   getFilteredRowModel,
   getPaginationRowModel,
@@ -26,7 +27,14 @@ import {
 import { cn, toTimestamp } from "@/lib/utils";
 import { GeistMono } from "geist/font/mono";
 import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/20/solid";
-import { DataTable } from "../ui/data-table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 export const columns: ColumnDef<PlayerData>[] = [
   {
@@ -356,7 +364,50 @@ export function OverviewTable({
   return (
     <div className="w-full">
       <div className="rounded-md border">
-        <DataTable table={table} />
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <TableHead key={header.id}>
+                    {flexRender(
+                      header.column.columnDef.header,
+                      header.getContext()
+                    )}
+                  </TableHead>
+                ))}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={table.getAllColumns().length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
       </div>
     </div>
   );
