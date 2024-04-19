bee6f180d3e51c1b21dcec4c82a857ef0b71c8f8
Lucas Doell <lucas@lux.dev>
2024-04-19 00:19:49 -0400

    Add cached function to get user teams with permissions

--- src/data/user-dto.tsx
+++ src/data/user-dto.tsx
@@ -1,7 +1,7 @@
 import "server-only";
 import prisma from "@/lib/prisma";
 import { cache } from "react";
-import { User } from "@prisma/client";
+import { $Enums, User } from "@prisma/client";
 
 async function getUserFn(email: string | null | undefined) {
   if (!email) {
@@ -28,3 +28,46 @@ async function getUserFn(email: string | null | undefined) {
  * @see {@link User}
  */
 export const getUser = cache(getUserFn);
+
+async function getTeamsWithPermsFn(email: string | null | undefined) {
+  const user = await getUser(email);
+
+  const teams = await prisma.team.findMany({
+    where: {
+      OR: [
+        {
+          ownerId: user?.id,
+        },
+        {
+          users: {
+            some: {
+              id: user?.id,
+              role: {
+                in: [$Enums.UserRole.MANAGER, $Enums.UserRole.ADMIN],
+              },
+            },
+          },
+        },
+        {
+          managers: {
+            some: {
+              userId: user?.id,
+            },
+          },
+        },
+      ],
+    },
+  });
+
+  return teams;
+}
+
+/**
+ * Get all teams that the user has permission to modify.
+ * This function is cached for performance.
+ *
+ * @param {string} userId - The user ID of the user to get teams for.
+ * @returns {Team[]} The teams that the user has permission to view.
+ * @see {@link Team}
+ */
+export const getTeamsWithPerms = cache(getTeamsWithPermsFn);
