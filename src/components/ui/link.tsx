6ba5acf9891b1b8610aacfea9da85cc2c9674381
Lucas Doell <lucas@lux.dev>
2024-04-27 22:57:01 -0400

    Refactor Link component to handle external links properly

--- src/components/ui/link.tsx
+++ src/components/ui/link.tsx
@@ -1,5 +1,6 @@
 import { default as NextLink } from "next/link";
 import { ExternalLinkIcon } from "@radix-ui/react-icons";
+import { cn } from "@/lib/utils";
 
 type LinkProps = React.ComponentProps<typeof NextLink> & {
   external?: boolean;
@@ -8,8 +9,10 @@ type LinkProps = React.ComponentProps<typeof NextLink> & {
 export function Link(props: LinkProps) {
   return (
     <NextLink {...props} target={props.external ? "_blank" : undefined}>
-      <span className="underline">{props.children}</span>{" "}
-      <ExternalLinkIcon className="inline h-4 w-4" />
+      <span className={cn(props.className, props.external && "underline")}>
+        {props.children}
+      </span>{" "}
+      {props.external && <ExternalLinkIcon className="inline h-4 w-4" />}
     </NextLink>
   );
 }
