f07302637752d9ff2e305dcf5780c35aa9e65133
Lucas Doell <lucas@lux.dev>
2024-06-04 23:48:18 -0400

    Remove console.log statement in StatsPage

--- src/app/stats/page.tsx
+++ src/app/stats/page.tsx
@@ -145,8 +145,6 @@ export default async function StatsPage() {
       coincidence_count DESC
     LIMIT 3;`;
 
-  console.log(ajaxes);
-
   return (
     <div className="flex-1 space-y-4 p-8 pt-6">
       <div className="flex items-center justify-between space-y-2">
