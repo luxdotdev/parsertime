9d212b88f25b193da8f59602f0de93fb10708c72
Lucas Doell <lucas@lux.dev>
2024-06-07 02:25:37 -0400

    Update MapWinsChart tooltip to display win percentage

--- src/components/stats/player/charts/map-wins-chart.tsx
+++ src/components/stats/player/charts/map-wins-chart.tsx
@@ -75,6 +75,11 @@ function CustomTooltip({
   label,
 }: TooltipProps<ValueType, NameType>) {
   if (active && payload && payload.length) {
+    const percentage =
+      ((payload[0].value as number) /
+        ((payload[0].value as number) + (payload[1].value as number))) *
+      100;
+
     return (
       <div className="z-50 overflow-hidden rounded-md bg-primary px-3 py-1.5 text-xs text-primary-foreground animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2">
         <h3 className="text-base font-bold">{label}</h3>
@@ -85,6 +90,14 @@ function CustomTooltip({
         <p className="text-sm">
           Losses:{" "}
           <span className="text-red-500">{payload[1].value as number}</span>
+        </p>
+        <p className="text-sm">
+          Percentage:{" "}
+          <span
+            className={cn(percentage >= 50 ? "text-green-500" : "text-red-500")}
+          >
+            {percentage.toFixed(2)}%
+          </span>
         </p>
       </div>
     );
