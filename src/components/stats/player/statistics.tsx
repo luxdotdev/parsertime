a0240d9c6fd42c62490d21e24a500c2ea9eea810
Lucas Doell <lucas@lux.dev>
2024-06-07 03:25:16 -0400

    Update Statistics component to display top 3 most killed heroes

--- src/components/stats/player/statistics.tsx
+++ src/components/stats/player/statistics.tsx
@@ -86,7 +86,7 @@ export function Statistics({
   const [filteredWins, setFilteredWins] = useState<Winrate>([]);
   const [filteredDeaths, setFilteredDeaths] = useState<Kill[]>([]);
   const [selectedStat, setSelectedStat] =
-    useState<keyof Omit<Stat, NonMappableStat>>("healing_dealt");
+    useState<keyof Omit<Stat, NonMappableStat>>("eliminations");
 
   useEffect(() => {
     if (timeframe === "custom") {
@@ -331,6 +331,27 @@ export function Statistics({
     .slice(0, 3);
 
   const top3MostDiedToHeroesLength = top3MostDiedToHeroesArray.length;
+
+  const top3MostKilledHeroes = filteredKills
+    .map((kill) => kill.victim_hero)
+    .reduce(
+      (acc, hero) => {
+        if (!acc[hero]) {
+          acc[hero] = 0;
+        }
+
+        acc[hero]++;
+
+        return acc;
+      },
+      {} as Record<string, number>
+    );
+
+  const top3MostKilledHeroesArray = Object.entries(top3MostKilledHeroes)
+    .sort((a, b) => b[1] - a[1])
+    .slice(0, 3);
+
+  const top3MostKilledHeroesLength = top3MostKilledHeroesArray.length;
 
   return (
     <section className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
@@ -736,7 +757,114 @@ export function Statistics({
           </p>
         </CardFooter>
       </Card>
-      <Card className="col-span-1 md:col-span-full xl:col-span-3">
+      <Card className="col-span-1 md:col-span-2 xl:col-span-1">
+        <CardHeader>
+          <CardTitle>Heroes Eliminated Most</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Rank</TableHead>
+                <TableHead>Hero</TableHead>
+                <TableHead>Eliminations</TableHead>
+              </TableRow>
+            </TableHeader>
+            <tbody>
+              {top3MostKilledHeroesArray.map(([hero, elims], idx) => (
+                // eslint-disable-next-line react/no-array-index-key
+                <TableRow key={`${hero}-${elims}-${idx}`}>
+                  <TableCell>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      className={cn(
+                        "h-4 w-4",
+                        idx === 0
+                          ? "text-amber-400"
+                          : idx === 1
+                            ? "text-gray-400"
+                            : idx === 2
+                              ? "text-amber-900"
+                              : "text-muted-foreground"
+                      )}
+                    >
+                      <path d="M6 9H4.5a2.5 2.5 0 0 1 0-5H6" />
+                      <path d="M18 9h1.5a2.5 2.5 0 0 0 0-5H18" />
+                      <path d="M4 22h16" />
+                      <path d="M10 14.66V17c0 .55-.47.98-.97 1.21C7.85 18.75 7 20.24 7 22" />
+                      <path d="M14 14.66V17c0 .55.47.98.97 1.21C16.15 18.75 17 20.24 17 22" />
+                      <path d="M18 2H6v7a6 6 0 0 0 12 0V2Z" />
+                    </svg>
+                  </TableCell>
+                  <TableCell>
+                    <span className="flex items-center space-x-2">
+                      <div className="pr-2">
+                        <Image
+                          src={`/heroes/${toHero(hero)}.png`}
+                          alt=""
+                          width={256}
+                          height={256}
+                          className={cn(
+                            "h-8 w-8 rounded border-2",
+                            idx === 0
+                              ? "border-amber-400"
+                              : idx === 1
+                                ? "border-gray-400"
+                                : idx === 2
+                                  ? "border-amber-900"
+                                  : "border-muted-foreground"
+                          )}
+                        />{" "}
+                      </div>
+                      {hero}
+                    </span>
+                  </TableCell>
+                  <TableCell>{elims}</TableCell>
+                </TableRow>
+              ))}
+              {top3MostKilledHeroesLength < 3 &&
+                Array.from({ length: 3 - top3Length }).map((_, idx) => (
+                  // eslint-disable-next-line react/no-array-index-key
+                  <TableRow key={idx}>
+                    <TableCell>-</TableCell>
+                    <TableCell>No data</TableCell>
+                    <TableCell>No data</TableCell>
+                  </TableRow>
+                ))}
+            </tbody>
+          </Table>
+        </CardContent>
+        <CardFooter>
+          <p className="text-sm text-muted-foreground">
+            {timeframe !== "custom" && timeframe !== "all-time" ? (
+              <>
+                Stats collected from {scrims[timeframe].length} scrims in the
+                last {timeframe.replace("-", " ")}
+              </>
+            ) : (
+              <>
+                Stats collected from{" "}
+                {timeframe === "custom"
+                  ? customScrims.length
+                  : timeframe === "all-time" && scrims["all-time"].length}{" "}
+                scrims{" "}
+                {timeframe === "all-time"
+                  ? "in all time data"
+                  : date?.from && date?.to
+                    ? `between ${date.from.toLocaleDateString()} - ${date.to.toLocaleDateString()}`
+                    : "all time"}
+              </>
+            )}
+          </p>
+        </CardFooter>
+      </Card>
+      <Card className="col-span-1 md:col-span-2">
         <CardHeader>
           <Select
             value={selectedStat}
