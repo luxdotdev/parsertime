8a548023d77d1072bc3e68b62cf1d080fcdc881e
Lucas Doell <lucas@lux.dev>
2024-06-07 02:42:04 -0400

    Add heroes died to most and adjust layout

--- src/components/stats/player/statistics.tsx
+++ src/components/stats/player/statistics.tsx
@@ -59,6 +59,7 @@ export function Statistics({
   hero,
   kills,
   mapWinrates,
+  deaths,
 }: {
   timeframe: Timeframe;
   date: DateRange | undefined;
@@ -68,11 +69,13 @@ export function Statistics({
   hero: HeroName | "all";
   kills: Kill[];
   mapWinrates: Winrate;
+  deaths: Kill[];
 }) {
   const [customScrims, setCustomScrims] = useState<Scrim[]>([]);
   const [filteredStats, setFilteredStats] = useState<PlayerStatRows>([]);
   const [filteredKills, setFilteredKills] = useState<Kill[]>([]);
   const [filteredWins, setFilteredWins] = useState<Winrate>([]);
+  const [filteredDeaths, setFilteredDeaths] = useState<Kill[]>([]);
 
   useEffect(() => {
     if (timeframe === "custom") {
@@ -137,6 +140,7 @@ export function Statistics({
     if (hero !== "all") {
       setFilteredStats(stats.filter((stat) => stat.player_hero === hero));
       setFilteredKills(kills.filter((kill) => kill.attacker_hero === hero));
+      setFilteredDeaths(deaths.filter((death) => death.victim_hero === hero));
     }
     if (hero === "all") {
       setFilteredStats(
@@ -149,8 +153,13 @@ export function Statistics({
           scrims[timeframe].some((scrim) => scrim.id === kill.scrimId)
         )
       );
+      setFilteredDeaths(
+        deaths.filter((death) =>
+          scrims[timeframe].some((scrim) => scrim.id === death.scrimId)
+        )
+      );
     }
-  }, [hero, stats, timeframe, scrims, kills]);
+  }, [hero, stats, timeframe, scrims, kills, deaths]);
 
   useEffect(() => {
     setFilteredKills(
@@ -195,6 +204,54 @@ export function Statistics({
       })
     );
   }, [timeframe, kills, scrims, customScrims]);
+
+  useEffect(() => {
+    setFilteredDeaths(
+      deaths.filter((death) => {
+        if (timeframe === "one-week") {
+          return scrims["one-week"].some((scrim) => scrim.id === death.scrimId);
+        }
+
+        if (timeframe === "two-weeks") {
+          return scrims["two-weeks"].some(
+            (scrim) => scrim.id === death.scrimId
+          );
+        }
+
+        if (timeframe === "one-month") {
+          return scrims["one-month"].some(
+            (scrim) => scrim.id === death.scrimId
+          );
+        }
+
+        if (timeframe === "three-months") {
+          return scrims["three-months"].some(
+            (scrim) => scrim.id === death.scrimId
+          );
+        }
+
+        if (timeframe === "six-months") {
+          return scrims["six-months"].some(
+            (scrim) => scrim.id === death.scrimId
+          );
+        }
+
+        if (timeframe === "one-year") {
+          return scrims["one-year"].some((scrim) => scrim.id === death.scrimId);
+        }
+
+        if (timeframe === "all-time") {
+          return scrims["all-time"].some((scrim) => scrim.id === death.scrimId);
+        }
+
+        if (timeframe === "custom") {
+          return customScrims.some((scrim) => scrim.id === death.scrimId);
+        }
+
+        return false;
+      })
+    );
+  }, [timeframe, deaths, scrims, customScrims]);
 
   useEffect(() => {
     if (timeframe === "all-time") {
@@ -243,9 +300,30 @@ export function Statistics({
 
   const top3Length = top3MostPlayedHeroesArray.length;
 
+  const top3MostDiedToHeroes = filteredDeaths
+    .map((death) => death.attacker_hero)
+    .reduce(
+      (acc, hero) => {
+        if (!acc[hero]) {
+          acc[hero] = 0;
+        }
+
+        acc[hero]++;
+
+        return acc;
+      },
+      {} as Record<string, number>
+    );
+
+  const top3MostDiedToHeroesArray = Object.entries(top3MostDiedToHeroes)
+    .sort((a, b) => b[1] - a[1])
+    .slice(0, 3);
+
+  const top3MostDiedToHeroesLength = top3MostDiedToHeroesArray.length;
+
   return (
     <section className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-      <Card>
+      <Card className="col-span-1 md:col-span-2 xl:col-span-1">
         <CardHeader>
           <CardTitle>Most Played Heroes</CardTitle>
         </CardHeader>
@@ -351,7 +429,7 @@ export function Statistics({
           </p>
         </CardFooter>
       </Card>
-      <Card>
+      <Card className="col-span-1 md:col-span-2 xl:col-span-1">
         <CardHeader>
           <CardTitle>Best Performance</CardTitle>
         </CardHeader>
@@ -477,7 +555,7 @@ export function Statistics({
           </p>
         </CardFooter>
       </Card>
-      <Card className="col-span-1 lg:col-span-2">
+      <Card className="col-span-1 md:col-span-2">
         <CardHeader>
           <CardTitle className="flex items-center gap-1">
             Average Hero Damage Dealt per 10 (per scrim){" "}
@@ -496,7 +574,7 @@ export function Statistics({
           better="higher"
         />
       </Card>
-      <Card className="col-span-1 lg:col-span-2">
+      <Card className="col-span-1 md:col-span-2">
         <CardHeader>
           <CardTitle className="flex items-center gap-1">
             Average Deaths per 10 (per scrim){" "}
@@ -515,13 +593,13 @@ export function Statistics({
           better="lower"
         />
       </Card>
-      <Card className="col-span-1">
+      <Card className="col-span-1 md:col-span-2 xl:col-span-1">
         <CardHeader>
           <CardTitle>Time Spent on Each Role</CardTitle>
         </CardHeader>
         <RolePieChart data={filteredStats} />
       </Card>
-      <Card className="col-span-1">
+      <Card className="col-span-1 md:col-span-2 xl:col-span-1">
         <CardHeader>
           <CardTitle>Final Blows By Method</CardTitle>
         </CardHeader>
@@ -533,11 +611,117 @@ export function Statistics({
         </CardHeader>
         <MapWinsChart data={filteredWins} />
       </Card>
-      <Card className="col-span-2 xl:col-span-1">
+      <Card className="col-span-1 md:col-span-2 xl:col-span-1">
         <CardHeader>
           <CardTitle>Winrate By Map Type</CardTitle>
         </CardHeader>
         <WinsPerMapTypeChart data={filteredWins} />
+      </Card>
+      <Card className="col-span-1 md:col-span-2 xl:col-span-1">
+        <CardHeader>
+          <CardTitle>Heroes Died To Most</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Rank</TableHead>
+                <TableHead>Hero</TableHead>
+                <TableHead>Deaths</TableHead>
+              </TableRow>
+            </TableHeader>
+            <tbody>
+              {top3MostDiedToHeroesArray.map(([hero, deaths], idx) => (
+                <TableRow key={hero}>
+                  <TableCell>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      className={cn(
+                        "h-4 w-4",
+                        idx === 0
+                          ? "text-amber-400"
+                          : idx === 1
+                            ? "text-gray-400"
+                            : idx === 2
+                              ? "text-amber-900"
+                              : "text-muted-foreground"
+                      )}
+                    >
+                      <path d="M6 9H4.5a2.5 2.5 0 0 1 0-5H6" />
+                      <path d="M18 9h1.5a2.5 2.5 0 0 0 0-5H18" />
+                      <path d="M4 22h16" />
+                      <path d="M10 14.66V17c0 .55-.47.98-.97 1.21C7.85 18.75 7 20.24 7 22" />
+                      <path d="M14 14.66V17c0 .55.47.98.97 1.21C16.15 18.75 17 20.24 17 22" />
+                      <path d="M18 2H6v7a6 6 0 0 0 12 0V2Z" />
+                    </svg>
+                  </TableCell>
+                  <TableCell>
+                    <span className="flex items-center space-x-2">
+                      <div className="pr-2">
+                        <Image
+                          src={`/heroes/${toHero(hero)}.png`}
+                          alt=""
+                          width={256}
+                          height={256}
+                          className={cn(
+                            "h-8 w-8 rounded border-2",
+                            idx === 0
+                              ? "border-amber-400"
+                              : idx === 1
+                                ? "border-gray-400"
+                                : idx === 2
+                                  ? "border-amber-900"
+                                  : "border-muted-foreground"
+                          )}
+                        />{" "}
+                      </div>
+                      {hero}
+                    </span>
+                  </TableCell>
+                  <TableCell>{deaths}</TableCell>
+                </TableRow>
+              ))}
+              {top3MostDiedToHeroesLength < 3 &&
+                Array.from({ length: 3 - top3Length }).map((_, idx) => (
+                  // eslint-disable-next-line react/no-array-index-key
+                  <TableRow key={idx}>
+                    <TableCell>-</TableCell>
+                    <TableCell>No data</TableCell>
+                    <TableCell>No data</TableCell>
+                  </TableRow>
+                ))}
+            </tbody>
+          </Table>
+        </CardContent>
+        <CardFooter>
+          <p className="text-sm text-muted-foreground">
+            {timeframe !== "custom" && timeframe !== "all-time" ? (
+              <>
+                Stats collected from {scrims[timeframe].length} scrims in the
+                last {timeframe.replace("-", " ")}
+              </>
+            ) : (
+              <>
+                Stats collected from{" "}
+                {timeframe === "custom"
+                  ? customScrims.length
+                  : timeframe === "all-time" && scrims["all-time"].length}{" "}
+                scrims{" "}
+                {timeframe === "all-time"
+                  ? "in all time data"
+                  : date?.from && date?.to
+                    ? `between ${date.from.toLocaleDateString()} - ${date.to.toLocaleDateString()}`
+                    : "all time"}
+              </>
+            )}
+          </p>
+        </CardFooter>
       </Card>
     </section>
   );
