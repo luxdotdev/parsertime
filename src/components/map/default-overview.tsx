9c8b80390164b6c48265efbeb0d6e3cf8770d564
Lucas Doell <lucas@lux.dev>
2024-06-06 23:55:50 -0400

    Extract winrate calculation to its own file

--- src/components/map/default-overview.tsx
+++ src/components/map/default-overview.tsx
@@ -22,6 +22,7 @@ import { $Enums } from "@prisma/client";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { HeroName, heroPriority, heroRoleMapping } from "@/types/heroes";
 import { getAjaxes } from "@/lib/analytics";
+import { calculateWinner } from "@/lib/winrate";
 
 export async function DefaultOverview({ id }: { id: number }) {
   const finalRound = await prisma.roundEnd.findFirst({
@@ -98,57 +99,12 @@ export async function DefaultOverview({ id }: { id: number }) {
     }
   }
 
-  function calculateWinner() {
-    if (!matchDetails) return "N/A";
-    if (!finalRound) return "N/A";
-
-    switch (mapType) {
-      case $Enums.MapType.Control:
-        return finalRound.team_1_score > finalRound.team_2_score
-          ? matchDetails.team_1_name
-          : matchDetails.team_2_name;
-
-      case $Enums.MapType.Escort:
-        if (team1Captures.length === 0) return matchDetails.team_2_name;
-        if (team2Captures.length === 0) return matchDetails.team_1_name;
-
-        if (team1Captures.length === team2Captures.length) {
-          return team1Captures[team1Captures.length - 1]?.match_time_remaining >
-            team2Captures[team2Captures.length - 1]?.match_time_remaining
-            ? matchDetails.team_1_name
-            : matchDetails.team_2_name;
-        }
-
-        return team1Captures.length > team2Captures.length
-          ? matchDetails.team_1_name
-          : matchDetails.team_2_name;
-
-      case $Enums.MapType.Flashpoint:
-        return finalRound.team_1_score > finalRound.team_2_score
-          ? matchDetails.team_1_name
-          : matchDetails.team_2_name;
-
-      case $Enums.MapType.Hybrid:
-        if (!team1Captures) return matchDetails.team_2_name;
-        if (!team2Captures) return matchDetails.team_1_name;
-
-        if (team1Captures.length === team2Captures.length) {
-          return team1Captures[team1Captures.length - 1]?.match_time_remaining >
-            team2Captures[team2Captures.length - 1]?.match_time_remaining
-            ? matchDetails.team_1_name
-            : matchDetails.team_2_name;
-        }
-
-        return team1Captures.length > team2Captures.length
-          ? matchDetails.team_1_name
-          : matchDetails.team_2_name;
-
-      case $Enums.MapType.Push:
-        return "N/A";
-      default:
-        return "N/A";
-    }
-  }
+  const winner = calculateWinner({
+    matchDetails,
+    finalRound,
+    team1Captures,
+    team2Captures,
+  });
 
   const numberOfRounds =
     mapType === $Enums.MapType.Flashpoint ? 5 : finalRound?.round_number ?? 1;
@@ -240,12 +196,12 @@ export async function DefaultOverview({ id }: { id: number }) {
                   Winner:{" "}
                   <span
                     className={
-                      calculateWinner() === matchDetails?.team_1_name
+                      winner === matchDetails?.team_1_name
                         ? "text-blue-500"
                         : "text-red-500"
                     }
                   >
-                    {calculateWinner()}
+                    {winner}
                   </span>
                 </>
               ) : (
---
+++ src/lib/winrate.ts
@@ -0,0 +1,70 @@
+import {
+  $Enums,
+  MatchStart,
+  ObjectiveCaptured,
+  RoundEnd,
+} from "@prisma/client";
+
+export function calculateWinner({
+  matchDetails,
+  finalRound,
+  team1Captures,
+  team2Captures,
+}: {
+  matchDetails: MatchStart | null;
+  finalRound: RoundEnd | null;
+  team1Captures: ObjectiveCaptured[];
+  team2Captures: ObjectiveCaptured[];
+}) {
+  const mapType = matchDetails ? matchDetails.map_type : $Enums.MapType.Control;
+
+  if (!matchDetails) return "N/A";
+  if (!finalRound) return "N/A";
+
+  switch (mapType) {
+    case $Enums.MapType.Control:
+      return finalRound.team_1_score > finalRound.team_2_score
+        ? matchDetails.team_1_name
+        : matchDetails.team_2_name;
+
+    case $Enums.MapType.Escort:
+      if (team1Captures.length === 0) return matchDetails.team_2_name;
+      if (team2Captures.length === 0) return matchDetails.team_1_name;
+
+      if (team1Captures.length === team2Captures.length) {
+        return team1Captures[team1Captures.length - 1]?.match_time_remaining >
+          team2Captures[team2Captures.length - 1]?.match_time_remaining
+          ? matchDetails.team_1_name
+          : matchDetails.team_2_name;
+      }
+
+      return team1Captures.length > team2Captures.length
+        ? matchDetails.team_1_name
+        : matchDetails.team_2_name;
+
+    case $Enums.MapType.Flashpoint:
+      return finalRound.team_1_score > finalRound.team_2_score
+        ? matchDetails.team_1_name
+        : matchDetails.team_2_name;
+
+    case $Enums.MapType.Hybrid:
+      if (!team1Captures) return matchDetails.team_2_name;
+      if (!team2Captures) return matchDetails.team_1_name;
+
+      if (team1Captures.length === team2Captures.length) {
+        return team1Captures[team1Captures.length - 1]?.match_time_remaining >
+          team2Captures[team2Captures.length - 1]?.match_time_remaining
+          ? matchDetails.team_1_name
+          : matchDetails.team_2_name;
+      }
+
+      return team1Captures.length > team2Captures.length
+        ? matchDetails.team_1_name
+        : matchDetails.team_2_name;
+
+    case $Enums.MapType.Push:
+      return "N/A";
+    default:
+      return "N/A";
+  }
+}
