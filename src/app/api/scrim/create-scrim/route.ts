f328a1d4cbd1c2d09ef7014629d5c36594dffe53
Lucas Doell <lucas@lux.dev>
2024-08-10 02:48:59 -0400

    Update create-scrim route to include rate limiting and suspicious activity webhook

--- src/app/api/scrim/create-scrim/route.ts
+++ src/app/api/scrim/create-scrim/route.ts
@@ -1,7 +1,16 @@
+import { getUser } from "@/data/user-dto";
 import { auth } from "@/lib/auth";
 import Logger from "@/lib/logger";
 import { createNewScrimFromParsedData } from "@/lib/parser";
+import {
+  newSuspiciousActivityWebhookConstructor,
+  sendDiscordWebhook,
+} from "@/lib/webhooks";
 import { ParserData } from "@/types/parser";
+import { User } from "@prisma/client";
+import { Ratelimit } from "@upstash/ratelimit";
+import { kv } from "@vercel/kv";
+import { NextRequest, userAgent } from "next/server";
 
 export type CreateScrimRequestData = {
   name: string;
@@ -10,10 +19,8 @@ export type CreateScrimRequestData = {
   map: ParserData;
 };
 
-export async function POST(request: Request) {
+export async function POST(request: NextRequest) {
   const session = await auth();
-
-  const data = (await request.json()) as CreateScrimRequestData;
 
   if (!session) {
     Logger.warn("Unauthorized request to create scrim");
@@ -22,6 +29,47 @@ export async function POST(request: Request) {
       status: 401,
     });
   }
+
+  // Create a new ratelimiter, that allows 5 requests per 1 minute
+  const ratelimit = new Ratelimit({
+    redis: kv,
+    limiter: Ratelimit.slidingWindow(5, "1 m"),
+    analytics: true,
+  });
+
+  // Limit the requests to 5 per minute per user
+  const identifier = request.ip ?? "127.0.0.1";
+  const { success } = await ratelimit.limit(identifier);
+
+  if (!success) {
+    Logger.log("Rate limit exceeded for scrim creation", identifier);
+
+    const ua = userAgent(request);
+
+    const user = await getUser(session.user.email);
+
+    const fallbackUser = {
+      name: "Unknown",
+      email: "unknown",
+      id: "unknown",
+    } as User;
+
+    const wh = newSuspiciousActivityWebhookConstructor(
+      user ?? fallbackUser,
+      "Scrim creation (rate limit exceeded)",
+      ua.ua,
+      ua.browser,
+      ua.os,
+      ua.device
+    );
+    await sendDiscordWebhook(process.env.DISCORD_WEBHOOK_URL, wh);
+
+    return new Response("Rate limit exceeded", {
+      status: 429,
+    });
+  }
+
+  const data = (await request.json()) as CreateScrimRequestData;
 
   if (data.map === null) {
     Logger.warn("Invalid map data");
