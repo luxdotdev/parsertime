df82578e4fbeecc6e8895fd9db39bf8bb1736c46
Kenny Miranda <kenny@lux.dev>
2024-08-14 19:46:55 -0400

    added HeroNameJP

--- src/types/heroes.ts
+++ src/types/heroes.ts
@@ -1,204 +1,286 @@
 export type Hero = Support | Tank | DPS;
 
 export type HeroName = Hero["name"];
+export type HeroNameJP = Hero["nameJP"];
+//export type HeroNameKR = Hero["nameKR"];
 
 export type Ana = {
   name: "Ana";
+  nameJP: "アナ";
+  //nameKR: "";
   image: "ana.png";
 };
 
 export type Ashe = {
   name: "Ashe";
+  nameJP: "アッシュ";
+  //nameKR: "";
   image: "ashe.png";
 };
 
 export type Baptiste = {
   name: "Baptiste";
+  nameJP: "バティスト";
+  //nameKR: "";
   image: "baptiste.png";
 };
 
 export type Bastion = {
   name: "Bastion";
+  nameJP: "バスティオン";
+  //nameKR: "";
   image: "bastion.png";
 };
 
 export type Brigitte = {
   name: "Brigitte";
+  nameJP: "ブリジット";
+  //nameKR: "";
   image: "brigitte.png";
 };
 
 export type Cassidy = {
   name: "Cassidy";
+  nameJP: "キャシディ";
+  //nameKR: "";
   image: "cassidy.png";
 };
 
 export type Doomfist = {
   name: "Doomfist";
+  nameJP: "ドゥームフィスト";
+  //nameKR: "";
   image: "doomfist.png";
 };
 
 export type Dva = {
   name: "D.Va";
+  nameJP: "D.VA";
+  //nameKR: "";
   image: "dva.png";
 };
 
 export type Echo = {
   name: "Echo";
+  nameJP: "エコー";
+  //nameKR: "";
   image: "echo.png";
 };
 
 export type Genji = {
   name: "Genji";
+  nameJP: "ゲンジ";
+  //nameKR: "";
   image: "genji.png";
 };
 
 export type Hanzo = {
   name: "Hanzo";
+  nameJP: "半蔵";
+  //nameKR: "";
   image: "hanzo.png";
 };
 
 export type Illari = {
   name: "Illari";
+  nameJP: "イラリー";
+  //nameKR: "";
   image: "illari.png";
 };
 
 export type JunkerQueen = {
   name: "Junker Queen";
+  nameJP: "ジャンカークイーン";
+  //nameKR: "";
   image: "junkerqueen.png";
 };
 
 export type Junkrat = {
   name: "Junkrat";
+  nameJP: "ジャンクラット";
+  //nameKR: "";
   image: "junkrat.png";
 };
 
 export type Kiriko = {
   name: "Kiriko";
+  nameJP: "キリコ";
+  //nameKR: "";
   image: "kiriko.png";
 };
 
 export type Lifeweaver = {
   name: "Lifeweaver";
+  nameJP: "ライフウィーバー";
+  //nameKR: "";
   image: "lifeweaver.png";
 };
 
 export type Lucio = {
   name: "Lúcio";
+  nameJP: "ルシオ";
+  //nameKR: "";
   image: "lucio.png";
 };
 
 export type Mauga = {
   name: "Mauga";
+  nameJP: "マウガ";
+  //nameKR: "";
   image: "mauga.png";
 };
 
 export type Mei = {
   name: "Mei";
+  nameJP: "メイ";
+  //nameKR: "";
   image: "mei.png";
 };
 
 export type Mercy = {
   name: "Mercy";
+  nameJP: "慈悲";
+  //nameKR: "";
   image: "mercy.png";
 };
 
 export type Moira = {
   name: "Moira";
+  nameJP: "モイラ";
+  //nameKR: "";
   image: "moira.png";
 };
 
 export type Orisa = {
   name: "Orisa";
+  nameJP: "オリサ";
+  //nameKR: "";
   image: "orisa.png";
 };
 
 export type Pharah = {
   name: "Pharah";
+  nameJP: "ファラ";
+  //nameKR: "";
   image: "pharah.png";
 };
 
 export type Ramattra = {
   name: "Ramattra";
+  nameJP: "ラマットラ";
+  //nameKR: "";
   image: "ramattra.png";
 };
 
 export type Reaper = {
   name: "Reaper";
+  nameJP: "刈取";
+  //nameKR: "";
   image: "reaper.png";
 };
 
 export type Reinhardt = {
   name: "Reinhardt";
+  nameJP: "ラインハルト";
+  //nameKR: "";
   image: "reinhardt.png";
 };
 
 export type Roadhog = {
   name: "Roadhog";
+  nameJP: "ロードホッグ";
+  //nameKR: "";
   image: "roadhog.png";
 };
 
 export type Sigma = {
   name: "Sigma";
+  nameJP: "シグマ";
+  //nameKR: "";
   image: "sigma.png";
 };
 
 export type Sojourn = {
   name: "Sojourn";
+  nameJP: "ソジョーン";
+  //nameKR: "";
   image: "sojourn.png";
 };
 
 export type Soldier76 = {
   name: "Soldier: 76";
+  nameJP: "ソルジャー76";
+  //nameKR: "";
   image: "soldier76.png";
 };
 
 export type Sombra = {
   name: "Sombra";
+  nameJP: "ソンブラ";
+  //nameKR: "";
   image: "sombra.png";
 };
 
 export type Symmetra = {
   name: "Symmetra";
+  nameJP: "シンメトラ";
+  //nameKR: "";
   image: "symmetra.png";
 };
 
 export type Torbjorn = {
   name: "Torbjörn";
+  nameJP: "トールビョーン";
+  //nameKR: "";
   image: "torbjorn.png";
 };
 
 export type Tracer = {
   name: "Tracer";
+  nameJP: "トレーサー";
+  //nameKR: "";
   image: "tracer.png";
 };
 
 export type Venture = {
   name: "Venture";
+  nameJP: "ベンチャー";
+  //nameKR: "";
   image: "venture.png";
 };
 
 export type Widowmaker = {
   name: "Widowmaker";
+  nameJP: "ウィドウメイカー";
+  //nameKR: "";
   image: "widowmaker.png";
 };
 
 export type Winston = {
   name: "Winston";
+  nameJP: "ウィンストン";
+  //nameKR: "";
   image: "winston.png";
 };
 
 export type WreckingBall = {
   name: "Wrecking Ball";
+  nameJP: "レッキング・ボール";
+  //nameKR: "";
   image: "wreckingball.png";
 };
 
 export type Zarya = {
   name: "Zarya";
+  nameJP: "ザリア";
+  //nameKR: "";
   image: "zarya.png";
 };
 
 export type Zenyatta = {
   name: "Zenyatta";
+  nameJP: "ゼニヤッタ";
+  //nameKR: "";
   image: "zenyatta.png";
 };
 
@@ -246,99 +328,183 @@ export type Support =
   | Moira
   | Zenyatta;
 
-export const heroRoleMapping: Record<HeroName, "Tank" | "Damage" | "Support"> =
-  {
-    Ana: "Support",
-    Ashe: "Damage",
-    Baptiste: "Support",
-    Bastion: "Damage",
-    Brigitte: "Support",
-    Cassidy: "Damage",
-    Doomfist: "Tank",
-    "D.Va": "Tank",
-    Echo: "Damage",
-    Genji: "Damage",
-    Hanzo: "Damage",
-    Illari: "Support",
-    "Junker Queen": "Tank",
-    Junkrat: "Damage",
-    Kiriko: "Support",
-    Lifeweaver: "Support",
-    Lúcio: "Support",
-    Mauga: "Tank",
-    Mei: "Damage",
-    Mercy: "Support",
-    Moira: "Support",
-    Orisa: "Tank",
-    Pharah: "Damage",
-    Ramattra: "Tank",
-    Reaper: "Damage",
-    Reinhardt: "Tank",
-    Roadhog: "Tank",
-    Sigma: "Tank",
-    Sojourn: "Damage",
-    "Soldier: 76": "Damage",
-    Sombra: "Damage",
-    Symmetra: "Damage",
-    Torbjörn: "Damage",
-    Tracer: "Damage",
-    Venture: "Damage",
-    Widowmaker: "Damage",
-    Winston: "Tank",
-    "Wrecking Ball": "Tank",
-    Zarya: "Tank",
-    Zenyatta: "Support",
-  };
+export const heroRoleMapping: Record<
+  HeroName | HeroNameJP,
+  "Tank" | "Damage" | "Support"
+> = {
+  Ana: "Support",
+  Ashe: "Damage",
+  Baptiste: "Support",
+  Bastion: "Damage",
+  Brigitte: "Support",
+  Cassidy: "Damage",
+  Doomfist: "Tank",
+  "D.Va": "Tank",
+  Echo: "Damage",
+  Genji: "Damage",
+  Hanzo: "Damage",
+  Illari: "Support",
+  "Junker Queen": "Tank",
+  Junkrat: "Damage",
+  Kiriko: "Support",
+  Lifeweaver: "Support",
+  Lúcio: "Support",
+  Mauga: "Tank",
+  Mei: "Damage",
+  Mercy: "Support",
+  Moira: "Support",
+  Orisa: "Tank",
+  Pharah: "Damage",
+  Ramattra: "Tank",
+  Reaper: "Damage",
+  Reinhardt: "Tank",
+  Roadhog: "Tank",
+  Sigma: "Tank",
+  Sojourn: "Damage",
+  "Soldier: 76": "Damage",
+  Sombra: "Damage",
+  Symmetra: "Damage",
+  Torbjörn: "Damage",
+  Tracer: "Damage",
+  Venture: "Damage",
+  Widowmaker: "Damage",
+  Winston: "Tank",
+  "Wrecking Ball": "Tank",
+  Zarya: "Tank",
+  Zenyatta: "Support",
+  // JP
+  アナ: "Support",
+  アッシュ: "Damage",
+  バティスト: "Support",
+  バスティオン: "Damage",
+  ブリジット: "Support",
+  キャシディ: "Damage",
+  ドゥームフィスト: "Tank",
+  "D.VA": "Tank",
+  エコー: "Damage",
+  ゲンジ: "Damage",
+  半蔵: "Damage",
+  イラリー: "Support",
+  ジャンカークイーン: "Tank",
+  ジャンクラット: "Damage",
+  キリコ: "Support",
+  ライフウィーバー: "Support",
+  ルシオ: "Support",
+  マウガ: "Tank",
+  メイ: "Damage",
+  慈悲: "Support",
+  モイラ: "Support",
+  オリサ: "Tank",
+  ファラ: "Damage",
+  ラマットラ: "Tank",
+  刈取: "Damage",
+  ラインハルト: "Tank",
+  ロードホッグ: "Tank",
+  シグマ: "Tank",
+  ソジョーン: "Damage",
+  ソルジャー76: "Damage",
+  ソンブラ: "Damage",
+  シンメトラ: "Damage",
+  トールビョーン: "Damage",
+  トレーサー: "Damage",
+  ベンチャー: "Damage",
+  ウィドウメイカー: "Damage",
+  ウィンストン: "Tank",
+  "レッキング・ボール": "Tank",
+  ザリア: "Tank",
+  ゼニヤッタ: "Support",
+  // KR  ;(
+};
 
 export const roleHeroMapping: Record<
   "Tank" | "Damage" | "Support",
-  HeroName[]
+  (HeroName | HeroNameJP)[]
 > = {
   Tank: [
     "D.Va",
+    "D.VA",
     "Doomfist",
+    "ドゥームフィスト",
     "Junker Queen",
+    "ジャンカークイーン",
     "Mauga",
+    "マウガ",
     "Orisa",
+    "オリサ",
     "Ramattra",
+    "ラマットラ",
     "Reinhardt",
+    "ラインハルト",
     "Roadhog",
+    "ロードホッグ",
     "Sigma",
+    "シグマ",
     "Winston",
+    "ウィンストン",
     "Wrecking Ball",
+    "レッキング・ボール",
     "Zarya",
+    "ザリア",
   ],
   Damage: [
     "Ashe",
+    "アッシュ",
     "Bastion",
+    "バスティオン",
     "Cassidy",
+    "キャシディ",
     "Echo",
+    "エコー",
     "Genji",
+    "ゲンジ",
     "Hanzo",
+    "半蔵",
     "Junkrat",
+    "ジャンクラット",
     "Mei",
+    "メイ",
     "Pharah",
+    "ファラ",
     "Reaper",
+    "刈取",
     "Sojourn",
+    "ソジョーン",
     "Soldier: 76",
+    "ソルジャー76",
     "Sombra",
+    "ソンブラ",
     "Symmetra",
+    "シンメトラ",
     "Torbjörn",
+    "トールビョーン",
     "Tracer",
+    "トレーサー",
     "Venture",
+    "ベンチャー",
     "Widowmaker",
+    "ウィドウメイカー",
   ],
   Support: [
     "Ana",
+    "アナ",
     "Baptiste",
+    "バティスト",
     "Brigitte",
+    "ブリジット",
     "Illari",
+    "イラリー",
     "Kiriko",
+    "キリコ",
     "Lifeweaver",
+    "ライフウィーバー",
     "Lúcio",
+    "ルシオ",
     "Mercy",
+    "慈悲",
     "Moira",
+    "モイラ",
     "Zenyatta",
+    "ゼニヤッタ",
   ],
 };
 
@@ -347,3 +513,50 @@ export const heroPriority = {
   Tank: 2,
   Support: 3,
 };
+
+export const japaneseToEnglishHeroName: Record<HeroNameJP, HeroName> = {
+  アナ: "Ana",
+  アッシュ: "Ashe",
+  バティスト: "Baptiste",
+  バスティオン: "Bastion",
+  ブリジット: "Brigitte",
+  キャシディ: "Cassidy",
+  ドゥームフィスト: "Doomfist",
+  "D.VA": "D.Va",
+  エコー: "Echo",
+  ゲンジ: "Genji",
+  半蔵: "Hanzo",
+  イラリー: "Illari",
+  ジャンカークイーン: "Junker Queen",
+  ジャンクラット: "Junkrat",
+  キリコ: "Kiriko",
+  ライフウィーバー: "Lifeweaver",
+  ルシオ: "Lúcio",
+  マウガ: "Mauga",
+  メイ: "Mei",
+  慈悲: "Mercy",
+  モイラ: "Moira",
+  オリサ: "Orisa",
+  ファラ: "Pharah",
+  ラマットラ: "Ramattra",
+  刈取: "Reaper",
+  ラインハルト: "Reinhardt",
+  ロードホッグ: "Roadhog",
+  シグマ: "Sigma",
+  ソジョーン: "Sojourn",
+  ソルジャー76: "Soldier: 76",
+  ソンブラ: "Sombra",
+  シンメトラ: "Symmetra",
+  トールビョーン: "Torbjörn",
+  トレーサー: "Tracer",
+  ベンチャー: "Venture",
+  ウィドウメイカー: "Widowmaker",
+  ウィンストン: "Winston",
+  "レッキング・ボール": "Wrecking Ball",
+  ザリア: "Zarya",
+  ゼニヤッタ: "Zenyatta",
+};
+
+//export const koreanToEnglishHeroName: Record<HeroNameKR, HeroName> = {};
+
+//export const englishToKoreanHeroName: Record<HeroNameKR, HeroName> = {}; Can make translations with this?
