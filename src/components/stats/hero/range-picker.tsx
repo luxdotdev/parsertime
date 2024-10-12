9666926a023c061993e440c4d0ad7710eff9d370
Lucas Doell <lucas@lux.dev>
2024-10-12 17:12:09 -0400

    Refactor with constants to avoid unnecessarily calling Date constructor

--- src/components/stats/hero/range-picker.tsx
+++ src/components/stats/hero/range-picker.tsx
@@ -52,27 +52,26 @@ export function RangePicker({
   deaths: Kill[];
   hero: HeroName;
 }) {
+  const TODAY = new Date();
+  const LAST_WEEK = addWeeks(TODAY, -1);
+
   const [timeframe, setTimeframe] = useState<Timeframe>("one-week");
   const [date, setDate] = useState<DateRange | undefined>({
-    from: addWeeks(new Date(), -1),
-    to: new Date(),
+    from: LAST_WEEK,
+    to: TODAY,
   });
 
   function onTimeframeChange(val: Timeframe) {
     setTimeframe(val);
 
-    if (val === "one-week")
-      setDate({ from: addWeeks(new Date(), -1), to: new Date() });
-    if (val === "two-weeks")
-      setDate({ from: addWeeks(new Date(), -2), to: new Date() });
-    if (val === "one-month")
-      setDate({ from: addMonths(new Date(), -1), to: new Date() });
+    if (val === "one-week") setDate({ from: addWeeks(TODAY, -1), to: TODAY });
+    if (val === "two-weeks") setDate({ from: addWeeks(TODAY, -2), to: TODAY });
+    if (val === "one-month") setDate({ from: addMonths(TODAY, -1), to: TODAY });
     if (val === "three-months")
-      setDate({ from: addMonths(new Date(), -3), to: new Date() });
+      setDate({ from: addMonths(TODAY, -3), to: TODAY });
     if (val === "six-months")
-      setDate({ from: addMonths(new Date(), -6), to: new Date() });
-    if (val === "one-year")
-      setDate({ from: addYears(new Date(), -1), to: new Date() });
+      setDate({ from: addMonths(TODAY, -6), to: TODAY });
+    if (val === "one-year") setDate({ from: addYears(TODAY, -1), to: TODAY });
     if (val === "all-time") setDate({ from: undefined, to: undefined });
   }
 
