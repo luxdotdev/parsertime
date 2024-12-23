de5d9874f44bf997bc0374d1c18b23dfb824e4f0
Lucas Doell <lucas@lux.dev>
2024-12-22 23:28:18 -0500

    Read locale from cookies

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,10 +1,9 @@
 /* eslint-disable */
+import { getUserLocale } from "@/lib/locale";
 import { getRequestConfig } from "next-intl/server";
 
 export default getRequestConfig(async () => {
-  // Provide a static locale, fetch a user setting,
-  // read from `cookies()`, `headers()`, etc.
-  const locale = "en";
+  const locale = await getUserLocale();
 
   return {
     locale,
