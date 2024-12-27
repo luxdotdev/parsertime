dcb6252e1042cf53f4ac85cc3d8f3607444b4d5e
Lucas Doell <lucas@lux.dev>
2024-12-27 02:48:38 -0500

    Add translations for team names in compare players component

--- src/components/map/compare-players.tsx
+++ src/components/map/compare-players.tsx
@@ -3,6 +3,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import prisma from "@/lib/prisma";
 import { HeroName, heroRoleMapping } from "@/types/heroes";
+import { getTranslations } from "next-intl/server";
 
 type PlayerToSort = {
   player_name: string;
@@ -30,6 +31,8 @@ function sortByRole(a: PlayerToSort, b: PlayerToSort) {
 }
 
 export async function ComparePlayers({ id }: { id: number }) {
+  const t = await getTranslations("mapPage.compare");
+
   const teamNames = await prisma.matchStart.findFirst({
     where: {
       MapDataId: id,
@@ -83,7 +86,7 @@ export async function ComparePlayers({ id }: { id: number }) {
     <div className="grid gap-4 md:grid-cols-3 xl:grid-cols-6">
       <Card className="col-span-3">
         <CardHeader>
-          <CardTitle>{teamNames?.team_1_name || "Team 1"}</CardTitle>
+          <CardTitle>{teamNames?.team_1_name || t("team1")}</CardTitle>
         </CardHeader>
         <CardContent>
           <Tabs defaultValue="0" className="space-y-4">
@@ -104,7 +107,7 @@ export async function ComparePlayers({ id }: { id: number }) {
       </Card>
       <Card className="col-span-3">
         <CardHeader>
-          <CardTitle>{teamNames?.team_2_name || "Team 2"}</CardTitle>
+          <CardTitle>{teamNames?.team_2_name || t("team2")}</CardTitle>
         </CardHeader>
         <CardContent>
           <Tabs defaultValue="0" className="space-y-4">
