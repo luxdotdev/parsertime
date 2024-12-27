4f9dbfcc45605699c4bd204ea6e1216204807a0e
Lucas Doell <lucas@lux.dev>
2024-12-26 23:48:42 -0500

    Add translations to overview table

--- src/components/map/overview-table.tsx
+++ src/components/map/overview-table.tsx
@@ -39,294 +39,7 @@ import {
 } from "@heroicons/react/20/solid";
 import { PlayerStat } from "@prisma/client";
 import { GeistMono } from "geist/font/mono";
-
-export const columns: ColumnDef<PlayerData>[] = [
-  {
-    accessorKey: "id",
-    header: "",
-    cell: ({ row }) => <div className="text-right">{row.index + 1}</div>,
-    enableSorting: false,
-    enableColumnFilter: false,
-  },
-  {
-    accessorKey: "playerName",
-    header: ({ header }) => (
-      <OverviewTableHeader tooltip={tooltips.playerName} header={header}>
-        Player Name
-      </OverviewTableHeader>
-    ),
-    cell: ({ row }) => (
-      <button
-        type="button"
-        onClick={() =>
-          (window.location.href = `${
-            window.location.href
-          }/player/${encodeURIComponent(row.getValue("playerName"))}`)
-        }
-      >
-        {row.getValue("playerName")}
-      </button>
-    ),
-    enableSorting: true,
-    sortingFn: "basic",
-    filterFn: "includesString",
-  },
-  {
-    accessorKey: "role",
-    header: ({ header }) => (
-      <OverviewTableHeader tooltip={tooltips.role} header={header}>
-        Role
-      </OverviewTableHeader>
-    ),
-    cell: ({ row }) => <div className="capitalize">{row.getValue("role")}</div>,
-    enableSorting: true,
-    enableColumnFilter: false,
-  },
-  {
-    accessorKey: "playerTeam",
-    header: ({ header }) => (
-      <OverviewTableHeader tooltip={tooltips.playerTeam} header={header}>
-        Player Team
-      </OverviewTableHeader>
-    ),
-    cell: ({ row }) => (
-      <div className="capitalize">{row.getValue("playerTeam")}</div>
-    ),
-    enableSorting: true,
-    enableColumnFilter: false,
-  },
-  {
-    accessorKey: "timePlayed",
-    header: ({ header }) => (
-      <OverviewTableHeader tooltip={tooltips.timePlayed} header={header}>
-        Time Played
-      </OverviewTableHeader>
-    ),
-    cell: ({ row }) => (
-      <div className={cn(GeistMono.className, "text-right capitalize")}>
-        {toTimestamp(row.getValue<number>("timePlayed"))}
-      </div>
-    ),
-    enableSorting: true,
-    enableColumnFilter: false,
-  },
-  {
-    accessorKey: "eliminations",
-    header: ({ header }) => (
-      <OverviewTableHeader tooltip={tooltips.eliminations} header={header}>
-        Eliminations
-      </OverviewTableHeader>
-    ),
-    cell: ({ row }) => (
-      <div className={cn(GeistMono.className, "text-right capitalize")}>
-        {row.getValue("eliminations")}
-      </div>
-    ),
-    enableSorting: true,
-    enableColumnFilter: false,
-  },
-  {
-    accessorKey: "kills",
-    header: ({ header }) => (
-      <OverviewTableHeader tooltip={tooltips.kills} header={header}>
-        Final Blows
-      </OverviewTableHeader>
-    ),
-    cell: ({ row }) => (
-      <div className={cn(GeistMono.className, "text-right capitalize")}>
-        {row.getValue("kills")}
-      </div>
-    ),
-    enableSorting: true,
-    enableColumnFilter: false,
-  },
-  {
-    accessorKey: "assists",
-    header: ({ header }) => (
-      <OverviewTableHeader tooltip={tooltips.assists} header={header}>
-        Assists
-      </OverviewTableHeader>
-    ),
-    cell: ({ row }) => (
-      <div className={cn(GeistMono.className, "text-right capitalize")}>
-        {row.getValue("assists")}
-      </div>
-    ),
-    enableSorting: true,
-    enableColumnFilter: false,
-  },
-  {
-    accessorKey: "deaths",
-    header: ({ header }) => (
-      <OverviewTableHeader tooltip={tooltips.deaths} header={header}>
-        Deaths
-      </OverviewTableHeader>
-    ),
-    cell: ({ row }) => (
-      <div className={cn(GeistMono.className, "text-right capitalize")}>
-        {row.getValue("deaths")}
-      </div>
-    ),
-    enableSorting: true,
-    enableColumnFilter: false,
-  },
-  {
-    accessorKey: "kd",
-    header: ({ header }) => (
-      <OverviewTableHeader tooltip={tooltips.kd} header={header}>
-        K/D
-      </OverviewTableHeader>
-    ),
-    cell: ({ row }) => (
-      <div className={cn(GeistMono.className, "text-right capitalize")}>
-        {row.getValue<number>("kd").toFixed(2)}
-      </div>
-    ),
-    enableSorting: true,
-    enableColumnFilter: false,
-  },
-  {
-    accessorKey: "kad",
-    header: ({ header }) => (
-      <OverviewTableHeader tooltip={tooltips.kad} header={header}>
-        KA/D
-      </OverviewTableHeader>
-    ),
-    cell: ({ row }) => (
-      <div className={cn(GeistMono.className, "text-right capitalize")}>
-        {row.getValue<number>("kad").toFixed(2)}
-      </div>
-    ),
-    enableSorting: true,
-    enableColumnFilter: false,
-  },
-  {
-    accessorKey: "heroDmgDealt",
-    header: ({ header }) => (
-      <OverviewTableHeader tooltip={tooltips.heroDmgDealt} header={header}>
-        Hero Damage Dealt
-      </OverviewTableHeader>
-    ),
-    cell: ({ row }) => (
-      <div className={cn(GeistMono.className, "text-right capitalize")}>
-        {row.getValue<number>("heroDmgDealt").toFixed(2)}
-      </div>
-    ),
-    enableSorting: true,
-    enableColumnFilter: false,
-  },
-  {
-    accessorKey: "dmgReceived",
-    header: ({ header }) => (
-      <OverviewTableHeader tooltip={tooltips.dmgReceived} header={header}>
-        Damage Received
-      </OverviewTableHeader>
-    ),
-    cell: ({ row }) => (
-      <div className={cn(GeistMono.className, "text-right capitalize")}>
-        {row.getValue<number>("dmgReceived").toFixed(2)}
-      </div>
-    ),
-    enableSorting: true,
-    enableColumnFilter: false,
-  },
-  {
-    accessorKey: "healingReceived",
-    header: ({ header }) => (
-      <OverviewTableHeader tooltip={tooltips.healingReceived} header={header}>
-        Healing Received
-      </OverviewTableHeader>
-    ),
-    cell: ({ row }) => (
-      <div className={cn(GeistMono.className, "text-right capitalize")}>
-        {row.getValue<number>("healingReceived").toFixed(2)}
-      </div>
-    ),
-    enableSorting: true,
-    enableColumnFilter: false,
-  },
-  {
-    accessorKey: "healingDealt",
-    header: ({ header }) => (
-      <OverviewTableHeader tooltip={tooltips.healingDealt} header={header}>
-        Healing Dealt
-      </OverviewTableHeader>
-    ),
-    cell: ({ row }) => (
-      <div className={cn(GeistMono.className, "text-right capitalize")}>
-        {row.getValue<number>("healingDealt").toFixed(2)}
-      </div>
-    ),
-    enableSorting: true,
-    enableColumnFilter: false,
-  },
-  {
-    accessorKey: "dmgToHealsRatio",
-    header: ({ header }) => (
-      <OverviewTableHeader tooltip={tooltips.dmgToHealsRatio} header={header}>
-        Damage Dealt:Healing Received
-      </OverviewTableHeader>
-    ),
-    cell: ({ row }) => (
-      <div className={cn(GeistMono.className, "text-right capitalize")}>
-        {row.getValue<number>("dmgToHealsRatio").toFixed(2)}
-      </div>
-    ),
-    enableSorting: true,
-    enableColumnFilter: false,
-  },
-  {
-    accessorKey: "ultsCharged",
-    header: ({ header }) => (
-      <OverviewTableHeader tooltip={tooltips.ultsCharged} header={header}>
-        Ultimates Charged
-      </OverviewTableHeader>
-    ),
-    cell: ({ row }) => (
-      <div className={cn(GeistMono.className, "text-right capitalize")}>
-        {row.getValue("ultsCharged")}
-      </div>
-    ),
-    enableSorting: true,
-    enableColumnFilter: false,
-  },
-  {
-    accessorKey: "ultsUsed",
-    header: ({ header }) => (
-      <OverviewTableHeader tooltip={tooltips.ultsUsed} header={header}>
-        Ultimates Used
-      </OverviewTableHeader>
-    ),
-    cell: ({ row }) => (
-      <div className={cn(GeistMono.className, "text-right capitalize")}>
-        {row.getValue("ultsUsed")}
-      </div>
-    ),
-    enableSorting: true,
-    enableColumnFilter: false,
-  },
-];
-
-const tooltips = {
-  playerName:
-    "The player's name. Click on their name to view their statistics.",
-  role: "The role the player played.",
-  playerTeam: "The team the player played for.",
-  timePlayed: "The time the player played.",
-  eliminations: "The number of eliminations the player had.",
-  kills: "The number of final blows the player had.",
-  assists: "The number of assists the player had.",
-  deaths: "The number of deaths the player had.",
-  kd: "The player's kill/death ratio.",
-  kad: "The player's kill/assist/death ratio.",
-  heroDmgDealt: "The amount of hero damage the player dealt.",
-  dmgReceived: "The amount of damage the player received.",
-  healingReceived: "The amount of healing the player received.",
-  healingDealt: "The amount of healing the player dealt.",
-  dmgToHealsRatio: "The player's damage dealt to healing received ratio.",
-  ultsCharged: "The number of ultimates the player charged.",
-  ultsUsed: "The number of ultimates the player used.",
-};
+import { useTranslations } from "next-intl";
 
 export function OverviewTable({ playerStats }: { playerStats: PlayerStat[] }) {
   const [sorting, setSorting] = React.useState<SortingState>([]);
@@ -341,6 +54,297 @@ export function OverviewTable({ playerStats }: { playerStats: PlayerStat[] }) {
     () => aggregatePlayerData(playerStats),
     [playerStats]
   );
+
+  const t = useTranslations("mapPage.overviewTable");
+
+  const columns: ColumnDef<PlayerData>[] = [
+    {
+      accessorKey: "id",
+      header: "",
+      cell: ({ row }) => <div className="text-right">{row.index + 1}</div>,
+      enableSorting: false,
+      enableColumnFilter: false,
+    },
+    {
+      accessorKey: "playerName",
+      header: ({ header }) => (
+        <OverviewTableHeader tooltip={tooltips.playerName} header={header}>
+          {t("header.playerName")}
+        </OverviewTableHeader>
+      ),
+      cell: ({ row }) => (
+        <button
+          type="button"
+          onClick={() =>
+            (window.location.href = `${
+              window.location.href
+            }/player/${encodeURIComponent(row.getValue("playerName"))}`)
+          }
+        >
+          {row.getValue("playerName")}
+        </button>
+      ),
+      enableSorting: true,
+      sortingFn: "basic",
+      filterFn: "includesString",
+    },
+    {
+      accessorKey: "role",
+      header: ({ header }) => (
+        <OverviewTableHeader tooltip={tooltips.role} header={header}>
+          {t("header.role")}
+        </OverviewTableHeader>
+      ),
+      cell: ({ row }) => (
+        <div className="capitalize">{row.getValue("role")}</div>
+      ),
+      enableSorting: true,
+      enableColumnFilter: false,
+    },
+    {
+      accessorKey: "playerTeam",
+      header: ({ header }) => (
+        <OverviewTableHeader tooltip={tooltips.playerTeam} header={header}>
+          {t("header.playerTeam")}
+        </OverviewTableHeader>
+      ),
+      cell: ({ row }) => (
+        <div className="capitalize">{row.getValue("playerTeam")}</div>
+      ),
+      enableSorting: true,
+      enableColumnFilter: false,
+    },
+    {
+      accessorKey: "timePlayed",
+      header: ({ header }) => (
+        <OverviewTableHeader tooltip={tooltips.timePlayed} header={header}>
+          {t("header.timePlayed")}
+        </OverviewTableHeader>
+      ),
+      cell: ({ row }) => (
+        <div className={cn(GeistMono.className, "text-right capitalize")}>
+          {toTimestamp(row.getValue<number>("timePlayed"))}
+        </div>
+      ),
+      enableSorting: true,
+      enableColumnFilter: false,
+    },
+    {
+      accessorKey: "eliminations",
+      header: ({ header }) => (
+        <OverviewTableHeader tooltip={tooltips.eliminations} header={header}>
+          {t("header.eliminations")}
+        </OverviewTableHeader>
+      ),
+      cell: ({ row }) => (
+        <div className={cn(GeistMono.className, "text-right capitalize")}>
+          {row.getValue("eliminations")}
+        </div>
+      ),
+      enableSorting: true,
+      enableColumnFilter: false,
+    },
+    {
+      accessorKey: "kills",
+      header: ({ header }) => (
+        <OverviewTableHeader tooltip={tooltips.kills} header={header}>
+          {t("header.kills")}
+        </OverviewTableHeader>
+      ),
+      cell: ({ row }) => (
+        <div className={cn(GeistMono.className, "text-right capitalize")}>
+          {row.getValue("kills")}
+        </div>
+      ),
+      enableSorting: true,
+      enableColumnFilter: false,
+    },
+    {
+      accessorKey: "assists",
+      header: ({ header }) => (
+        <OverviewTableHeader tooltip={tooltips.assists} header={header}>
+          {t("header.assists")}
+        </OverviewTableHeader>
+      ),
+      cell: ({ row }) => (
+        <div className={cn(GeistMono.className, "text-right capitalize")}>
+          {row.getValue("assists")}
+        </div>
+      ),
+      enableSorting: true,
+      enableColumnFilter: false,
+    },
+    {
+      accessorKey: "deaths",
+      header: ({ header }) => (
+        <OverviewTableHeader tooltip={tooltips.deaths} header={header}>
+          {t("header.deaths")}
+        </OverviewTableHeader>
+      ),
+      cell: ({ row }) => (
+        <div className={cn(GeistMono.className, "text-right capitalize")}>
+          {row.getValue("deaths")}
+        </div>
+      ),
+      enableSorting: true,
+      enableColumnFilter: false,
+    },
+    {
+      accessorKey: "kd",
+      header: ({ header }) => (
+        <OverviewTableHeader tooltip={tooltips.kd} header={header}>
+          {t("header.kd")}
+        </OverviewTableHeader>
+      ),
+      cell: ({ row }) => (
+        <div className={cn(GeistMono.className, "text-right capitalize")}>
+          {row.getValue<number>("kd").toFixed(2)}
+        </div>
+      ),
+      enableSorting: true,
+      enableColumnFilter: false,
+    },
+    {
+      accessorKey: "kad",
+      header: ({ header }) => (
+        <OverviewTableHeader tooltip={tooltips.kad} header={header}>
+          {t("header.kad")}
+        </OverviewTableHeader>
+      ),
+      cell: ({ row }) => (
+        <div className={cn(GeistMono.className, "text-right capitalize")}>
+          {row.getValue<number>("kad").toFixed(2)}
+        </div>
+      ),
+      enableSorting: true,
+      enableColumnFilter: false,
+    },
+    {
+      accessorKey: "heroDmgDealt",
+      header: ({ header }) => (
+        <OverviewTableHeader tooltip={tooltips.heroDmgDealt} header={header}>
+          {t("header.heroDmgDealt")}
+        </OverviewTableHeader>
+      ),
+      cell: ({ row }) => (
+        <div className={cn(GeistMono.className, "text-right capitalize")}>
+          {row.getValue<number>("heroDmgDealt").toFixed(2)}
+        </div>
+      ),
+      enableSorting: true,
+      enableColumnFilter: false,
+    },
+    {
+      accessorKey: "dmgReceived",
+      header: ({ header }) => (
+        <OverviewTableHeader tooltip={tooltips.dmgReceived} header={header}>
+          {t("header.dmgReceived")}
+        </OverviewTableHeader>
+      ),
+      cell: ({ row }) => (
+        <div className={cn(GeistMono.className, "text-right capitalize")}>
+          {row.getValue<number>("dmgReceived").toFixed(2)}
+        </div>
+      ),
+      enableSorting: true,
+      enableColumnFilter: false,
+    },
+    {
+      accessorKey: "healingReceived",
+      header: ({ header }) => (
+        <OverviewTableHeader tooltip={tooltips.healingReceived} header={header}>
+          {t("header.healingReceived")}
+        </OverviewTableHeader>
+      ),
+      cell: ({ row }) => (
+        <div className={cn(GeistMono.className, "text-right capitalize")}>
+          {row.getValue<number>("healingReceived").toFixed(2)}
+        </div>
+      ),
+      enableSorting: true,
+      enableColumnFilter: false,
+    },
+    {
+      accessorKey: "healingDealt",
+      header: ({ header }) => (
+        <OverviewTableHeader tooltip={tooltips.healingDealt} header={header}>
+          {t("header.healingDealt")}
+        </OverviewTableHeader>
+      ),
+      cell: ({ row }) => (
+        <div className={cn(GeistMono.className, "text-right capitalize")}>
+          {row.getValue<number>("healingDealt").toFixed(2)}
+        </div>
+      ),
+      enableSorting: true,
+      enableColumnFilter: false,
+    },
+    {
+      accessorKey: "dmgToHealsRatio",
+      header: ({ header }) => (
+        <OverviewTableHeader tooltip={tooltips.dmgToHealsRatio} header={header}>
+          {t("header.dmgToHealsRatio")}
+        </OverviewTableHeader>
+      ),
+      cell: ({ row }) => (
+        <div className={cn(GeistMono.className, "text-right capitalize")}>
+          {row.getValue<number>("dmgToHealsRatio").toFixed(2)}
+        </div>
+      ),
+      enableSorting: true,
+      enableColumnFilter: false,
+    },
+    {
+      accessorKey: "ultsCharged",
+      header: ({ header }) => (
+        <OverviewTableHeader tooltip={tooltips.ultsCharged} header={header}>
+          {t("header.ultsCharged")}
+        </OverviewTableHeader>
+      ),
+      cell: ({ row }) => (
+        <div className={cn(GeistMono.className, "text-right capitalize")}>
+          {row.getValue("ultsCharged")}
+        </div>
+      ),
+      enableSorting: true,
+      enableColumnFilter: false,
+    },
+    {
+      accessorKey: "ultsUsed",
+      header: ({ header }) => (
+        <OverviewTableHeader tooltip={tooltips.ultsUsed} header={header}>
+          {t("header.ultsUsed")}
+        </OverviewTableHeader>
+      ),
+      cell: ({ row }) => (
+        <div className={cn(GeistMono.className, "text-right capitalize")}>
+          {row.getValue("ultsUsed")}
+        </div>
+      ),
+      enableSorting: true,
+      enableColumnFilter: false,
+    },
+  ];
+
+  const tooltips = {
+    playerName: t("tooltips.playerName"),
+    role: t("tooltips.role"),
+    playerTeam: t("tooltips.playerTeam"),
+    timePlayed: t("tooltips.timePlayed"),
+    eliminations: t("tooltips.eliminations"),
+    kills: t("tooltips.kills"),
+    assists: t("tooltips.assists"),
+    deaths: t("tooltips.deaths"),
+    kd: t("tooltips.kd"),
+    kad: t("tooltips.kad"),
+    heroDmgDealt: t("tooltips.heroDmgDealt"),
+    dmgReceived: t("tooltips.dmgReceived"),
+    healingReceived: t("tooltips.healingReceived"),
+    healingDealt: t("tooltips.healingDealt"),
+    dmgToHealsRatio: t("tooltips.dmgToHealsRatio"),
+    ultsCharged: t("tooltips.ultsCharged"),
+    ultsUsed: t("tooltips.ultsUsed"),
+  };
 
   const table = useReactTable({
     data: tableData,
@@ -403,7 +407,7 @@ export function OverviewTable({ playerStats }: { playerStats: PlayerStat[] }) {
                   colSpan={table.getAllColumns().length}
                   className="h-24 text-center"
                 >
-                  No results.
+                  {t("noResult")}
                 </TableCell>
               </TableRow>
             )}
