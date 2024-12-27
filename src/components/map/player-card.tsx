cbdd8177ba808c67ddf63c119d3bae069616c12b
Lucas Doell <lucas@lux.dev>
2024-12-27 02:48:46 -0500

    Add translation support for player card title

--- src/components/map/player-card.tsx
+++ src/components/map/player-card.tsx
@@ -3,10 +3,13 @@ import SpecificHero from "@/components/player/specific-hero";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { getPlayerFinalStats } from "@/data/scrim-dto";
+import { getTranslations } from "next-intl/server";
 
 type Props = { id: number; playerName: string };
 
 export default async function PlayerCard({ playerName, id }: Props) {
+  const t = await getTranslations("mapPage.compare.playerCard");
+
   const playerStatsByFinalRound = await getPlayerFinalStats(id, playerName);
 
   const playerStats = playerStatsByFinalRound.filter(
@@ -24,13 +27,15 @@ export default async function PlayerCard({ playerName, id }: Props) {
     <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
       <Card className="col-span-full">
         <CardHeader>
-          <CardTitle>Player Statistics</CardTitle>
+          <CardTitle>{t("title")}</CardTitle>
         </CardHeader>
         <CardContent className="pl-4">
           {heroesPlayed.length > 1 && (
             <Tabs defaultValue="all-heroes" className="space-y-4">
               <TabsList>
-                <TabsTrigger value="all-heroes">All Heroes</TabsTrigger>
+                <TabsTrigger value="all-heroes">
+                  {t("allHeroes.title")}
+                </TabsTrigger>
                 {heroesPlayed.map((hero) => {
                   return (
                     <TabsTrigger key={hero} value={hero}>
