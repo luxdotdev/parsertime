c78e90c47a8d922a55c0927a0995647d206390fd
Lucas Doell <lucas@lux.dev>
2024-12-28 02:51:21 -0500

    Add translations for map names in MapWinsChart

--- src/components/stats/player/charts/map-wins-chart.tsx
+++ src/components/stats/player/charts/map-wins-chart.tsx
@@ -2,7 +2,10 @@
 
 import { CardContent, CardFooter } from "@/components/ui/card";
 import { Winrate } from "@/data/scrim-dto";
-import { cn, toTitleCase } from "@/lib/utils";
+import { cn, toKebabCase, toTitleCase, useMapNames } from "@/lib/utils";
+import { MapName, mapNameToMapTypeMapping } from "@/types/map";
+import { $Enums } from "@prisma/client";
+import { useTranslations } from "next-intl";
 import {
   Bar,
   BarChart,
@@ -34,7 +37,10 @@ const mapVariantMapping: Record<string, string> = {
   "Hollywood (Halloween)": "Hollywood",
 };
 
-function processMapWinrates(mapWinrates: Winrate): Data {
+function processMapWinrates(
+  mapWinrates: Winrate,
+  maps: Map<string, string>
+): Data {
   const mapData: Record<string, { wins: number; losses: number }> = {};
 
   mapWinrates.forEach((winrate) => {
@@ -61,7 +67,7 @@ function processMapWinrates(mapWinrates: Winrate): Data {
   });
 
   const data: Data = Object.keys(mapData).map((mapName) => ({
-    name: toTitleCase(mapName),
+    name: toTitleCase(maps.get(toKebabCase(mapName)) || mapName),
     wins: mapData[mapName].wins,
     losses: mapData[mapName].losses,
   }));
@@ -74,6 +80,8 @@ function CustomTooltip({
   payload,
   label,
 }: TooltipProps<ValueType, NameType>) {
+  const t = useTranslations("statsPage.playerStats.mapWinrates");
+
   if (active && payload && payload.length) {
     const percentage =
       ((payload[0].value as number) /
@@ -84,15 +92,15 @@ function CustomTooltip({
       <div className="z-50 overflow-hidden rounded-md bg-primary px-3 py-1.5 text-xs text-primary-foreground animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2">
         <h3 className="text-base font-bold">{label}</h3>
         <p className="text-sm">
-          Wins:{" "}
+          {t("tooltip.wins")}{" "}
           <span className="text-blue-500">{payload[0].value as number}</span>
         </p>
         <p className="text-sm">
-          Losses:{" "}
+          {t("tooltip.losses")}{" "}
           <span className="text-red-500">{payload[1].value as number}</span>
         </p>
         <p className="text-sm">
-          Percentage:{" "}
+          {t("tooltip.percentage")}{" "}
           <span
             className={cn(percentage >= 50 ? "text-green-500" : "text-red-500")}
           >
@@ -106,16 +114,14 @@ function CustomTooltip({
   return null;
 }
 
-import React from "react";
-import { MapName, mapNameToMapTypeMapping } from "@/types/map";
-import { $Enums } from "@prisma/client";
-
 type Props = {
   data: Winrate;
 };
 
 export function MapWinsChart({ data }: Props) {
-  const processedData = processMapWinrates(data);
+  const t = useTranslations("statsPage.playerStats.mapWinrates");
+  const maps = useMapNames();
+  const processedData = processMapWinrates(data, maps);
 
   return (
     <>
@@ -143,10 +149,10 @@ export function MapWinsChart({ data }: Props) {
             <YAxis />
             <Tooltip content={<CustomTooltip />} />
             <Legend layout="vertical" verticalAlign="top" align="left" />
-            <Bar dataKey="wins" name="Wins" stackId="a" fill="#3b82f6" />
+            <Bar dataKey="wins" name={t("wins")} stackId="a" fill="#3b82f6" />
             <Bar
               dataKey="losses"
-              name="Losses"
+              name={t("losses")}
               stackId="a"
               fill="#ef4444"
               radius={[4, 4, 0, 0]}
@@ -155,11 +161,7 @@ export function MapWinsChart({ data }: Props) {
         </ResponsiveContainer>
       </CardContent>
       <CardFooter>
-        <p className="text-sm text-muted-foreground">
-          Map winrates are calculated based on the number of wins and losses for
-          each map. Please note that Push maps are not included in this chart as
-          they do not have a way to track their win/loss condition.
-        </p>
+        <p className="text-sm text-muted-foreground">{t("footer")}</p>
       </CardFooter>
     </>
   );
