53265044dd82f666f05935085f17e4213af2c8b2
Lucas Doell <lucas@lux.dev>
2024-12-29 00:21:53 -0500

    Add internationalization support for join success page

--- src/app/team/join/success/page.tsx
+++ src/app/team/join/success/page.tsx
@@ -2,18 +2,20 @@
 
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { useToast } from "@/components/ui/use-toast";
+import { useTranslations } from "next-intl";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 export default function TeamJoinSuccessPage() {
+  const t = useTranslations("teamPage.join");
+
   const { toast } = useToast();
   const router = useRouter();
 
   // Show a success message if the user has successfully joined the team
   toast({
-    title: "Joined team",
-    description:
-      "You have successfully joined the team. Redirecting you to your dashboard.",
+    title: t("handleSubmit.title"),
+    description: t("handleSubmit.description"),
     duration: 5000,
   });
 
@@ -26,16 +28,18 @@ export default function TeamJoinSuccessPage() {
     <div className="flex h-screen flex-col items-center justify-center">
       <Card className="h-48 max-w-md">
         <CardHeader className="scroll-m-20 text-xl font-semibold tracking-tight">
-          Success!
+          {t("success.title")}
         </CardHeader>
         <CardContent>
-          <p>You have successfully joined the team.</p>
+          <p>{t("success.description")}</p>
           <p>
-            Redirecting you to your dashboard. If you are not redirected,{" "}
-            <Link href="/dashboard" className="underline">
-              click here
-            </Link>
-            .
+            {t.rich("success.redirect", {
+              link: (chunks) => (
+                <Link href="/dashboard" className="underline">
+                  {chunks}
+                </Link>
+              ),
+            })}
           </p>
         </CardContent>
       </Card>
