543e1d13c69f11afbe83eb8d470cb287c618feff
Lucas Doell <lucas@lux.dev>
2024-12-29 21:17:00 -0500

    Add translations to verify request page

--- src/app/(auth)/verify-request/page.tsx
+++ src/app/(auth)/verify-request/page.tsx
@@ -1,18 +1,18 @@
 import { Button } from "@/components/ui/button";
+import { getTranslations } from "next-intl/server";
 import Link from "next/link";
 
-export default function VerifyRequestPage() {
+export default async function VerifyRequestPage() {
+  const t = await getTranslations("verifyRequest");
+
   return (
     <div className="flex h-[90vh] flex-col items-center justify-center space-y-6 p-6 text-center">
-      <h1 className="text-3xl font-bold">
-        Check your email for a sign-in link
-      </h1>
+      <h1 className="text-3xl font-bold">{t("title")}</h1>
       <p className="max-w-[600px] text-gray-500 dark:text-gray-400">
-        We&apos;ve sent an email to you with a link to sign in. Click the link
-        in the email to complete the sign-in process.
+        {t("description")}
       </p>
       <Button className="mx-auto" asChild>
-        <Link href="/">Back to Home</Link>
+        <Link href="/">{t("back")}</Link>
       </Button>
     </div>
   );
