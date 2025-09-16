41a54e8465093eaf93662c83a150935aac28e36f
Lucas Doell <lucas@lux.dev>
2025-09-15 21:35:55 -0400

    Remove LayoutPropsWithLocale type from next.ts

--- src/types/next.ts
+++ src/types/next.ts
@@ -1,13 +1,9 @@
-import { AppRoutes, LayoutRoutes } from "../../.next/types/routes";
+import { AppRoutes } from "../../.next/types/routes";
 
 export type SearchParams = Promise<{
   [key: string]: string | string[] | undefined;
 }>;
 
 export type PagePropsWithLocale<T extends AppRoutes> = PageProps<T> & {
-  params: Promise<{ locale: string }>;
-};
-
-export type LayoutPropsWithLocale<T extends LayoutRoutes> = LayoutProps<T> & {
   params: Promise<{ locale: string }>;
 };
