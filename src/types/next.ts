f60252e363544f8e087f3fb355e4385839dc3ea5
Lucas Doell <lucas@lux.dev>
2025-09-15 21:12:37 -0400

    Refactor SearchParams and add PagePropsWithLocale and LayoutPropsWithLocale types

--- src/types/next.ts
+++ src/types/next.ts
@@ -1,1 +1,13 @@
-export type SearchParams = { [key: string]: string | string[] | undefined };
+import { AppRoutes, LayoutRoutes } from "../../.next/types/routes";
+
+export type SearchParams = Promise<{
+  [key: string]: string | string[] | undefined;
+}>;
+
+export type PagePropsWithLocale<T extends AppRoutes> = PageProps<T> & {
+  params: Promise<{ locale: string }>;
+};
+
+export type LayoutPropsWithLocale<T extends LayoutRoutes> = LayoutProps<T> & {
+  params: Promise<{ locale: string }>;
+};
