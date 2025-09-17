c2e601e5a61c07ac559337f61975a31e5e5fc564
Lucas Doell <lucas@lux.dev>
2025-09-16 21:40:10 -0400

    Update auth pages with new design

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,12 +1,10 @@
 import { UserAuthForm } from "@/components/auth/user-auth-form";
-import { buttonVariants } from "@/components/ui/button";
+import { Link } from "@/components/ui/link";
 import { auth } from "@/lib/auth";
-import { cn } from "@/lib/utils";
 import type { PagePropsWithLocale } from "@/types/next";
 import type { Metadata } from "next";
 import { getTranslations } from "next-intl/server";
 import Image from "next/image";
-import Link from "next/link";
 import { redirect } from "next/navigation";
 
 export async function generateMetadata(
@@ -52,58 +50,24 @@ export default async function AuthenticationPage() {
   const t = await getTranslations("signInPage");
 
   return (
-    <div className="relative container h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-      <Link href="/" className="md:hidden">
-        <div className="relative top-2 right-4 z-20 flex items-center text-lg font-medium md:top-8 md:right-8">
-          <Image
-            src="/parsertime.png"
-            alt=""
-            width={50}
-            height={50}
-            className="dark:invert"
-          />
-          {t("parsertime")}
-        </div>
-      </Link>
-      <Link
-        href="/sign-up"
-        className={cn(
-          buttonVariants({ variant: "ghost" }),
-          "absolute top-4 right-4 md:top-8 md:right-8"
-        )}
-      >
-        {t("signUp")}
-      </Link>
-      <div className="bg-muted relative hidden h-full flex-col p-10 text-black lg:flex dark:border-r dark:text-white">
-        <div className="bg-topography dark:bg-dark-topography absolute inset-0" />
-        <Link href="/">
-          <div className="relative z-20 flex items-center text-lg font-medium">
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Link
+          href="/"
+          className="flex items-center gap-2 self-center font-medium"
+        >
+          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
             <Image
+              className="invert dark:invert-0"
               src="/parsertime.png"
-              alt=""
-              width={50}
-              height={50}
-              className="dark:invert"
+              alt="Parsertime Logo"
+              width={24}
+              height={24}
             />
-            {t("parsertime")}
           </div>
+          {t("parsertime")}
         </Link>
-        <div className="relative z-20 mt-auto">
-          <p className="space-y-2 text-lg">{t("madeBy")}</p>
-        </div>
-      </div>
-      <div className="pt-20 lg:p-8 lg:pt-8">
-        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-          <div className="flex flex-col space-y-2 text-center">
-            <h1 className="text-2xl font-semibold tracking-tight">
-              {t("signIn")}
-            </h1>
-            <p className="text-muted-foreground text-sm">
-              {t("enterEmailSignIn")}
-            </p>
-          </div>
-          <UserAuthForm />
-        </div>
+        <UserAuthForm />
       </div>
     </div>
   );
