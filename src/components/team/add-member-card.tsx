cc8d819a38c779c61e076f1d26253950c63670c8
Lucas Doell <lucas@lux.dev>
2025-09-17 18:46:54 -0400

    Refactor AddMemberCard component for improved layout

--- src/components/team/add-member-card.tsx
+++ src/components/team/add-member-card.tsx
@@ -14,11 +14,11 @@ export function AddMemberCard() {
   return (
     <div className="w-full p-2 md:w-1/2 xl:w-1/3">
       <Card className="flex h-36 max-w-md flex-col items-center justify-center border-dashed">
-        <CardHeader className="text-center text-xl">
-          <span className="inline-flex items-center justify-center space-x-2">
-            <PlusCircledIcon className="h-6 w-6" />{" "}
+        <CardHeader className="flex items-center justify-center text-xl">
+          <div className="flex items-center gap-2 whitespace-nowrap">
+            <PlusCircledIcon className="h-6 w-6" />
             <span>{t("addMember")}</span>
-          </span>
+          </div>
         </CardHeader>
         <CardDescription className="pb-4">
           <Button onClick={() => setShowInviteMemberModal(true)}>
