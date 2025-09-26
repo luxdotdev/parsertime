bcd1800feef626948b1311c71715ff2243739d87
Lucas Doell <lucas@lux.dev>
2025-09-26 15:23:08 -0400

    Remove teamId column from User model in schema.prisma

---
+++ prisma/migrations/20250926192258_remove_user_team_id/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `teamId` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."User" DROP COLUMN "teamId";
