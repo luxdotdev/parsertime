81803f157c6d9db0d23aafbc2126cd64e9801395
Lucas Doell <lucas@lux.dev>
2025-09-27 22:11:05 -0400

    Add function to retrieve app settings for a user and cache the result

--- src/data/user-dto.tsx
+++ src/data/user-dto.tsx
@@ -53,3 +53,23 @@ async function getTeamsWithPermsFn(email: string | undefined) {
  * @returns The teams that the user has permission to view.
  */
 export const getTeamsWithPerms = cache(getTeamsWithPermsFn);
+
+async function getAppSettingsFn(email: string | undefined) {
+  const user = await getUser(email);
+  if (!user) return null;
+
+  const appSettings = await prisma.appSettings.findFirst({
+    where: { userId: user?.id },
+  });
+
+  return appSettings;
+}
+
+/**
+ * Get the app settings for a user.
+ * This function is cached for performance.
+ *
+ * @param email - The email address of the user to get app settings for.
+ * @returns The app settings for the user.
+ */
+export const getAppSettings = cache(getAppSettingsFn);
