669bb485edc6d609b3e9d46cd208d4b07efd2e71
Caleb Krainman <caleb@lux.dev>
2025-10-18 16:07:25 -0400

    Implement API for creating and fetching notes in scrim, including validation and authorization checks

---
+++ src/app/api/scrim/edit-note/route.ts
@@ -0,0 +1,29 @@
+import { noteDataSchema } from "@/components/tiptap/tiptap";
+import { getUser } from "@/data/user-dto";
+import { auth } from "@/lib/auth";
+import { upsertNote } from "@/lib/notes";
+import { notFound, unauthorized } from "next/navigation";
+import type { NextRequest } from "next/server";
+
+export async function POST(request: NextRequest) {
+  const session = await auth();
+  if (!session) unauthorized();
+
+  const user = await getUser(session.user.email);
+  if (!user) notFound();
+
+  const body = await request.json();
+
+  const noteData = noteDataSchema.safeParse(body);
+  if (!noteData.success) {
+    return new Response("Invalid note data", { status: 400 });
+  }
+
+  await upsertNote({
+    scrimId: noteData.data.scrimId,
+    mapDataId: noteData.data.mapDataId,
+    content: noteData.data.content,
+  });
+
+  return new Response("Note created", { status: 201 });
+}
--- src/components/tiptap/tiptap.tsx
+++ src/components/tiptap/tiptap.tsx
@@ -8,15 +8,65 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { useQuery } from "@tanstack/react-query";
 import Highlight from "@tiptap/extension-highlight";
 import TextAlign from "@tiptap/extension-text-align";
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import { useTranslations } from "next-intl";
+import { usePathname } from "next/navigation";
+import z from "zod";
+
+export const noteDataSchema = z.object({
+  scrimId: z.number(),
+  mapDataId: z.number(),
+  content: z.string(),
+});
+
+async function fetchNotes(mapDataId: number, scrimId: number, content: string) {
+  const response = await fetch("/api/scrim/edit-note", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ mapDataId, scrimId, content }),
+  });
+  if (!response.ok) {
+    throw new Error("Failed to fetch notes");
+  }
+  const data = noteDataSchema.parse(await response.json());
+
+  return data;
+}
 
 export function TipTap() {
   const t = useTranslations("mapPage.tiptap");
+
+  const pathname = usePathname();
+  const pathSegments = pathname.split("/");
+
+  const scrimId = Number(pathSegments[3]);
+  const mapDataId = Number(pathSegments[5]);
+
+  const {
+    data: noteData,
+    isError,
+    isLoading,
+  } = useQuery({
+    queryKey: ["fetch-scrim-notes"],
+    queryFn: async () => {
+      return fetchNotes(mapDataId, scrimId, "");
+    },
+  });
+
+  const content = isError
+    ? "Error loading notes."
+    : isLoading
+      ? "Loading notes..."
+      : noteData?.content;
+
   const editor = useEditor({
+    content,
     extensions: [
       StarterKit.configure({
         bulletList: {
@@ -40,12 +90,10 @@ export function TipTap() {
     ],
     editorProps: {
       attributes: {
-        enableTabIndentation: "true",
         class:
-          "min-h-[300px] p-4 border border-muted rounded-lg bg-background prose prose-sm sm:prose lg:prose-lg xl:prose-xl focus:outline-none focus:ring-2 focus:ring-ring focus:border-ring dark:prose-invert max-w-full",
+          "min-h-[300px] p-4 border border-muted rounded-lg bg-background prose prose-sm sm:prose lg:prose-lg xl:prose-xl focus:outline-none focus:ring-1 focus:ring-ring focus:border-ring dark:prose-invert max-w-full",
       },
     },
-    content: "",
     immediatelyRender: false,
   });
 
