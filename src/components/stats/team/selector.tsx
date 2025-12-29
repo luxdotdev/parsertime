96fca683a7595286d4c27102fa2c93dfd5ca691a
Lucas Doell <lucas@lux.dev>
2025-12-29 16:12:06 -0500

    Add localization for Team Selector component, including team selection placeholder

--- src/components/stats/team/selector.tsx
+++ src/components/stats/team/selector.tsx
@@ -9,17 +9,19 @@ import {
 } from "@/components/ui/select";
 import type { Team } from "@prisma/client";
 import type { Route } from "next";
+import { useTranslations } from "next-intl";
 import { useRouter } from "next/navigation";
 
 export function TeamSelector({ teams }: { teams: Team[] }) {
   const router = useRouter();
+  const t = useTranslations("teamStatsPage.selector");
 
   return (
     <Select
       onValueChange={(value) => router.push(`/stats/team/${value}` as Route)}
     >
       <SelectTrigger className="w-full max-w-md md:max-w-lg">
-        <SelectValue placeholder="Select a team" />
+        <SelectValue placeholder={t("select")} />
       </SelectTrigger>
       <SelectContent>
         {teams.map((team) => (
