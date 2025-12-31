25546f136c4280a7d2ccb683d3af55e8ee91d8f1
Caleb Krainman <caleb@lux.dev>
2025-12-31 02:49:38 -0500

    Refactor VOD POST request handler to improve input validation and error logging

--- src/app/api/vod/route.ts
+++ src/app/api/vod/route.ts
@@ -1,15 +1,37 @@
+import { getUser } from "@/data/user-dto";
+import { auth } from "@/lib/auth";
+import { Logger } from "@/lib/logger";
+import { unauthorized } from "next/dist/client/components/navigation";
+
 type VodRequestBody = {
   mapId: number;
   vodUrl: string;
 };
 export async function POST(req: Request) {
+  const session = await auth();
+  if (!session) unauthorized();
+
+  const user = await getUser(session.user?.email);
+
+  if (!user) {
+    Logger.error("User not found for session: ", session);
+    throw new Error("User not found");
+  }
+
   const body = (await req.json()) as VodRequestBody;
   const { mapId, vodUrl } = body;
 
   // Validate input
-  if (!mapId || !vodUrl) {
-    return new Response("Missing mapId or vodUrl", { status: 400 });
+  if (!mapId) {
+    Logger.error("Missing mapId in request body");
+    return new Response("Missing mapId", { status: 400 });
   }
+
+  if (!vodUrl) {
+    Logger.error("Invalid or missing vodUrl in request body");
+    return new Response("Invalid or missing vodUrl", { status: 400 });
+  }
+
   const updatedVod = await prisma?.map.upsert({
     where: { id: mapId },
     update: { vod: vodUrl },
