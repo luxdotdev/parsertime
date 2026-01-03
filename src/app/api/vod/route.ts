df25f741c933ded65c8695679865b56b28f2ad97
Caleb Krainman <krainmcd@miamioh.edu>
2026-01-02 16:04:51 -0800

    Refactor VOD route to enhance input validation and streamline response handling

--- src/app/api/vod/route.ts
+++ src/app/api/vod/route.ts
@@ -3,56 +3,52 @@ import { auth } from "@/lib/auth";
 import { Logger } from "@/lib/logger";
 import { NextResponse } from "next/server";
 import z from "zod";
+import prisma from "@/lib/prisma";
+import { unauthorized } from "next/navigation";
 
+const ALLOWED_DOMAINS = [
+  "https://www.youtube.com/",
+  "https://youtube.com/",
+  "https://youtu.be/",
+  "https://www.twitch.tv/",
+  "https://twitch.tv/",
+];
 const VodSchema = z.object({
   mapId: z.number().min(1),
-  vodUrl: z.string().url(),
+  vodUrl: z
+    .string()
+    .url()
+    .refine((url) => {
+      return ALLOWED_DOMAINS.some((domain) => url.startsWith(domain));
+    }),
 });
 
 export async function POST(req: Request) {
-  try {
-    const session = await auth();
+  const session = await auth();
 
-    if (!session) {
-      return new NextResponse("Unauthorized", { status: 401 });
-    }
+  if (!session) unauthorized();
 
-    const user = await getUser(session.user.email);
+  const user = await getUser(session.user.email);
 
-    if (!user) {
-      Logger.error("User not found for session: ", session);
-      throw new Error("User not found");
-    }
-
-    const json = await req.json();
-    const body = VodSchema.safeParse(json);
-
-    if (!body.success) {
-      Logger.error("Invalid request body: ", body.error);
-      return new NextResponse("Invalid request body", { status: 400 });
-    }
-
-    const { mapId, vodUrl } = body.data;
-
-    // Validate input
-    if (!mapId) {
-      Logger.error("Missing mapId in request body");
-      return new NextResponse("Missing mapId", { status: 400 });
-    }
-
-    if (!vodUrl) {
-      Logger.error("Invalid or missing vodUrl in request body");
-      return new NextResponse("Invalid or missing vodUrl", { status: 400 });
-    }
-
-    const updatedVod = await prisma?.map.upsert({
-      where: { id: mapId },
-      update: { vod: vodUrl },
-      create: { id: mapId, vod: vodUrl, name: `Map ${mapId}` },
-    });
-    return new Response(JSON.stringify(updatedVod), { status: 200 });
-  } catch (error) {
-    Logger.error("Error in /api/vod route: ", error);
-    return new NextResponse("Internal Server Error", { status: 500 });
+  if (!user) {
+    Logger.error("User not found for session: ", session.user);
+    return new NextResponse("User not found", { status: 404 });
   }
+
+  const json = await req.json();
+  const body = VodSchema.safeParse(json);
+
+  if (!body.success) {
+    Logger.error("Invalid request body: ", body.error);
+    return new NextResponse("Invalid request body", { status: 400 });
+  }
+
+  const { mapId, vodUrl } = body.data;
+
+  const updatedVod = await prisma.map.update({
+    where: { id: mapId },
+    data: { vod: vodUrl },
+  });
+
+  return new NextResponse(JSON.stringify(updatedVod), { status: 200 });
 }
