16aace8a3021d2cda2f066a331d36fb4a5a9853f
Caleb Krainman <krainmcd@miamioh.edu>
2026-01-02 16:04:55 -0800

    Refactor VodForm component to enhance URL validation and streamline VOD linking process

--- src/components/vods/vod-form.tsx
+++ src/components/vods/vod-form.tsx
@@ -6,30 +6,49 @@ import { Form, FormField, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useTranslations } from "next-intl";
+import { useRouter } from "next/navigation";
 import { type Dispatch, useState } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 import { z } from "zod";
 
+const ALLOWED_DOMAINS = [
+  "https://www.youtube.com/",
+  "https://youtube.com/",
+  "https://youtu.be/",
+  "https://www.twitch.tv/",
+  "https://twitch.tv/",
+];
 const formSchema = z.object({
-  vodUrl: z.string().min(1, { message: "Please enter a VOD URL" }),
+  vodUrl: z
+    .string()
+    .min(1, { message: "Please enter a VOD URL" })
+    .refine(
+      (url) => {
+        return ALLOWED_DOMAINS.some((domain) => url.startsWith(domain));
+      },
+      { message: "URL must be from YouTube or Twitch" }
+    ),
 });
 
 export function VodForm({
   mapId,
   setVodState,
+  vodState,
   setIsOpen,
 }: {
   mapId: number;
   setVodState: Dispatch<React.SetStateAction<string>>;
+  vodState: string;
   setIsOpen: Dispatch<React.SetStateAction<boolean>>;
 }) {
+  const router = useRouter();
   const t = useTranslations("mapPage.vod");
   const [loading, setLoading] = useState(false);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      vodUrl: "",
+      vodUrl: vodState || "",
     },
   });
 
@@ -40,6 +59,7 @@ export function VodForm({
       mapId,
       vodUrl: data.vodUrl,
     };
+
     try {
       const res = await fetch("/api/vod", {
         method: "POST",
@@ -48,15 +68,18 @@ export function VodForm({
         },
         body: JSON.stringify(reqBody),
       });
+
       if (!res.ok) {
         toast.error("Failed to link VOD:");
         setLoading(false);
         return;
       }
+
       toast.success("Successfully linked VOD");
       setVodState(data.vodUrl);
       setLoading(false);
       setIsOpen(false);
+      router.refresh();
       return res.json();
     } catch (error) {
       toast.error("An error occurred while linking the VOD.", {
