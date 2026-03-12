8e7aaaddb49c9d499126b64e0a8a3584458ac215
Lucas Doell <lucas@lux.dev>
2026-03-12 12:56:50 -0400

    Refactor DashboardLayout component by removing FeatureFlagsProvider and restructuring layout for improved readability and organization

--- src/components/dashboard-layout.tsx
+++ src/components/dashboard-layout.tsx
@@ -1,7 +1,6 @@
 import { MainNav } from "@/components/dashboard/main-nav";
 import { Search } from "@/components/dashboard/search";
 import { TeamSwitcher } from "@/components/dashboard/team-switcher";
-import { FeatureFlagsProvider } from "@/components/feature-flags-provider";
 import { Footer } from "@/components/footer";
 import { GuestNav } from "@/components/guest-nav";
 import { LocaleSwitcher } from "@/components/locale-switcher";
@@ -27,51 +26,49 @@ export async function DashboardLayout({
   const scoutingEnabled = await scoutingTool();
 
   return (
-    <FeatureFlagsProvider scoutingEnabled={scoutingEnabled}>
-      <TeamSwitcherProvider>
-        <div className="min-h-[90vh] flex-col md:flex">
-          <div className="border-b">
-            <div className="hidden h-16 items-center px-4 md:flex">
-              <TeamSwitcher session={session} />
-              <MainNav
-                scoutingEnabled={scoutingEnabled}
-                className="mx-6 hidden lg:block"
-              />
-              <MobileNav className="block pl-2 lg:hidden" session={session} />
-              <div className="ml-auto flex items-center space-x-4">
-                <Search user={user} />
-                <ModeToggle />
-                <LocaleSwitcher />
-                {session ? (
-                  <>
-                    <Notifications />
-                    <UserNav />
-                  </>
-                ) : (
-                  <GuestNav guestMode={guestMode ?? false} />
-                )}
-              </div>
-            </div>
-            <div className="flex h-16 items-center px-4 md:hidden">
-              <MobileNav session={session} />
-              <div className="ml-auto flex items-center space-x-4">
-                <ModeToggle />
-                <LocaleSwitcher />
-                {session ? (
-                  <>
-                    <Notifications />
-                    <UserNav />
-                  </>
-                ) : (
-                  <GuestNav guestMode={guestMode ?? false} />
-                )}
-              </div>
+    <TeamSwitcherProvider>
+      <div className="min-h-[90vh] flex-col md:flex">
+        <div className="border-b">
+          <div className="hidden h-16 items-center px-4 md:flex">
+            <TeamSwitcher session={session} />
+            <MainNav
+              scoutingEnabled={scoutingEnabled}
+              className="mx-6 hidden lg:block"
+            />
+            <MobileNav className="block pl-2 lg:hidden" session={session} />
+            <div className="ml-auto flex items-center space-x-4">
+              <Search user={user} />
+              <ModeToggle />
+              <LocaleSwitcher />
+              {session ? (
+                <>
+                  <Notifications />
+                  <UserNav />
+                </>
+              ) : (
+                <GuestNav guestMode={guestMode ?? false} />
+              )}
             </div>
           </div>
-          {children}
+          <div className="flex h-16 items-center px-4 md:hidden">
+            <MobileNav session={session} />
+            <div className="ml-auto flex items-center space-x-4">
+              <ModeToggle />
+              <LocaleSwitcher />
+              {session ? (
+                <>
+                  <Notifications />
+                  <UserNav />
+                </>
+              ) : (
+                <GuestNav guestMode={guestMode ?? false} />
+              )}
+            </div>
+          </div>
         </div>
-        <Footer />
-      </TeamSwitcherProvider>
-    </FeatureFlagsProvider>
+        {children}
+      </div>
+      <Footer />
+    </TeamSwitcherProvider>
   );
 }
