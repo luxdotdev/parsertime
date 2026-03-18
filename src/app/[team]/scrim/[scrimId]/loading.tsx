2ebfbb671b59f177638db6bde56987413a2455fd
Lucas Doell <lucas@lux.dev>
2026-03-18 18:20:28 -0400

    Refactor ScrimLoading component to improve loading skeleton structure and styling

--- src/app/[team]/scrim/[scrimId]/loading.tsx
+++ src/app/[team]/scrim/[scrimId]/loading.tsx
@@ -1,5 +1,4 @@
 import { DashboardLayout } from "@/components/dashboard-layout";
-import { Card } from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
 import { getTranslations } from "next-intl/server";
 
@@ -8,25 +7,27 @@ export default async function ScrimLoading() {
 
   return (
     <DashboardLayout>
-      <div className="min-h-[90vh] flex-col md:flex">
-        <div className="flex-1 space-y-4 p-8 pt-6">
+      <div className="flex-1 space-y-6 p-8 pt-6">
+        <div>
           <Skeleton className="h-6 w-24" />
-          <div className="flex items-center justify-between">
+          <div className="mt-2 flex items-center justify-between">
             <Skeleton className="h-8 w-48" />
           </div>
-          <Skeleton className="h-4 w-36" />
-          <p className="scroll-m-20 pb-2 text-2xl font-semibold tracking-tight">
+          <Skeleton className="mt-2 h-4 w-36" />
+        </div>
+
+        <div className="space-y-4">
+          <h3 className="text-2xl font-semibold tracking-tight">
             {t("maps.title")}
-          </p>
-          <div className="-m-2 flex flex-wrap">
-            {/* Simulate loading for maps */}
+          </h3>
+
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
             {Array.from({ length: 6 }).map((_, index) => (
-              // eslint-disable-next-line react/no-array-index-key -- Elements are not unique
-              <div key={index} className="w-full p-2 md:w-1/3">
-                <Card className="h-48 max-w-md">
-                  <Skeleton className="h-full w-full" />
-                </Card>
-              </div>
+              <Skeleton
+                // eslint-disable-next-line react/no-array-index-key -- Skeleton elements are not unique
+                key={index}
+                className="aspect-video rounded-xl"
+              />
             ))}
           </div>
         </div>
