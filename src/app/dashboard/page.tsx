af7a4e74e77357440f658b1528b17445e69911f7
Lucas Doell <lucas@lux.dev>
2026-03-17 21:20:02 -0400

    Add text-balance for dashboard title

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -53,7 +53,9 @@ export default async function DashboardPage() {
   return (
     <div className="flex-1 space-y-4 p-8 pt-6">
       <div className="flex items-center justify-between space-y-2">
-        <h2 className="text-3xl font-bold tracking-tight">{t("title")}</h2>
+        <h2 className="text-3xl font-bold tracking-tight text-balance">
+          {t("title")}
+        </h2>
       </div>
       <Tabs defaultValue="overview" className="space-y-4">
         {isAdmin && (
