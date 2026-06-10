7ff4aa7c23aff360666a7b167b71718c23928aad
Lucas Doell <lucas@lux.dev>
2026-06-10 02:23:20 -0400

    Avoid runtime Prisma enum import in hotfix

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,27 @@
-import { CalculatedStatType, PrismaClient, type Prisma } from "@prisma/client";
+import {
+  PrismaClient,
+  type CalculatedStatType,
+  type Prisma,
+} from "@prisma/client";
 
 // Main can share prod data with newer deploys. Hide enum rows this client
 // cannot decode until the schema/client update lands.
-const supportedCalculatedStatTypes = Object.values(CalculatedStatType);
+const supportedCalculatedStatTypes = [
+  "FLETA_DEADLIFT_PERCENTAGE",
+  "FIRST_PICK_PERCENTAGE",
+  "FIRST_PICK_COUNT",
+  "FIRST_DEATH_PERCENTAGE",
+  "FIRST_DEATH_COUNT",
+  "MVP_SCORE",
+  "MAP_MVP_COUNT",
+  "AJAX_COUNT",
+  "AVERAGE_ULT_CHARGE_TIME",
+  "AVERAGE_TIME_TO_USE_ULT",
+  "AVERAGE_DROUGHT_TIME",
+  "KILLS_PER_ULTIMATE",
+  "DUEL_WINRATE_PERCENTAGE",
+  "FIGHT_REVERSAL_PERCENTAGE",
+] satisfies CalculatedStatType[];
 
 function onlySupportedCalculatedStatTypes<
   T extends { where?: Prisma.CalculatedStatWhereInput },
